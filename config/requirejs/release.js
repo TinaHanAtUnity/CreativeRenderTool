368c4fd8d79507a364e93aa23ee64ddb41a41e30
Ville Orkas <villeo@unity3d.com>
2015-09-07 15:28:14 +0300

    Fix paths for travis

--- config/requirejs/release.js
+++ config/requirejs/release.js
@@ -1,9 +1,9 @@
 ({
-  name: 'main',
-  baseUrl: './../../build/js/',
+  name: 'Main',
+  baseUrl: '../../build/js/',
   paths: {
-    text: './../../src/vendor/require-text',
-    html: './../../src/html'
+    text: '../../node_modules/requirejs-text/text',
+    html: '../../src/html'
   },
   config: {
     text: {
@@ -12,7 +12,7 @@
       }
     }
   },
-  out: './../../build/main.js',
+  out: '../../build/main.js',
   optimize: 'uglify2',
   uglify2: {
     output: {
