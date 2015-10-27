11189a920e9f1ad1bc19afea7aaf3ce601b0df96
Ville Orkas <villeo@unity3d.com>
2015-10-27 11:56:10 +0200

    Skip & video duration tweaks

--- src/ts/Views/Overlay.ts
+++ src/ts/Views/Overlay.ts
@@ -8,6 +8,7 @@ export default class Overlay extends View {
 
     private _skipEnabled: boolean;
     private _skipDuration: number;
+    private _skipRemaining: number;
 
     private _videoDuration: number;
     private _videoProgress: number;
@@ -58,27 +59,20 @@ export default class Overlay extends View {
     }
 
     public setSkipDuration(value: number): void {
-        this._skipDuration = value * 1000;
-        if(value <= 0) {
-            this._skipElement.innerHTML = 'Skip Video';
-        } else {
-            this._skipDurationElement.innerHTML = value.toString();
-        }
+        this._skipDuration = this._skipRemaining = value * 1000;
+        this.setSkipText(value);
     }
 
     public setVideoDuration(value: number): void {
         this._videoDuration = value;
+        this._videoDurationElement.innerHTML = Math.round(value / 1000).toString();
     }
 
     public setVideoProgress(value: number): void {
         this._videoProgress = value;
-        if(this._skipEnabled && this._skipDuration >= 0) {
-            let skipRemaining: number = Math.round((this._skipDuration - value) / 1000);
-            if(skipRemaining <= 0) {
-                this._skipElement.innerHTML = 'Skip Video';
-            } else {
-                this._skipDurationElement.innerHTML = skipRemaining.toString();
-            }
+        if(this._skipEnabled && this._skipRemaining > 0) {
+            this._skipRemaining = Math.round((this._skipDuration - value) / 1000);
+            this.setSkipText(this._skipRemaining);
         }
         this._videoDurationElement.innerHTML = Math.round((this._videoDuration - value) / 1000).toString();
     }
@@ -87,9 +81,17 @@ export default class Overlay extends View {
         return this._muted;
     }
 
+    private setSkipText(skipRemaining: number): void {
+        if(skipRemaining <= 0) {
+            this._skipElement.innerHTML = 'Skip Video';
+        } else {
+            this._skipDurationElement.innerHTML = skipRemaining.toString();
+        }
+    }
+
     private onSkip(event: Event): void {
         event.preventDefault();
-        if(this._videoProgress > this._skipDuration) {
+        if(this._skipEnabled && this._videoProgress > this._skipDuration) {
             this.trigger('skip');
         }
     }
