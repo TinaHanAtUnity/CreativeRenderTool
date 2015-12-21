1526a22f9eb3c6964ccc6f17925a504e18372879
Ville Orkas <villeo@unity3d.com>
2015-12-21 18:31:43 +0200

    Add cache object to fake cache api

--- test/Api/Cache.ts
+++ test/Api/Cache.ts
@@ -3,14 +3,18 @@
 export class Cache {
 
     private static _cacheQueue: string[] = [];
+    private static _cache: { [key: string]: string } = {};
 
     public download(url: string, overwrite: boolean): any[] {
         if(Cache._cacheQueue.some(cacheUrl => cacheUrl === url)) {
             return ['ERROR', 'FILE_ALREADY_IN_QUEUE', url];
+        } else if(url in Cache._cache) {
+            return ['ERROR', 'FILE_ALREADY_IN_CACHE', url];
         } else {
             Cache._cacheQueue.push(url);
             setTimeout(() => {
                 Cache._cacheQueue = Cache._cacheQueue.filter(cacheUrl => cacheUrl !== url);
+                Cache._cache[url] = url.replace('http', 'file');
                 window['nativebridge'].handleEvent('CACHE_DOWNLOAD_END', url);
             }, 0);
             return ['OK'];
@@ -18,7 +22,11 @@ export class Cache {
     }
 
     public getFileUrl(url: string): any[] {
-        return ['OK', url.replace('http', 'file')];
+        if(url in Cache._cache) {
+            return ['OK', Cache._cache[url]];
+        } else {
+            return ['ERROR', 'FILE_NOT_FOUND'];
+        }
     }
 
 }
