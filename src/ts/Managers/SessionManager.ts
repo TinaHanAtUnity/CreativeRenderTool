5aa80dd2ae809e26519bc8a7c23bfd30e1045577
Ville Orkas <villeo@unity3d.com>
2016-01-04 11:43:58 +0200

    Forget about storage for now

--- src/ts/Managers/SessionManager.ts
+++ src/ts/Managers/SessionManager.ts
@@ -1,6 +1,5 @@
 import { NativeBridge } from 'NativeBridge';
 import { Session } from 'Models/Session';
-import { StorageManager, StorageType } from 'Managers/StorageManager';
 
 export enum SessionEventType {
     OPERATIVE,
@@ -12,21 +11,13 @@ export class SessionManager {
     private _nativeBridge: NativeBridge;
     private _currentSession: Session;
 
-    private _storageManager: StorageManager;
-
     constructor(nativeBridge: NativeBridge) {
         this._nativeBridge = nativeBridge;
-        this._storageManager = new StorageManager(nativeBridge);
     }
 
     public create(): Promise<void> {
-        return this._storageManager.read(StorageType.PUBLIC).then(() => {
-            return this._storageManager.get(StorageType.PUBLIC, 'session_start');
-        }).then(([sessionStart]) => {
-            console.log(sessionStart);
-            return this._nativeBridge.invoke('DeviceInfo', 'getUniqueEventId').then(([id]) => {
-                this._currentSession = new Session(id);
-            });
+        return this._nativeBridge.invoke('DeviceInfo', 'getUniqueEventId').then(([id]) => {
+            this._currentSession = new Session(id);
         });
     }
 
