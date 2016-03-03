cd3719950880bf345ffdb4fc046e33b70b426604
Janne Sinivirta <janne.sinivirta@gmail.com>
2016-03-03 09:27:55 +0200

    tests for onVideoStart

--- test/EventHandlers/VideoAdEventHandlersTest.ts
+++ test/EventHandlers/VideoAdEventHandlersTest.ts
@@ -34,7 +34,52 @@ describe('VideoAdEventHandlersTest', () => {
             assert.isOk(adUnitMock.setVideoPosition.notCalled);
             assert.isOk(overlay.setVideoProgress.calledWith(-5));
         });
+    });
 
+    describe('with onVideoStart', () => {
+        let adUnitMock;
+        let sendStart;
+        let invoke;
+
+        beforeEach(() => {
+            invoke = sinon.spy();
+            sendStart = sinon.spy();
+            adUnitMock = {
+                getWatches: sinon.mock(),
+                getSessionManager: () => ({ sendStart: sendStart }),
+                newWatch: sinon.spy(),
+                getNativeBridge: () => ({ invoke: invoke }),
+                getPlacement: () => ({ getId: () => 1 })
+            };
+        });
+
+        it('should send start event with SessionManager', () => {
+            VideoAdEventHandlers.onVideoStart(<VideoAdUnit> <any> adUnitMock);
+
+            assert.isOk(sendStart.calledWith(adUnitMock));
+        });
+
+        it('should call newWatch', () => {
+            VideoAdEventHandlers.onVideoStart(<VideoAdUnit> <any> adUnitMock);
+
+            assert.isOk(adUnitMock.newWatch.called);
+        });
+
+        it('on first watch, should call sendStartEvent callback', () => {
+            adUnitMock.getWatches.returns(0);
+            VideoAdEventHandlers.onVideoStart(<VideoAdUnit> <any> adUnitMock);
+
+            assert.isOk(invoke.calledWith('Listener', 'sendStartEvent', [1]));
+        });
+
+        it('on second watch, should not call sendStartEvent', () => {
+            adUnitMock.getWatches.returns(1);
+            VideoAdEventHandlers.onVideoStart(<VideoAdUnit> <any> adUnitMock);
+
+            assert.isOk(invoke.notCalled);
+        });
 
     });
+
+
 });
