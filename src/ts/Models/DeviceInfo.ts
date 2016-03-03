7de1f2573b7e375d707511ef3b49ed5a05fff1c0
Ville Orkas <villeo@unity3d.com>
2016-03-03 18:06:40 +0200

    Initial fix for Promise "branching" in batched calls

--- src/ts/Models/DeviceInfo.ts
+++ src/ts/Models/DeviceInfo.ts
@@ -41,35 +41,37 @@ export class DeviceInfo extends Model {
 
     public fetch(nativeBridge: NativeBridge): Promise<any[]> {
         let className: string = 'DeviceInfo';
+        let promises = [];
         let batch: BatchInvocation = new BatchInvocation(nativeBridge);
-        batch.queue(className, 'getAndroidId').then(([androidId]) => this._androidId = androidId);
-        batch.queue(className, 'getAdvertisingTrackingId').then(([advertisingIdentifier]) => this._advertisingIdentifier = advertisingIdentifier);
-        batch.queue(className, 'getLimitAdTrackingFlag').then(([limitAdTracking]) => this._limitAdTracking = limitAdTracking);
-        batch.queue(className, 'getApiLevel').then(([apiLevel]) => this._apiLevel = apiLevel);
-        batch.queue(className, 'getOsVersion').then(([osVersion]) => this._osVersion = osVersion);
-        batch.queue(className, 'getManufacturer').then(([manufacturer]) => this._manufacturer = manufacturer);
-        batch.queue(className, 'getModel').then(([model]) => this._model = model);
-        batch.queue(className, 'getConnectionType').then(([connectionType]) => this._connectionType = connectionType);
-        batch.queue(className, 'getNetworkType').then(([networkType]) => this._networkType = networkType);
-        batch.queue(className, 'getScreenLayout').then(([screenLayout]) => this._screenLayout = screenLayout);
-        batch.queue(className, 'getScreenDensity').then(([screenDensity]) => this._screenDensity = screenDensity);
-        batch.queue(className, 'getScreenWidth').then(([screenWidth]) => this._screenWidth = screenWidth);
-        batch.queue(className, 'getScreenHeight').then(([screenHeight]) => this._screenHeight = screenHeight);
-        batch.queue(className, 'getNetworkOperatorName').then(([networkOperator]) => this._networkOperator = networkOperator);
-        batch.queue(className, 'getHeadset').then(([headset]) => this._headset = headset);
-        batch.queue(className, 'getRingerMode').then(([ringerMode]) => this._ringerMode = ringerMode);
-        batch.queue(className, 'getSystemLanguage').then(([language]) => this._language = language);
-        batch.queue(className, 'getDeviceVolume', [StreamType.STREAM_SYSTEM]).then(([volume]) => this._volume = volume);
-        batch.queue(className, 'getScreenBrightness').then(([screenBrightness]) => this._screenBrightness = screenBrightness);
-        batch.queue(className, 'getFreeSpace', [StorageType[StorageType.EXTERNAL]]).then(([freeExternalSpace]) => this._freeExternalSpace = freeExternalSpace);
-        batch.queue(className, 'getTotalSpace', [StorageType[StorageType.EXTERNAL]]).then(([totalExternalSpace]) => this._totalExternalSpace = totalExternalSpace);
-        batch.queue(className, 'getFreeSpace', [StorageType[StorageType.INTERNAL]]).then(([freeInternalSpace]) => this._freeInternalSpace = freeInternalSpace);
-        batch.queue(className, 'getTotalSpace', [StorageType[StorageType.INTERNAL]]).then(([totalInternalSpace]) => this._totalInternalSpace = totalInternalSpace);
-        batch.queue(className, 'getBatteryLevel').then(([batteryLevel]) => this._batteryLevel = batteryLevel);
-        batch.queue(className, 'getBatteryStatus').then(([batteryStatus]) => this._batteryStatus = batteryStatus);
-        batch.queue(className, 'getFreeMemory').then(([freeMemory]) => this._freeMemory = freeMemory);
-        batch.queue(className, 'getTotalMemory').then(([totalMemory]) => this._totalMemory = totalMemory);
-        return nativeBridge.invokeBatch(batch);
+        promises.push(batch.queue(className, 'getAndroidId').then(([androidId]) => this._androidId = androidId));
+        promises.push(batch.queue(className, 'getAdvertisingTrackingId').then(([advertisingIdentifier]) => this._advertisingIdentifier = advertisingIdentifier));
+        promises.push(batch.queue(className, 'getLimitAdTrackingFlag').then(([limitAdTracking]) => this._limitAdTracking = limitAdTracking));
+        promises.push(batch.queue(className, 'getApiLevel').then(([apiLevel]) => this._apiLevel = apiLevel));
+        promises.push(batch.queue(className, 'getOsVersion').then(([osVersion]) => this._osVersion = osVersion));
+        promises.push(batch.queue(className, 'getManufacturer').then(([manufacturer]) => this._manufacturer = manufacturer));
+        promises.push(batch.queue(className, 'getModel').then(([model]) => this._model = model));
+        promises.push(batch.queue(className, 'getConnectionType').then(([connectionType]) => this._connectionType = connectionType));
+        promises.push(batch.queue(className, 'getNetworkType').then(([networkType]) => this._networkType = networkType));
+        promises.push(batch.queue(className, 'getScreenLayout').then(([screenLayout]) => this._screenLayout = screenLayout));
+        promises.push(batch.queue(className, 'getScreenDensity').then(([screenDensity]) => this._screenDensity = screenDensity));
+        promises.push(batch.queue(className, 'getScreenWidth').then(([screenWidth]) => this._screenWidth = screenWidth));
+        promises.push(batch.queue(className, 'getScreenHeight').then(([screenHeight]) => this._screenHeight = screenHeight));
+        promises.push(batch.queue(className, 'getNetworkOperatorName').then(([networkOperator]) => this._networkOperator = networkOperator));
+        promises.push(batch.queue(className, 'getHeadset').then(([headset]) => this._headset = headset));
+        promises.push(batch.queue(className, 'getRingerMode').then(([ringerMode]) => this._ringerMode = ringerMode));
+        promises.push(batch.queue(className, 'getSystemLanguage').then(([language]) => this._language = language));
+        promises.push(batch.queue(className, 'getDeviceVolume', [StreamType.STREAM_SYSTEM]).then(([volume]) => this._volume = volume));
+        promises.push(batch.queue(className, 'getScreenBrightness').then(([screenBrightness]) => this._screenBrightness = screenBrightness));
+        promises.push(batch.queue(className, 'getFreeSpace', [StorageType[StorageType.EXTERNAL]]).then(([freeExternalSpace]) => this._freeExternalSpace = freeExternalSpace).catch(([error]) => this._freeExternalSpace = undefined));
+        promises.push(batch.queue(className, 'getTotalSpace', [StorageType[StorageType.EXTERNAL]]).then(([totalExternalSpace]) => this._totalExternalSpace = totalExternalSpace).catch(([error]) => this._totalExternalSpace = undefined));
+        promises.push(batch.queue(className, 'getFreeSpace', [StorageType[StorageType.INTERNAL]]).then(([freeInternalSpace]) => this._freeInternalSpace = freeInternalSpace).catch(([error]) => this._freeInternalSpace = undefined));
+        promises.push(batch.queue(className, 'getTotalSpace', [StorageType[StorageType.INTERNAL]]).then(([totalInternalSpace]) => this._totalInternalSpace = totalInternalSpace).catch(([error]) => this._totalInternalSpace = undefined));
+        promises.push(batch.queue(className, 'getBatteryLevel').then(([batteryLevel]) => this._batteryLevel = batteryLevel));
+        promises.push(batch.queue(className, 'getBatteryStatus').then(([batteryStatus]) => this._batteryStatus = batteryStatus));
+        promises.push(batch.queue(className, 'getFreeMemory').then(([freeMemory]) => this._freeMemory = freeMemory));
+        promises.push(batch.queue(className, 'getTotalMemory').then(([totalMemory]) => this._totalMemory = totalMemory));
+        nativeBridge.invokeBatch(batch);
+        return Promise.all(promises);
     }
 
     public getAndroidId(): string {
