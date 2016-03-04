7818f763678de3fd5b894ce92074891a9a0cd380
Janne Sinivirta <janne.sinivirta@gmail.com>
2016-03-04 08:45:35 +0200

    tests for onVideoCompleted

--- test/EventHandlers/VideoAdEventHandlersTest.ts
+++ test/EventHandlers/VideoAdEventHandlersTest.ts
@@ -5,6 +5,7 @@ import { assert } from 'chai';
 import * as sinon from 'sinon';
 import { VideoAdEventHandlers } from '../../src/ts/EventHandlers/VideoAdEventHandlers';
 import { VideoAdUnit } from '../../src/ts/Models/VideoAdUnit';
+import {FinishState} from '../../src/ts/Models/AdUnit';
 
 describe('VideoAdEventHandlersTest', () => {
 
@@ -77,6 +78,91 @@ describe('VideoAdEventHandlersTest', () => {
             VideoAdEventHandlers.onVideoStart(<VideoAdUnit> <any> adUnitMock);
 
             assert.isOk(invoke.notCalled);
+        });
+
+    });
+
+    describe('with onVideoCompleted', () => {
+        let sendView;
+        let invoke;
+        let rawInvoke;
+        let adUnitMock;
+        let overlayHide;
+        let endscreenShow;
+        let storageManagerGet;
+        let prom;
+
+        beforeEach(() => {
+            prom = Promise.resolve(false);
+
+            sendView = sinon.spy();
+            invoke = sinon.spy();
+            rawInvoke = sinon.spy();
+            overlayHide = sinon.spy();
+            endscreenShow = sinon.spy();
+            storageManagerGet = sinon.mock().returns(prom);
+
+            adUnitMock = {
+                setVideoActive: sinon.spy(),
+                setFinishState: sinon.spy(),
+                getSessionManager: () => ({ sendView: sendView }),
+                getNativeBridge: () => ({ invoke: invoke, rawInvoke: rawInvoke }),
+                getOverlay: () => ({ hide: overlayHide }),
+                getEndScreen: () => ({ show: endscreenShow }),
+                getStorageManager: () => ({ get: storageManagerGet }),
+                getPlacement: () => ({ getId: () => 1 })
+            };
+        });
+
+        it('should set video to inactive', () => {
+            VideoAdEventHandlers.onVideoCompleted(<VideoAdUnit> <any> adUnitMock, 'foo_url');
+
+            assert.isOk(adUnitMock.setVideoActive.calledWith(false));
+        });
+
+        it('should set finnish state to COMPLETED', () => {
+            VideoAdEventHandlers.onVideoCompleted(<VideoAdUnit> <any> adUnitMock, 'foo_url');
+
+            assert.isOk(adUnitMock.setFinishState.calledWith(FinishState.COMPLETED));
+        });
+
+        it('should send view to session manager', () => {
+            VideoAdEventHandlers.onVideoCompleted(<VideoAdUnit> <any> adUnitMock, 'foo_url');
+
+            assert.isOk(sendView.calledWith(adUnitMock));
+        });
+
+        it('should hide overlay', () => {
+            VideoAdEventHandlers.onVideoCompleted(<VideoAdUnit> <any> adUnitMock, 'foo_url');
+
+            assert.isOk(overlayHide.called);
+        });
+
+        it('should show endscreen', () => {
+            VideoAdEventHandlers.onVideoCompleted(<VideoAdUnit> <any> adUnitMock, 'foo_url');
+
+            assert.isOk(endscreenShow.called);
+        });
+
+        it('should not call rawinvoke, if integration testing is disabled', (done) => {
+            VideoAdEventHandlers.onVideoCompleted(<VideoAdUnit> <any> adUnitMock, 'foo_url');
+
+            prom.then(() => {
+                assert.isOk(rawInvoke.notCalled);
+                done();
+            });
+        });
+
+        it('should call rawinvoke, if integration testing is enabled', (done) => {
+            prom = Promise.resolve(true);
+            storageManagerGet = sinon.mock().returns(prom);
+
+            VideoAdEventHandlers.onVideoCompleted(<VideoAdUnit> <any> adUnitMock, 'foo_url');
+
+            prom.then(() => {
+                assert.isOk(rawInvoke.calledWith('com.unity3d.ads.test.integration', 'IntegrationTest', 'onVideoCompleted', [1]));
+                done();
+            });
         });
 
     });
