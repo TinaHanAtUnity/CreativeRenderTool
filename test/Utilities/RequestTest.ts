146caeac5469fa52fe2894497ed578e6eb295cbf
Janne Nikula <jannen@unity3d.com>
2016-03-07 16:01:48 +0200

    Refactor request tests to stop using fake API

--- test/Utilities/RequestTest.ts
+++ test/Utilities/RequestTest.ts
@@ -1,24 +1,86 @@
 import { Request } from '../../src/ts/Utilities/Request';
 import { WebViewBridge } from '../WebViewBridge';
 import { NativeBridge } from '../../src/ts/NativeBridge';
+import { TestBridge, TestBridgeApi } from '../TestBridge';
 
 import 'mocha';
 import { assert } from 'chai';
+
+class Url extends TestBridgeApi {
+    private _retryCount: number = 0;
+
+    public get(id: string, url: string, headers: [string, string][]): any[] {
+        if(url.indexOf('/success') !== -1) {
+            this.sendSuccessResponse(id, url, 'Success response', []);
+        } else if(url.indexOf('/fail') !== -1) {
+            this.sendFailResponse(id, url, 'Fail response');
+        } else if(url.indexOf('/forwardheader') !== -1) {
+            if(headers[0][0] === 'X-Test') {
+                this.sendSuccessResponse(id, url, headers[0][1], []);
+            } else {
+                this.sendFailResponse(id, url, 'No X-Test header found');
+            }
+        } else if(url.indexOf('/retry')) {
+            if(this._retryCount === 3) {
+                this.sendSuccessResponse(id, url, 'Success response', []);
+            } else {
+                this.sendFailResponse(id, url, 'Must continue retrying');
+            }
+
+            this._retryCount++;
+        }
+
+        return ['OK'];
+    }
+
+    public post(id: string, url: string, body: string, headers: [string, string][]): any[] {
+        if(url.indexOf('/success') !== -1) {
+            this.sendSuccessResponse(id, url, 'Success response', []);
+        } else if(url.indexOf('/fail') !== -1) {
+            this.sendFailResponse(id, url, 'Fail response');
+        } else if(url.indexOf('/forwardheader') !== -1) {
+            if(headers[0][0] === 'X-Test') {
+                this.sendSuccessResponse(id, url, headers[0][1], []);
+            } else {
+                this.sendFailResponse(id, url, 'No X-Test header found');
+            }
+        } else if(url.indexOf('/forwardbody') !== -1) {
+            this.sendSuccessResponse(id, url, body, []);
+        } else if(url.indexOf('/retry')) {
+            if(this._retryCount === 3) {
+                this.sendSuccessResponse(id, url, 'Success response', []);
+            } else {
+                this.sendFailResponse(id, url, 'Must continue retrying');
+            }
+
+            this._retryCount++;
+        }
+
+        return ['OK'];
+    }
+
+    private sendSuccessResponse(id: string, url: string, body: string, headers: [string, string][]) {
+        setTimeout(() => { this.getNativeBridge().handleEvent(['URL_COMPLETE', id, url, body, 200, headers]); }, 0);
+    }
+
+    private sendFailResponse(id: string, url: string, message: string) {
+        setTimeout(() => { this.getNativeBridge().handleEvent(['URL_FAILED', id, url, message]); }, 0);
+    }
+}
 
 describe('RequestTest', () => {
     it('Request get without headers (expect success)', function(done: MochaDone): void {
         let successUrl: string = 'http://www.example.org/success';
         let successMessage: string = 'Success response';
 
-        let testBridge: WebViewBridge = new WebViewBridge();
-        let nativeBridge: NativeBridge = new NativeBridge(testBridge);
-        testBridge.setNativeBridge(nativeBridge);
-        let request: Request = new Request(nativeBridge);
+        let testBridge: TestBridge = new TestBridge();
+        testBridge.setApi('Url', new Url());
+        let request: Request = new Request(testBridge.getNativeBridge());
 
         request.get(successUrl).then(([response]) => {
             assert.equal(successMessage, response, 'Did not receive correct response');
             done();
-        }).catch(([error]) => {
+        }).catch((error) => {
             done(new Error('Get without headers failed: ' + error));
         });
     });
@@ -27,14 +89,13 @@ describe('RequestTest', () => {
         let failUrl: string = 'http://www.example.org/fail';
         let failMessage: string = 'Fail response';
 
-        let testBridge: WebViewBridge = new WebViewBridge();
-        let nativeBridge: NativeBridge = new NativeBridge(testBridge);
-        testBridge.setNativeBridge(nativeBridge);
-        let request: Request = new Request(nativeBridge);
+        let testBridge: TestBridge = new TestBridge();
+        testBridge.setApi('Url', new Url());
+        let request: Request = new Request(testBridge.getNativeBridge());
 
         request.get(failUrl).then(([response]) => {
             done(new Error('Request should have failed but got response: ' + response));
-        }).catch(([error]) => {
+        }).catch((error) => {
             assert.equal(failMessage, error, 'Did not receive correct error message');
             done();
         });
@@ -45,16 +106,33 @@ describe('RequestTest', () => {
         let headerField: string = 'X-Test';
         let headerMessage: string = 'Header message';
 
-        let testBridge: WebViewBridge = new WebViewBridge();
-        let nativeBridge: NativeBridge = new NativeBridge(testBridge);
-        testBridge.setNativeBridge(nativeBridge);
-        let request: Request = new Request(nativeBridge);
+        let testBridge: TestBridge = new TestBridge();
+        testBridge.setApi('Url', new Url());
+        let request: Request = new Request(testBridge.getNativeBridge());
 
         request.get(headerUrl, [[headerField, headerMessage]]).then(([response]) => {
             assert.equal(headerMessage, response, 'Did not get correctly forwarded header response');
             done();
-        }).catch(([error]) => {
+        }).catch((error) => {
             done(new Error('Get with header forwarding failed: ' + error));
+        });
+    });
+
+    it('Request get with three retries', function(done: MochaDone): void {
+        let retryUrl: string = 'http://www.example.org/retry';
+        let successMessage: string = 'Success response';
+        let retryAttempts: number = 3;
+        let retryDelay: number = 10;
+
+        let testBridge: TestBridge = new TestBridge();
+        testBridge.setApi('Url', new Url());
+        let request: Request = new Request(testBridge.getNativeBridge());
+
+        request.get(retryUrl, [], retryAttempts, retryDelay).then(([response]) => {
+            assert.equal(successMessage, response, 'Did not get success message when retrying');
+            done();
+        }).catch((error) => {
+            done(new Error('Get with retrying failed: ' + error));
         });
     });
 
@@ -62,15 +140,14 @@ describe('RequestTest', () => {
         let successUrl: string = 'http://www.example.org/success';
         let successMessage: string = 'Success response';
 
-        let testBridge: WebViewBridge = new WebViewBridge();
-        let nativeBridge: NativeBridge = new NativeBridge(testBridge);
-        testBridge.setNativeBridge(nativeBridge);
-        let request: Request = new Request(nativeBridge);
+        let testBridge: TestBridge = new TestBridge();
+        testBridge.setApi('Url', new Url());
+        let request: Request = new Request(testBridge.getNativeBridge());
 
         request.post(successUrl, 'Test').then(([response]) => {
             assert.equal(successMessage, response, 'Did not receive correct response');
             done();
-        }).catch(([error]) => {
+        }).catch((error) => {
             done(new Error('Post without headers failed: ' + error));
         });
     });
@@ -79,14 +156,13 @@ describe('RequestTest', () => {
         let failUrl: string = 'http://www.example.org/fail';
         let failMessage: string = 'Fail response';
 
-        let testBridge: WebViewBridge = new WebViewBridge();
-        let nativeBridge: NativeBridge = new NativeBridge(testBridge);
-        testBridge.setNativeBridge(nativeBridge);
-        let request: Request = new Request(nativeBridge);
+        let testBridge: TestBridge = new TestBridge();
+        testBridge.setApi('Url', new Url());
+        let request: Request = new Request(testBridge.getNativeBridge());
 
         request.post(failUrl, 'Test').then(([response]) => {
             done(new Error('Request should have failed but got response: ' + response));
-        }).catch(([error]) => {
+        }).catch((error) => {
             assert.equal(failMessage, error, 'Did not receive correct error message');
             done();
         });
@@ -97,15 +173,14 @@ describe('RequestTest', () => {
         let headerField: string = 'X-Test';
         let headerMessage: string = 'Header message';
 
-        let testBridge: WebViewBridge = new WebViewBridge();
-        let nativeBridge: NativeBridge = new NativeBridge(testBridge);
-        testBridge.setNativeBridge(nativeBridge);
-        let request: Request = new Request(nativeBridge);
+        let testBridge: TestBridge = new TestBridge();
+        testBridge.setApi('Url', new Url());
+        let request: Request = new Request(testBridge.getNativeBridge());
 
         request.post(headerUrl, 'Test', [[headerField, headerMessage]]).then(([response]) => {
             assert.equal(headerMessage, response, 'Did not get correctly forwarded header response');
             done();
-        }).catch(([error]) => {
+        }).catch((error) => {
             done(new Error('Post with header forwarding failed: ' + error));
         });
     });
@@ -114,16 +189,33 @@ describe('RequestTest', () => {
         let testUrl: string = 'http://www.example.org/forwardbody';
         let bodyMessage: string = 'Body message';
 
-        let testBridge: WebViewBridge = new WebViewBridge();
-        let nativeBridge: NativeBridge = new NativeBridge(testBridge);
-        testBridge.setNativeBridge(nativeBridge);
-        let request: Request = new Request(nativeBridge);
+        let testBridge: TestBridge = new TestBridge();
+        testBridge.setApi('Url', new Url());
+        let request: Request = new Request(testBridge.getNativeBridge());
 
         request.post(testUrl, bodyMessage).then(([response]) => {
             assert.equal(bodyMessage, response, 'Did not get correctly forwarded body');
             done();
-        }).catch(([error]) => {
+        }).catch((error) => {
             done(new Error('Post with body forwarding failed: ' + error));
+        });
+    });
+
+    it('Request post with three retries', function(done: MochaDone): void {
+        let retryUrl: string = 'http://www.example.org/retry';
+        let successMessage: string = 'Success response';
+        let retryAttempts: number = 3;
+        let retryDelay: number = 10;
+
+        let testBridge: TestBridge = new TestBridge();
+        testBridge.setApi('Url', new Url());
+        let request: Request = new Request(testBridge.getNativeBridge());
+
+        request.post(retryUrl, 'Test', [], retryAttempts, retryDelay).then(([response]) => {
+            assert.equal(successMessage, response, 'Did not get success message when retrying');
+            done();
+        }).catch((error) => {
+            done(new Error('Post with retrying failed: ' + error));
         });
     });
 });
