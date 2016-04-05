4c2d892ebdfbda96a1779b741174aaa8b4a07226
Janne Sinivirta <janne.sinivirta@gmail.com>
2016-04-05 08:24:19 +0300

    use a real Observable to check rest of the functionality in ConnectivityManager

--- test/Managers/ConnectivityManagerTest.ts
+++ test/Managers/ConnectivityManagerTest.ts
@@ -2,16 +2,17 @@ import 'mocha';
 import * as sinon from 'sinon';
 
 import { ConnectivityManager } from '../../src/ts/Managers/ConnectivityManager';
+import { Observable } from '../../src/ts/Utilities/Observable';
 
 describe('ConnectivityManager', () => {
     let connectivityManager: ConnectivityManager;
     let nativeBridgeMock;
 
     before(() => {
-        nativeBridgeMock = {
-            subscribe: sinon.spy(),
-            invoke: sinon.spy(),
-        };
+        nativeBridgeMock = new Observable();
+
+        sinon.spy(nativeBridgeMock, 'subscribe');
+        nativeBridgeMock.invoke = sinon.spy();
 
         connectivityManager = new ConnectivityManager(nativeBridgeMock);
     });
@@ -29,6 +30,21 @@ describe('ConnectivityManager', () => {
         it('should call invoke DeviceInfo on native bridge', () => {
             sinon.assert.calledWith(nativeBridgeMock.invoke, 'DeviceInfo', 'setConnectionMonitoring', [true]);
         });
+    });
 
+    describe('when nativeBridge receives CONNECTIVITY_CONNECTED', () => {
+
+        let connectionListenerMock;
+
+        before(() => {
+            connectionListenerMock = sinon.spy();
+            connectivityManager.subscribe('connected', connectionListenerMock);
+
+            nativeBridgeMock.trigger('CONNECTIVITY_CONNECTED', 'wififoo', 'networkfoo');
+        });
+
+        it('should invoke connected method on subscriber', () => {
+            sinon.assert.calledWith(connectionListenerMock, 'wififoo', 'networkfoo');
+        });
     });
 });
