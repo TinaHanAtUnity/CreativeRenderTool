3dd727eb0b0782b84f7275e27051fb34c1c5d71b
Janne Nikula <jannen@unity3d.com>
2016-04-29 11:42:40 +0300

    Refactor broadcast api according to pr comments

--- src/ts/Native/Api/Broadcast.ts
+++ src/ts/Native/Api/Broadcast.ts
@@ -1,5 +1,5 @@
 import { NativeApi } from 'Native/NativeApi';
-import { Observable0 } from 'Utilities/Observable';
+import { Observable4 } from 'Utilities/Observable';
 import { NativeBridge } from 'Native/NativeBridge';
 
 enum BroadcastEvent {
@@ -7,40 +7,31 @@ enum BroadcastEvent {
 }
 
 export class BroadcastApi extends NativeApi {
-    public onScreenOn: Observable0 = new Observable0();
-    public onScreenOff: Observable0 = new Observable0();
-
-    private _screenListenerName = 'screenListener';
-
-    private ACTION_SCREEN_ON: string = 'android.intent.action.SCREEN_ON';
-    private ACTION_SCREEN_OFF: string = 'android.intent.action.SCREEN_OFF';
+    public onBroadcastAction: Observable4<string, string, string, any> = new Observable4();
 
     constructor(nativeBridge: NativeBridge) {
         super(nativeBridge, 'Broadcast');
     }
 
-    public setListenScreenStatus(status: boolean): Promise<void> {
-        if(status) {
-            return this._nativeBridge.invoke<void>(this._apiClass, 'addBroadcastListener', [this._screenListenerName, [this.ACTION_SCREEN_ON, this.ACTION_SCREEN_OFF]]);
-        } else {
-            return this._nativeBridge.invoke<void>(this._apiClass, 'removeBroadcastListener', [this._screenListenerName]);
-        }
+    public addBroadcastListener(listenerName: string, actions: string[]): Promise<void> {
+        return this._nativeBridge.invoke<void>(this._apiClass, 'addBroadcastListener', [name, actions]);
+    }
+
+    public addDataSchemeBroadcastListener(listenerName: string, dataScheme: string, actions: string[]): Promise<void> {
+        return this._nativeBridge.invoke<void>(this._apiClass, 'addBroadcastListener', [name, dataScheme, actions]);
+    }
+
+    public removeBroadcastListener(listenerName: string): Promise<void> {
+        return this._nativeBridge.invoke<void>(this._apiClass, 'removeBroadcastListener', [name]);
+    }
+
+    public removeAllBroadcastListeners(): Promise<void> {
+        return this._nativeBridge.invoke<void>(this._apiClass, 'removeAllBroadcastListeners', []);
     }
 
     public handleEvent(event: string, parameters: any[]): void {
-        if(event === BroadcastEvent[BroadcastEvent.ACTION] && parameters[0] === this._screenListenerName) {
-            switch (parameters[1]) {
-                case this.ACTION_SCREEN_ON:
-                    this.onScreenOn.trigger();
-                    break;
-
-                case this.ACTION_SCREEN_OFF:
-                    this.onScreenOff.trigger();
-                    break;
-
-                default:
-                    super.handleEvent(event, parameters);
-            }
+        if(event === BroadcastEvent[BroadcastEvent.ACTION]) {
+            this.onBroadcastAction.trigger(parameters[0], parameters[1], parameters[2], parameters[3]);
         } else {
             super.handleEvent(event, parameters);
         }
--- test/Native/Api/BroadcastApiTest.ts
+++ test/Native/Api/BroadcastApiTest.ts
@@ -16,35 +16,14 @@ describe('BroadcastApi', () => {
         }, false);
     });
 
-    it('should invoke addBroadcastListener', () => {
-        nativeBridge.Broadcast.setListenScreenStatus(true);
-        sinon.assert.calledWith(handleInvocation, JSON.stringify([['com.unity3d.ads.api.Broadcast', 'addBroadcastListener', ['screenListener', ['android.intent.action.SCREEN_ON', 'android.intent.action.SCREEN_OFF']], '1']]));
-    });
-
-    it('should invoke removeBroadcastListener', () => {
-        nativeBridge.Broadcast.setListenScreenStatus(false);
-        sinon.assert.calledWith(handleInvocation, JSON.stringify([['com.unity3d.ads.api.Broadcast', 'removeBroadcastListener', ['screenListener'], '1']]));
-    });
-
-    it('should trigger onScreenOn', () => {
+    it('should handle SCREEN_ON event', () => {
         let spy = sinon.spy();
-        nativeBridge.Broadcast.onScreenOn.subscribe(spy);
+        nativeBridge.Broadcast.onBroadcastAction.subscribe(spy);
         nativeBridge.Broadcast.handleEvent('ACTION', ['screenListener', 'android.intent.action.SCREEN_ON', '', {}]);
-        sinon.assert.calledOnce(spy);
-    });
-
-    it('should trigger onScreenOff', () => {
-        let spy = sinon.spy();
-        nativeBridge.Broadcast.onScreenOff.subscribe(spy);
-        nativeBridge.Broadcast.handleEvent('ACTION', ['screenListener', 'android.intent.action.SCREEN_OFF', '', {}]);
-        sinon.assert.calledOnce(spy);
+        sinon.assert.calledWith(spy, 'screenListener', 'android.intent.action.SCREEN_ON', '', {});
     });
 
     it('should throw on invalid events', () => {
-        assert.throws(() => {
-            nativeBridge.Broadcast.handleEvent('ACTION', ['screenListener', 'android.intent.action.UNKNOWN_ACTION', '', {}]);
-        });
-
         assert.throws(() => {
             nativeBridge.Broadcast.handleEvent('INVALID', []);
         });
