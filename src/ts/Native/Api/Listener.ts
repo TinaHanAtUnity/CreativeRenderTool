38f90d1b1f07aa315775aa47f84e531947146c47
Janne Sinivirta <janne.sinivirta@gmail.com>
2016-05-09 12:37:32 +0300

    fix implicit any in Listener

--- src/ts/Native/Api/Listener.ts
+++ src/ts/Native/Api/Listener.ts
@@ -16,7 +16,7 @@ export class ListenerApi extends NativeApi {
         return this._nativeBridge.invoke<void>(this._apiClass, 'sendStartEvent', [placementId]);
     }
 
-    public sendFinishEvent(placementId, result: FinishState): Promise<void> {
+    public sendFinishEvent(placementId: string, result: FinishState): Promise<void> {
         return this._nativeBridge.invoke<void>(this._apiClass, 'sendFinishEvent', [placementId, FinishState[result]]);
     }
 
@@ -24,7 +24,7 @@ export class ListenerApi extends NativeApi {
         return this._nativeBridge.invoke<void>(this._apiClass, 'sendErrorEvent', [error, message]);
     }
 
-    public sendClickEvent(placementId): Promise<void> {
+    public sendClickEvent(placementId: string): Promise<void> {
         return this._nativeBridge.invoke<void>(this._apiClass, 'sendClickEvent', [placementId]);
     }
 
