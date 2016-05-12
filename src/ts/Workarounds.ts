e1ec9acd3ae4291f6eb4ac327aafb24380cf22fe
Janne Sinivirta <janne.sinivirta@gmail.com>
2016-05-12 08:08:50 +0300

    fix implicit types in Workarounds except one

--- src/ts/Workarounds.ts
+++ src/ts/Workarounds.ts
@@ -44,32 +44,33 @@ if(!('classList' in document.documentElement) && Object.defineProperty && typeof
     Object.defineProperty(HTMLElement.prototype, 'classList', {
         get: function() {
             let self = this;
-            function update(fn) {
-                return function(value) {
-                    let classes = self.className.split(/\s+/), index = classes.indexOf(value);
+            function update(fn: Function) {
+                return function(value: string) {
+                    let classes = self.className.split(/\s+/);
+                    let index = classes.indexOf(value);
                     fn(classes, index, value);
                     self.className = classes.join(' ');
                 };
             }
 
             let ret = {
-                add: update(function(classes, index, value) {
+                add: update(function(classes: string[], index: number, value: string) {
                     ~index || classes.push(value);
                 }),
 
-                remove: update(function(classes, index) {
+                remove: update(function(classes: string[], index: number) {
                     ~index && classes.splice(index, 1);
                 }),
 
-                toggle: update(function(classes, index, value) {
+                toggle: update(function(classes: string[], index: number, value: string) {
                     ~index ? classes.splice(index, 1) : classes.push(value);
                 }),
 
-                contains: function(value) {
+                contains: function(value: string) {
                     return !!~self.className.split(/\s+/).indexOf(value);
                 },
 
-                item: function(i) {
+                item: function(i: number) {
                     return self.className.split(/\s+/)[i] || null;
                 }
             };
