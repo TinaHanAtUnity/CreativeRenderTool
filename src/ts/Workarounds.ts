42ef4f3b0bf19e20d1dd9f224d3a9b776f64f158
Janne Sinivirta <janne.sinivirta@gmail.com>
2016-05-12 12:35:10 +0300

    remove bind polyfill

--- src/ts/Workarounds.ts
+++ src/ts/Workarounds.ts
@@ -1,32 +1,4 @@
-/* tslint:disable:no-bitwise no-unused-expression no-empty */
-
-if(!Function.prototype.bind) {
-    Function.prototype.bind = function(oThis) {
-        if (typeof this !== 'function') {
-            // closest thing possible to the ECMAScript 5
-            // internal IsCallable function
-            throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
-        }
-
-        let aArgs   = Array.prototype.slice.call(arguments, 1),
-            fToBind = this,
-            fNOP    = function() {},
-            fBound  = function() {
-                return fToBind.apply(this instanceof fNOP
-                        ? this
-                        : oThis,
-                    aArgs.concat(Array.prototype.slice.call(arguments)));
-            };
-
-        if (this.prototype) {
-            // native functions don't have a prototype
-            fNOP.prototype = this.prototype;
-        }
-        fBound.prototype = new fNOP();
-
-        return fBound;
-    };
-}
+/* tslint:disable:no-bitwise no-unused-expression */
 
 if(!Array.prototype.forEach) {
     Array.prototype.forEach = function(callback, thisArg) {
