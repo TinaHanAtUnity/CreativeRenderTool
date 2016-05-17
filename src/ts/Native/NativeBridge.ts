3b517e0ec1cabe61557c7ac44344578d327ffc3f
Janne Sinivirta <janne.sinivirta@gmail.com>
2016-05-17 13:51:17 +0300

    typings.json fixed to use imports the way that typings 1.0 wants them. references to main.d.ts switched to index.d.ts

--- src/ts/Native/NativeBridge.ts
+++ src/ts/Native/NativeBridge.ts
@@ -1,4 +1,4 @@
-/// <reference path="../../../typings/main.d.ts" />
+/// <reference path="../../../typings/index.d.ts" />
 /// <reference path="WebViewBridge.d.ts" />
 
 import { INativeBridge } from 'Native/INativeBridge';
