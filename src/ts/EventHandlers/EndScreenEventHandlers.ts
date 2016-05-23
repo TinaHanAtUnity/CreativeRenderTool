1320ad9b966bd027c4b8937ea176e82917ff0ceb
Ville Orkas <villeo@unity3d.com>
2016-05-23 17:53:26 +0300

    Add reprepare handling

--- src/ts/EventHandlers/EndScreenEventHandlers.ts
+++ src/ts/EventHandlers/EndScreenEventHandlers.ts
@@ -5,6 +5,7 @@ import { Request } from 'Utilities/Request';
 import { SessionManager } from 'Managers/SessionManager';
 import { Platform } from 'Constants/Platform';
 import { Campaign } from 'Models/Campaign';
+import { IAppSheetOptions } from 'Native/Api/AppSheet';
 
 export class EndScreenEventHandlers {
 
@@ -49,13 +50,15 @@ export class EndScreenEventHandlers {
         } else {
             sessionManager.sendClick(adUnit);
             if(platform === Platform.IOS) {
-                if(!campaign.getBypassAppSheet()) {
-                    nativeBridge.AppSheet.present({ // todo: needs check to prepare appsheet after presenting on ios 8+
-                        id: parseInt(campaign.getAppStoreId(), 10)
-                    });
-                } else {
-                    nativeBridge.UrlScheme.open(EndScreenEventHandlers.getAppStoreUrl(platform, campaign));
-                }
+                nativeBridge.AppSheet.canOpen().then(canOpenAppSheet => {
+                    if(canOpenAppSheet && !campaign.getBypassAppSheet()) {
+                        EndScreenEventHandlers.openAppSheet(nativeBridge, {
+                            id: parseInt(campaign.getAppStoreId(), 10)
+                        });
+                    } else {
+                        nativeBridge.UrlScheme.open(EndScreenEventHandlers.getAppStoreUrl(platform, campaign));
+                    }
+                });
             } else {
                 nativeBridge.Intent.launch({
                     'action': 'android.intent.action.VIEW',
@@ -65,12 +68,39 @@ export class EndScreenEventHandlers {
         }
     }
 
+    public static onClose(nativeBridge: NativeBridge, adUnit: VideoAdUnit): void {
+        if(nativeBridge.getPlatform() === Platform.IOS && !adUnit.getCampaign().getBypassAppSheet()) {
+            nativeBridge.AppSheet.destroy({
+                id: parseInt(adUnit.getCampaign().getAppStoreId(), 10)
+            });
+        }
+        adUnit.hide();
+    }
+
     private static getAppStoreUrl(platform: Platform, campaign: Campaign) {
         if(platform === Platform.IOS) {
             return 'https://itunes.apple.com/' + campaign.getAppStoreCountry() + '/app/id' + campaign.getAppStoreId();
         } else {
             return 'market://details?id=' + campaign.getAppStoreId();
         }
+    }
+
+    private static openAppSheet(nativeBridge: NativeBridge, options: IAppSheetOptions) {
+        nativeBridge.AppSheet.present(options).then(() => {
+            return nativeBridge.AppSheet.destroy(options);
+        }).catch(([error, options]) => {
+            if(error === 'APPSHEET_NOT_FOUND') {
+                return nativeBridge.AppSheet.prepare(options).then(() => {
+                    let preparedObserver = nativeBridge.AppSheet.onPrepared.subscribe(() => {
+                        nativeBridge.AppSheet.present(options).then(() => {
+                            nativeBridge.AppSheet.destroy(options);
+                        });
+                        nativeBridge.AppSheet.onPrepared.unsubscribe(preparedObserver);
+                    });
+                });
+            }
+            throw [error, options];
+        });
     }
 
 }
