cafa78d33779202e551a185a287d97545665f15f
Ville Orkas <villeo@unity3d.com>
2016-05-24 18:52:48 +0300

    Remove replay functionality

--- src/ts/Views/EndScreen.ts
+++ src/ts/Views/EndScreen.ts
@@ -9,7 +9,6 @@ import { Campaign } from 'Models/Campaign';
 export class EndScreen extends View {
 
     public onDownload: Observable0 = new Observable0();
-    public onReplay: Observable0 = new Observable0();
     public onClose: Observable0 = new Observable0();
 
     constructor(campaign: Campaign) {
@@ -35,11 +34,6 @@ export class EndScreen extends View {
             },
             {
                 event: 'click',
-                listener: (event) => this.onReplayEvent(event),
-                selector: '.btn-watch-again-region'
-            },
-            {
-                event: 'click',
                 listener: (event) => this.onCloseEvent(event),
                 selector: '.btn-close-region'
             }
@@ -49,11 +43,6 @@ export class EndScreen extends View {
     private onDownloadEvent(event: Event): void {
         event.preventDefault();
         this.onDownload.trigger();
-    }
-
-    private onReplayEvent(event: Event): void {
-        event.preventDefault();
-        this.onReplay.trigger();
     }
 
     private onCloseEvent(event: Event): void {
