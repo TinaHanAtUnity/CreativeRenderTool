b85034f8f849e0fc20e7092798ecee1c02a2c5c0
Janne Sinivirta <janne.sinivirta@gmail.com>
2016-08-23 14:22:45 +0300

    add tests for DiagnosticError serialization

--- test/Errors/DiagnosticErrorTest.ts
+++ test/Errors/DiagnosticErrorTest.ts
@@ -2,11 +2,10 @@
 
 import 'mocha';
 import { assert } from 'chai';
-
 import { DiagnosticError } from '../../src/ts/Errors/DiagnosticError';
 
 describe('DiagnosticErrorTest', () => {
-    it('with existing error', () => {
+    describe('with existing error', () => {
         let originalError;
 
         before(() => {
@@ -18,14 +17,24 @@ describe('DiagnosticErrorTest', () => {
 
             assert.equal(diagnosticError.name, 'SyntaxError');
             assert.equal(diagnosticError.message, 'foo message');
+            assert.equal(diagnosticError.stack, originalError.stack);
         });
 
         it('should contain the diagnostic data', () => {
-            let diagnosticError = new DiagnosticError(originalError, { foo: 'foo1', bar: 'bar2'});
+            let diagnosticError = new DiagnosticError(originalError, {foo: 'foo1', bar: 'bar2'});
 
             /* tslint:disable:no-string-literal */
             assert.equal(diagnosticError.diagnostic['foo'], 'foo1');
             assert.equal(diagnosticError.diagnostic['bar'], 'bar2');
+        });
+
+        it('should serialize correctly', () => {
+            let diagnosticError = new DiagnosticError(originalError, {foo: 'foo1', bar: 'bar2'});
+            let result = JSON.stringify(diagnosticError);
+
+            assert.notEqual(result.indexOf('"name":"SyntaxError"'), -1);
+            assert.notEqual(result.indexOf('"message":"foo message"'), -1);
+            assert.notEqual(result.indexOf('"diagnostic":{"foo":"foo1","bar":"bar2"}'), -1);
         });
     });
 });
