d2369770793bf9f170f84bcdc9f6b3a415d9fa89
Janne Sinivirta <janne.sinivirta@gmail.com>
2016-08-23 14:25:31 +0300

    switch JsonParser to use DiagnosticError instead of JsonParseError

--- src/ts/Utilities/JsonParser.ts
+++ src/ts/Utilities/JsonParser.ts
@@ -1,17 +1,12 @@
-export class JsonSyntaxError extends SyntaxError {
-    public failingContent: string;
-}
 
+import { DiagnosticError } from '../Errors/DiagnosticError';
 export class JsonParser {
 
     public static parse(text: string, reviver?: (key: any, value: any) => any): any {
         try {
             return JSON.parse(text, reviver);
         } catch(e) {
-            let error = <JsonSyntaxError>e;
-            error.failingContent = text;
-            error.name = 'JsonSyntaxError';
-            throw error;
+            throw new DiagnosticError(e, { json: text });
         }
 
     }
