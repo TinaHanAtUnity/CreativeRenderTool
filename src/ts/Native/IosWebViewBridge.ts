283892d65c2387eb6406dac9253157b69666471a
Ville Orkas <villeo@unity3d.com>
2016-08-31 12:11:58 +0300

    Initial WKWebView prototype

--- src/ts/Native/IosWebViewBridge.ts
+++ src/ts/Native/IosWebViewBridge.ts
@@ -1,17 +1,11 @@
 export class IosWebViewBridge implements IWebViewBridge {
 
-    private static _nativeUrl = 'https://webviewbridge.unityads.unity3d.com';
-
     public handleInvocation(invocations: string): void {
-        let xhr = new XMLHttpRequest();
-        xhr.open('POST', IosWebViewBridge._nativeUrl + '/handleInvocation', false);
-        xhr.send(invocations);
+        window.webkit.messageHandlers.handleInvocation.postMessage(invocations);
     }
 
     public handleCallback(id: string, status: string, parameters?: string) {
-        let xhr = new XMLHttpRequest();
-        xhr.open('POST', IosWebViewBridge._nativeUrl + '/handleCallback', false);
-        xhr.send('{"id":"' + id + '","status":"' + status + '","parameters":' + parameters + '}');
+        window.webkit.messageHandlers.handleCallback.postMessage({id: id, status: status, parameters: parameters});
     }
 
 }
--- src/ts/Native/WebViewBridge.d.ts
+++ src/ts/Native/WebViewBridge.d.ts
@@ -3,7 +3,21 @@ interface IWebViewBridge {
     handleCallback(id: string, status: string, parameters?: string): void;
 }
 
+interface IMessageHandler {
+    postMessage: Function;
+}
+
+interface IMessageHandlers {
+    handleInvocation: IMessageHandler;
+    handleCallback: IMessageHandler;
+}
+
+interface IWebKit {
+    messageHandlers: IMessageHandlers;
+}
+
 /* tslint:disable:interface-name */
 interface Window {
     webviewbridge: IWebViewBridge;
+    webkit: IWebKit;
 }
