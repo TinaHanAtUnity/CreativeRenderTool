1b182341a67adb2f47cf12276b718ec7bbb70e1b
Janne Nikula <jannen@unity3d.com>
2016-10-06 17:33:49 +0300

    Mock CampaignManager

--- test/Params/ParamsTest.ts
+++ test/Params/ParamsTest.ts
@@ -1,28 +1,49 @@
-/*
 import 'mocha';
 import * as sinon from 'sinon';
-import { assert } from 'chai';
+// import { assert } from 'chai';
 
 import { NativeBridge } from 'Native/NativeBridge';
-import { DeviceInfo } from 'Models/DeviceInfo';
 import { Request } from 'Utilities/Request';
 import { TestFixtures } from '../TestHelpers/TestFixtures';
 import { CampaignManager } from 'Managers/CampaignManager';
-import { VastParser } from 'Utilities/VastParser';
 import { Platform } from 'Constants/Platform';
+import { WakeUpManager } from 'Managers/WakeUpManager';
+import { StorageType, StorageApi } from 'Native/Api/Storage';
+import { RequestApi } from 'Native/Api/Request';
+
+class TestStorageApi extends StorageApi {
+    public get<T>(storageType: StorageType, key: string): Promise<T> {
+        return Promise.reject(['COULDNT_GET_VALUE', key]);
+    }
+
+    public getKeys(type: StorageType, key: string, recursive: boolean): Promise<string[]> {
+        return Promise.resolve([]);
+    }
+}
+
+class TestRequestApi extends RequestApi {
+    public post(id: string, url: string, requestBody: string, headers: [string, string][], connectTimeout: number, readTimeout: number): Promise<string> {
+        setTimeout(() => {
+            this.onComplete.trigger(id, url, '{}', 200, []);
+        }, 1);
+        return Promise.resolve(id);
+    }
+}
 
 describe('Parameter specification test', () => {
-    let deviceInfo: DeviceInfo;
     let nativeBridge: NativeBridge;
     let request: Request;
 
     it('Ad request', () => {
         nativeBridge = TestFixtures.getNativeBridge();
-        request = sinon.spy();
-        let campaignManager: CampaignManager = new CampaignManager(nativeBridge, request, TestFixtures.getClientInfo(), TestFixtures.getDeviceInfo(Platform.ANDROID), new VastParser());
-        campaignManager.request();
-
-        console.log(request.getCall(0).args[0]);
+        nativeBridge.Storage = new TestStorageApi(nativeBridge);
+        nativeBridge.Request = new TestRequestApi(nativeBridge);
+        request = new Request(nativeBridge, new WakeUpManager(nativeBridge));
+        let requestSpy = sinon.spy(request, 'post');
+        let campaignManager: CampaignManager = new CampaignManager(nativeBridge, request, TestFixtures.getClientInfo(), TestFixtures.getDeviceInfo(Platform.ANDROID), TestFixtures.getVastParser());
+        return campaignManager.request().then(() => {
+            // todo: assert arguments
+            console.log('args ' + requestSpy.getCall(0).args[0]);
+        });
     });
 });
-    */
