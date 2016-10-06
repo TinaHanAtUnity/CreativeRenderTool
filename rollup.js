027acdf9b253428ae87f7a62de698da88c382451
Ville Orkas <villeo@unity3d.com>
2016-10-06 21:07:27 +0300

    Fix one thing, break another

--- rollup.js
+++ rollup.js
@@ -1,16 +1,20 @@
 import includePaths from 'rollup-plugin-includepaths';
 import string from 'rollup-plugin-string';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
 
 export default {
     entry: 'build/release/js/Main.js',
     format: 'iife',
     dest: 'build/release/bundle.js',
     plugins: [
+        replace({
+            'text!': ''
+        }),
         includePaths({
             paths: [
                 'build/release/js',
-                'build/release/html'
+                'build/release'
             ],
             extensions: ['.js', '.html']
         }),
