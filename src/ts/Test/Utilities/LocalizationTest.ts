93de106d85084a5d0525db9923458a36129a8765
Ville Orkas <villeo@unity3d.com>
2016-10-12 14:44:29 +0300

    Add namespaces

--- src/ts/Test/Utilities/LocalizationTest.ts
+++ src/ts/Test/Utilities/LocalizationTest.ts
@@ -6,19 +6,25 @@ import { Localization } from 'Utilities/Localization';
 describe('LocalizationTest', () => {
 
     it('should return phrase if language is not found', () => {
-        let localization = new Localization('asdasd');
+        let localization = new Localization('asdasd', 'asdasd');
+        let phrase = 'foobar';
+        assert.equal(localization.translate(phrase), phrase, 'Localization did not return original phrase');
+    });
+
+    it('should return phrase if no namespace is found', () => {
+        let localization = new Localization('en', 'asdasd');
         let phrase = 'foobar';
         assert.equal(localization.translate(phrase), phrase, 'Localization did not return original phrase');
     });
 
     it('should return phrase if no translation is found', () => {
-        let localization = new Localization('en');
+        let localization = new Localization('en', 'endscreen');
         let phrase = 'foobar';
         assert.equal(localization.translate(phrase), phrase, 'Localization did not return original phrase');
     });
 
     it('should translate', () => {
-        let localization = new Localization('fi');
+        let localization = new Localization('fi', 'endscreen');
         let phrase = 'Download For Free';
         assert.equal(localization.translate(phrase), 'Lataa ilmaiseksi', 'Localization did not translate');
     });
--- src/ts/Utilities/Localization.ts
+++ src/ts/Utilities/Localization.ts
@@ -1,25 +1,39 @@
 import EnglishEndscreen from 'json/locale/en/endscreen.json';
+import EnglishOverlay from 'json/locale/en/overlay.json';
 import FinnishEndscreen from 'json/locale/fi/endscreen.json';
+import FinnishOverlay from 'json/locale/fi/overlay.json';
 
 export class Localization {
 
     private static _languageMap = {
-        'en': JSON.parse(EnglishEndscreen),
-        'fi': JSON.parse(FinnishEndscreen)
+        'en': {
+            'endscreen': JSON.parse(EnglishEndscreen),
+            'overlay': JSON.parse(EnglishOverlay)
+        },
+        'fi': {
+            'endscreen': JSON.parse(FinnishEndscreen),
+            'overlay': JSON.parse(FinnishOverlay)
+        }
     };
 
     private _language: string;
+    private _namespace: string;
 
-    constructor(language: string) {
+    constructor(language: string, namespace: string) {
         this._language = language;
+        this._namespace = namespace;
     }
 
     public translate(phrase: string): string {
-        const map = Localization._languageMap[this._language];
-        if(!map || !(phrase in map)) {
+        const languageMap = Localization._languageMap[this._language];
+        if(!languageMap) {
             return phrase;
         }
-        return map[phrase];
+        const namespaceMap = languageMap[this._namespace];
+        if(!namespaceMap || !(phrase in namespaceMap)) {
+            return phrase;
+        }
+        return namespaceMap[phrase];
     }
 
 }
