3fb233ff0d4a9ecb3415527096279f3e0ec06aed
Ville Orkas <villeo@unity3d.com>
2016-10-27 16:10:45 +0300

    Cleanup

--- typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="globals/es6-promise/index.d.ts" />
-/// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/xmldom/index.d.ts" />
-/// <reference path="modules/assertion-error/index.d.ts" />
-/// <reference path="modules/chai/index.d.ts" />
-/// <reference path="modules/sinon/index.d.ts" />
