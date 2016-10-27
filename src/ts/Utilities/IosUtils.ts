d4a8e0d908f7bb01b215c79a4570692b842a04e4
Mikko Mäntysalmi <mikkoman@unity3d.com>
2016-10-27 16:24:12 +0300

    Use regex for checking ios version

--- src/ts/Utilities/IosUtils.ts
+++ src/ts/Utilities/IosUtils.ts
@@ -1,8 +1,7 @@
 export class IosUtils {
 
     public static isAppSheetBroken(osVersion: string): boolean {
-        if(osVersion.indexOf('8.0') === 0 || osVersion.indexOf('8.1') === 0
-            || osVersion.indexOf('8.2') === 0 || osVersion.indexOf('8.3') === 0) {
+        if(osVersion.match('^(8.0|8.1|8.2|8.3)')) {
             return true;
         }
         return false;
