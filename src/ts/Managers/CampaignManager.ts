a48ea3a8750875f5c32d3656362d7b024323cc53
Ville Orkas <villeo@unity3d.com>
2016-11-30 11:51:35 +0200

    Update playables test to use targeted campaigns and all AB groups except 6 and 7

--- src/ts/Managers/CampaignManager.ts
+++ src/ts/Managers/CampaignManager.ts
@@ -80,22 +80,44 @@ export class CampaignManager {
 
     private parsePerformanceCampaign(json: any) {
         this._nativeBridge.Sdk.logInfo('Unity Ads server returned game advertisement for AB Group ' + json.abGroup);
+        const htmlCampaign = this.parseHtmlCampaign(json);
+        if(htmlCampaign) {
+            this.onThirdPartyCampaign.trigger(htmlCampaign);
+        } else {
+            const campaign = new Campaign(json.campaign, json.gamerId, json.abGroup);
+            this.onCampaign.trigger(campaign);
+        }
+    }
+
+    private parseHtmlCampaign(json: any): HtmlCampaign | undefined {
         const campaign = new Campaign(json.campaign, json.gamerId, json.abGroup);
         let resource: string | undefined;
-        switch(campaign.getGameId()) {
-            case 11326: // Game of War iOS
+        switch(campaign.getId()) {
+            // Game of War iOS
+            case '583dfda0d933a3630a53249c':
+            case '583dfd52abb1feee0909882b':
+            case '583dfd45669a903e086e38d2':
+            case '583dfd4c9ceadb4708b021de':
                 resource = 'https://static.applifier.com/playables/SG_ios/index_ios.html';
                 break;
 
-            case 13480: // Game of War Android
+            // Game of War Android
+            case '583dfca5a93bfa6700d8c6f3':
+            case '583dfcb54622865a0a246bdf':
                 resource = 'https://static.applifier.com/playables/SG_android/index_android.html';
                 break;
 
-            case 53872: // Mobile Strike iOS
+            // Mobile Strike iOS
+            case '583dfb9a5b79df3f0a274f0b':
+            case '583dfe483fe2166c0ac9e6fb':
+            case '583dfba09bfc2a2d0a9a0b1c':
+            case '583dfba69d308fe203d7d740':
                 resource = 'https://static.applifier.com/playables/SMA_ios/index_ios.html';
                 break;
 
-            case 52447: // Mobile Strike Android
+            // Mobile Strike Android
+            case '583dfc532e4d9b5008c934d1':
+            case '583dfc667f448e630ac6a4bc':
                 resource = 'https://static.applifier.com/playables/SMA_android/index_android.html';
                 break;
 
@@ -104,12 +126,10 @@ export class CampaignManager {
         }
 
         const abGroup = campaign.getAbGroup();
-        if(resource && (abGroup === 10 || abGroup === 11 || abGroup === 12 || abGroup === 13)) {
-            const htmlCampaign = new HtmlCampaign(json.campaign, json.gamerId, json.abGroup, resource);
-            this.onThirdPartyCampaign.trigger(htmlCampaign);
-        } else {
-            this.onCampaign.trigger(campaign);
+        if(resource && abGroup !== 6 && abGroup !== 7) {
+            return new HtmlCampaign(json.campaign, json.gamerId, json.abGroup, resource);
         }
+        return undefined;
     }
 
     private parseVastCampaign(json: any) {
