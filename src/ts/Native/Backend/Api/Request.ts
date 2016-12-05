c1dcb0fa084bc7baa478928ed99e4ebd4a2db712
Daniel Alam <daniel.alam@unity3d.com>
2016-12-05 13:24:05 -0800

    Add support for mocking VAST responses.

--- src/ts/Native/Backend/Api/Request.ts
+++ src/ts/Native/Backend/Api/Request.ts
@@ -21,12 +21,36 @@ export class Request {
     }
 
     public static post(id: string, url: string, body: string, headers: [string, string][], connectTimeout: number, readTimeout: number) {
+        const vastBodyElement = <HTMLInputElement>window.parent.document.getElementById('vastBody');
+
+        Request._requestLog.push(url);
+        const xhr = new XMLHttpRequest();
+        xhr.onload = (event: Event) => {
+            let responseText = xhr.responseText;
+
+            if (vastBodyElement.value !== '' && url.match(/games\/\d{1,}\/fill/)) {
+                responseText = `{
+                                  "abGroup": 3,
+                                  "vast": {
+                                    "data": "` + encodeURIComponent(vastBodyElement.value) + `"
+                                  },
+                                  "gamerId": "5712983c481291b16e1be03b"
+                                }`;
+            }
+
+            Backend.sendEvent('REQUEST', 'COMPLETE', id, url, responseText, xhr.status, xhr.getAllResponseHeaders());
+        };
+        xhr.open('POST', url);
+        xhr.send(body);
+    }
+
+    public static head(id: string, url: string, body: string, headers: [string, string][], connectTimeout: number, readTimeout: number) {
         Request._requestLog.push(url);
         const xhr = new XMLHttpRequest();
         xhr.onload = (event: Event) => {
             Backend.sendEvent('REQUEST', 'COMPLETE', id, url, xhr.responseText, xhr.status, xhr.getAllResponseHeaders());
         };
-        xhr.open('POST', url);
+        xhr.open('HEAD', url);
         xhr.send(body);
     }
 
