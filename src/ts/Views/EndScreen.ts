6ac37820bdbe2a2cc9fda9660e6ccb2ffd450156
Mikko Mäntysalmi <mikkoman@unity3d.com>
2017-01-04 12:05:59 +0200

    Remove xmas theme

--- src/ts/Views/EndScreen.ts
+++ src/ts/Views/EndScreen.ts
@@ -89,10 +89,6 @@ export class EndScreen extends View {
     }
 
     private getEndscreenAlt(campaign: Campaign) {
-        const abGroup = campaign.getAbGroup();
-        if(abGroup !== 8 && abGroup !== 9) {
-            return 'xmas';
-        }
         return undefined;
     }
 
