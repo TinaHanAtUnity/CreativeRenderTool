338fb3c16ae7b1b63d10b4d4b416f28857851d2f
Janne Nikula <jannen@unity3d.com>
2017-02-24 17:05:27 +0200

    Add simple test

--- src/ts/Test/Unit/Utilities/UrlTest.ts
+++ src/ts/Test/Unit/Utilities/UrlTest.ts
@@ -8,4 +8,19 @@ describe('UrlTest', () => {
         const url: string = Url.addParameters('http://www.google.fi', {test: true});
         assert.equal(url, 'http://www.google.fi?test=true');
     });
+
+    describe('validation', () => {
+        it('should validate legal URLs', () => {
+            assert.equal(true, Url.isValid('https://www.unity3d.com'), 'valid url was not accepted');
+            assert.equal(true, Url.isValid('https://www.example.net/~user/dynamic$test.png'), 'valid url was not accepted');
+            assert.equal(true, Url.isValid('http://localhost:8000/build/dev/config.json'), 'valid url was not accepted');
+            assert.equal(true, Url.isValid('https://www.unity3d.com/search?foo=bar'), 'valid url was not accepted');
+        });
+
+        it('should not validate illegal URLs', () => {
+            assert.equal(false, Url.isValid('rtmp://a.rtmp.youtube.com/videolive?ns=yt-live'), 'invalid url was accepted');
+            assert.equal(false, Url.isValid('https://example.net/"url".png'), 'invalid url was accepted');
+            assert.equal(false, Url.isValid(''), 'invalid url was accepted');
+        });
+    });
 });
