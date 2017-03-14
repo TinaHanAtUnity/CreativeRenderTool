0a9b8f5d689936894eb647bc51733000b1a96b43
Daniel Alam <daniel.alam@unity3d.com>
2017-03-13 20:07:25 -0700

    Set iframe height and width in pixels based on the screen size.

--- src/ts/Views/MRAID.ts
+++ src/ts/Views/MRAID.ts
@@ -57,6 +57,9 @@ export class MRAID extends View {
         this._closeElement = <HTMLElement>this._container.querySelector('.close-region');
 
         const iframe: any = this._iframe = <HTMLIFrameElement>this._container.querySelector('#mraid-iframe');
+        iframe.width = screen.width;
+        iframe.height = screen.height;
+
         this.createMRAID().then(mraid => {
             iframe.srcdoc = mraid;
         });
@@ -65,6 +68,7 @@ export class MRAID extends View {
 
     public show(): void {
         super.show();
+        const iframe: any = this._iframe;
 
         let originalLength = 30;
         if(this._placement.allowSkip()) {
@@ -94,6 +98,8 @@ export class MRAID extends View {
             });
         }
         this._resizeHandler = (event: Event) => {
+            iframe.width = window.innerWidth;
+            iframe.height = window.innerHeight;
             if(this._iframe.contentWindow) {
                 this._iframe.contentWindow.postMessage({
                     type: 'resize',
