bebd3a6361485b493de0d9cb9ebd52a967b92714
Pekka Palmu <pekka.palmu@gmail.com>
2017-04-03 15:54:44 +0300

    get clearTestMetaData value and delete test metadata only if it is set to true

--- src/ts/Utilities/TestEnvironment.ts
+++ src/ts/Utilities/TestEnvironment.ts
@@ -2,16 +2,24 @@ import { MetaData } from 'Utilities/MetaData';
 
 export class TestEnvironment {
     public static setup(metaData: MetaData): Promise<string[]> {
-        return metaData.getKeys('test').then(keys => {
-            const promises: any[] = [];
-            keys.forEach((key) => {
-                promises.push(metaData.get('test.' + key, true).then(([found, value]) => {
-                    if(found) {
-                        this._testEnvironment[key] = value;
-                    }
-                }));
+        return metaData.get("test.clearTestMetaData", false).then(([found, value]) => {
+            let deleteValue = false;
+            if(found && typeof value === 'boolean') {
+                deleteValue = value;
+            }
+            return deleteValue;
+        }).then(deleteValue => {
+            return metaData.getKeys('test').then(keys => {
+                const promises: any[] = [];
+                keys.forEach((key) => {
+                    promises.push(metaData.get('test.' + key, deleteValue).then(([found, value]) => {
+                        if(found) {
+                            this._testEnvironment[key] = value;
+                        }
+                    }));
+                });
+                return Promise.all(promises);
             });
-            return Promise.all(promises);
         });
     }
 
