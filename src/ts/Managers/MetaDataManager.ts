dee9aa05cc7f5eb36f75251544e48123430416fc
Pekka Palmu <pekka.palmu@gmail.com>
2017-04-28 17:36:00 +0300

    Rewrite metadata manager

--- src/ts/Managers/MetaDataManager.ts
+++ src/ts/Managers/MetaDataManager.ts
@@ -1,49 +1,34 @@
 import { NativeBridge } from 'Native/NativeBridge';
-import { FrameworkMetaData } from 'Models/MetaData/FrameworkMetaData';
-import { AdapterMetaData } from 'Models/MetaData/AdapterMetaData';
-import { MediationMetaData } from 'Models/MetaData/MediationMetaData';
-import { PlayerMetaData } from 'Models/MetaData/PlayerMetaData';
+import { BaseMetaData } from 'Models/MetaData/BaseMetaData';
 
 export class MetaDataManager {
+    private _metaDataCache = {};
+    private _nativeBridge: NativeBridge;
 
-    public static getValues(category: string, keys: string[], nativeBridge: NativeBridge) {
-        return Promise.resolve([]);
+    constructor(nativeBridge: NativeBridge) {
+        this._nativeBridge = nativeBridge;
     }
 
-    public static fetchFrameworkMetaData(nativeBridge: NativeBridge, cache = true): Promise<FrameworkMetaData> {
-        return Promise.resolve(<FrameworkMetaData>{});
+    public fetch<T extends BaseMetaData>(MetaDataConstructor: { new(): T; }, cache: boolean = true, keys?: string[]): Promise<T> {
+        let metaData: T = new MetaDataConstructor();
+
+        if (this._metaDataCache[metaData.getCategory()]) {
+            metaData = this._metaDataCache[metaData.getCategory()];
+        }
+
+        return metaData.fetch(this._nativeBridge, keys).then((success) => {
+            if (success) {
+                if(cache) {
+                    this._metaDataCache[metaData.getCategory()] = metaData;
+                }
+                return metaData;
+            }
+
+            return undefined;
+        });
     }
 
-    public static fetchAdapterMetaData(nativeBridge: NativeBridge, cache = true): Promise<AdapterMetaData> {
-        return Promise.resolve(<AdapterMetaData>{});
-    }
-
-    // this method is for getting metadata object but it does not update the ordinal
-    public static fetchMediationMetaData(nativeBridge: NativeBridge, cache = true): Promise<MediationMetaData> {
-        return Promise.resolve(<MediationMetaData>{});
-    }
-
-    // this method needs to be invoked when show is called to refresh mediation ordinal
-    public static updateMediationMetaData(nativeBridge: NativeBridge): Promise<void> {
-        return Promise.resolve();
-    }
-
-    public static fetchPlayerMetaData(nativeBridge: NativeBridge): Promise<PlayerMetaData> {
-        return Promise.resolve(<PlayerMetaData>{});
-    }
-
-    public static fetch(category: string, keys: string[], nativeBridge: NativeBridge, cache = true): Promise<void> {
-        return Promise.resolve(undefined);
-    }
-
-    public static createAndCache(category: string, data: string[], cache = true) {
-        return undefined;
-    }
-
-    public static createByCategory(category: string, data: string[]): undefined {
-        return undefined;
-    }
-
-    public static clearCaches() {
+    public clearCache() {
+        this._metaDataCache = {};
     }
 }
