7dd469bc0a814c802c118fc763773e106ff9ebff
Ville Orkas <villeo@unity3d.com>
2017-05-15 12:27:28 +0300

    Cleanup Session.ts and add prevention for multiple click events

--- src/ts/EventHandlers/VastVideoEventHandlers.ts
+++ src/ts/EventHandlers/VastVideoEventHandlers.ts
@@ -1,14 +1,15 @@
 import { VastAdUnit } from 'AdUnits/VastAdUnit';
 import { SessionManager } from 'Managers/SessionManager';
+import { EventType } from 'Models/Session';
 
 export class VastVideoEventHandlers {
 
     public static onVideoStart(sessionManager: SessionManager, adUnit: VastAdUnit): void {
         if (sessionManager.getSession()) {
-            if (sessionManager.getSession().impressionSent) {
+            if (sessionManager.getSession().getEventSent(EventType.IMPRESSION)) {
                 return;
             }
-            sessionManager.getSession().impressionSent = true;
+            sessionManager.getSession().setEventSent(EventType.IMPRESSION);
         }
         adUnit.sendImpressionEvent(sessionManager.getEventManager(), sessionManager.getSession().getId(), sessionManager.getClientInfo().getSdkVersion());
         adUnit.sendTrackingEvent(sessionManager.getEventManager(), 'creativeView', sessionManager.getSession().getId(), sessionManager.getClientInfo().getSdkVersion());
@@ -17,10 +18,10 @@ export class VastVideoEventHandlers {
 
     public static onVideoCompleted(sessionManager: SessionManager, adUnit: VastAdUnit) {
         if (sessionManager.getSession()) {
-            if (sessionManager.getSession().vastCompleteSent) {
+            if (sessionManager.getSession().getEventSent(EventType.VAST_COMPLETE)) {
                 return;
             }
-            sessionManager.getSession().vastCompleteSent = true;
+            sessionManager.getSession().setEventSent(EventType.VAST_COMPLETE);
         }
         adUnit.sendTrackingEvent(sessionManager.getEventManager(), 'complete', sessionManager.getSession().getId(), sessionManager.getClientInfo().getSdkVersion());
 
