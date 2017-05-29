2e9d9e49708b8ea5e64f2e04dcdc8a860c8ac2bc
Pekka Palmu <pekka.palmu@gmail.com>
2017-05-29 12:40:45 +0300

    Add previous campaign to campaign manager

--- src/ts/Managers/CampaignManager.ts
+++ src/ts/Managers/CampaignManager.ts
@@ -74,6 +74,7 @@ export class CampaignManager {
     private _deviceInfo: DeviceInfo;
     private _vastParser: VastParser;
     private _requesting: boolean;
+    private _previousPlacementId: string;
 
     constructor(nativeBridge: NativeBridge, configuration: Configuration, assetManager: AssetManager, request: Request, clientInfo: ClientInfo, deviceInfo: DeviceInfo, vastParser: VastParser, metaDataManager: MetaDataManager) {
         this._nativeBridge = nativeBridge;
@@ -122,6 +123,14 @@ export class CampaignManager {
                 this.onError.trigger(error);
             }
         });
+    }
+
+    public setPreviousPlacementId(id: string) {
+        this._previousPlacementId = id;
+    }
+
+    public getPreviousPlacementId(): string {
+        return this._previousPlacementId;
     }
 
     private parseCampaign(response: INativeResponse) {
@@ -435,6 +444,10 @@ export class CampaignManager {
             language: this.getParameter('language', this._deviceInfo.getLanguage(), 'string'),
             timeZone: this.getParameter('timeZone', this._deviceInfo.getTimeZone(), 'string')
         };
+
+        if (this.getPreviousPlacementId()) {
+            body.previousPlacementId = this.getPreviousPlacementId();
+        }
 
         if(typeof navigator !== 'undefined' && navigator.userAgent) {
             body.webviewUa = this.getParameter('webviewUa', navigator.userAgent, 'string');
