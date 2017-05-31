57735d92c1bf1ccbc49b81377629f627cd387ad4
Mikko Mäntysalmi <mikkoman@unity3d.com>
2017-05-31 15:00:21 +0300

    Connect Promo ad unit and view

--- src/ts/AdUnits/PromoAdUnit.ts
+++ src/ts/AdUnits/PromoAdUnit.ts
@@ -1,23 +1,38 @@
 import { AbstractAdUnit } from "AdUnits/AbstractAdUnit";
 import { NativeBridge } from "Native/NativeBridge";
-import {AdUnitContainer, ForceOrientation} from "AdUnits/Containers/AdUnitContainer";
+import { AdUnitContainer, ForceOrientation } from "AdUnits/Containers/AdUnitContainer";
 import { SessionManager } from "Managers/SessionManager";
 import { Placement } from "Models/Placement";
 import { Campaign } from "Models/Campaign";
+import { Promo } from 'Views/Promo';
 
 export class PromoAdUnit extends AbstractAdUnit {
-    constructor(nativeBridge: NativeBridge, container: AdUnitContainer, sessionManager: SessionManager, placement: Placement, campaign: Campaign, options: any) {
+
+    private _promoView: Promo;
+
+    constructor(nativeBridge: NativeBridge, container: AdUnitContainer, sessionManager: SessionManager, placement: Placement, campaign: Campaign, promo: Promo, options: any) {
         super(nativeBridge, container, placement, campaign);
+
+        this._promoView = promo;
     }
     public show(): Promise<void> {
+        this._promoView.show();
+
         return this._container.open(this, false, false, ForceOrientation.NONE, true, true, {} );
     }
 
     public hide(): Promise<void> {
+        this._promoView.hide();
+
+        this.unsetReferences();
         return this._container.close();
     }
 
     public description(): string {
         return 'mraid';
+    }
+
+    private unsetReferences() {
+        delete this._promoView;
     }
 }
