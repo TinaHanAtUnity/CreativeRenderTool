725acf0454b56fe3d4c26835ea12eb4777543f76
Brandon Zarzoza <brandonz@unity3d.com>
2017-06-02 10:24:41 +0300

    Adjust campaign manager to set a default value for appStoreId

--- src/ts/Managers/CampaignManager.ts
+++ src/ts/Managers/CampaignManager.ts
@@ -236,6 +236,7 @@ export class CampaignManager {
     private parsePerformanceCampaign(json: any): Promise<void> {
         this._nativeBridge.Sdk.logInfo('Unity Ads server returned game advertisement for AB Group ' + json.abGroup);
         if(json.promo && json.promo.iapProductId) {
+            json.promo.appStoreId = "iapPromo";
             const campaign = new PromoCampaign(json.promo, json.gamerId, CampaignManager.AbGroup ? CampaignManager.AbGroup : json.abGroup);
             return this._assetManager.setup(campaign).then(() => this.onPromoCampaign.trigger(campaign));
         } else if(json.campaign && json.campaign.mraidUrl) {
