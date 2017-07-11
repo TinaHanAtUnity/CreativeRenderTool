c1354ffbfaa6395d4f30080a4d9594e554cda581
Janne Nikula <jannen@unity3d.com>
2017-07-11 14:46:42 +0300

    Deploy to A/B group 13

--- src/ts/Managers/AssetManager.ts
+++ src/ts/Managers/AssetManager.ts
@@ -63,6 +63,10 @@ export class AssetManager {
             throw new Error('Invalid required assets in campaign ' + campaign.getId());
         }
 
+        if(campaign.getAbGroup() === 13) {
+            this._cacheMode = CacheMode.ADAPTIVE;
+        }
+
         if(this._cacheMode === CacheMode.DISABLED) {
             return Promise.resolve(campaign);
         }
