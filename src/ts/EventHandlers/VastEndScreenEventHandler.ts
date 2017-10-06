53e3977465cf8615183457dacfb4402c3ae07df9
Pekka Palmu <pekka.palmu@gmail.com>
2017-10-06 15:16:03 +0300

    remove unused event handlers and add new vast event handlers

---
+++ src/ts/EventHandlers/VastEndScreenEventHandler.ts
@@ -0,0 +1,55 @@
+import { IVastEndScreenHandler } from 'Views/VastEndScreen';
+import { IAdUnitParameters } from 'AdUnits/AbstractAdUnit';
+import { NativeBridge } from 'Native/NativeBridge';
+import { ClientInfo } from 'Models/ClientInfo';
+import { VastAdUnit } from 'AdUnits/VastAdUnit';
+import { Request } from 'Utilities/Request';
+import { Platform } from 'Constants/Platform';
+import { KeyCode } from 'Constants/Android/KeyCode';
+
+export class VastEndScreenEventHandler implements IVastEndScreenHandler {
+    private _nativeBridge: NativeBridge;
+    private _adUnit: VastAdUnit;
+    private _clientInfo: ClientInfo;
+    private _request: Request;
+
+    constructor(nativeBridge: NativeBridge, adUnit: VastAdUnit, parameters: IAdUnitParameters) {
+        this._nativeBridge = nativeBridge;
+        this._adUnit = adUnit;
+        this._clientInfo = parameters.clientInfo;
+    }
+
+    public onVastEndScreenClick(): Promise<void> {
+        const platform = this._nativeBridge.getPlatform();
+        const clickThroughURL = this._adUnit.getCompanionClickThroughUrl() || this._adUnit.getVideoClickThroughURL();
+        this._adUnit.sendTrackingEvent('videoEndCardClick', this._adUnit.getCampaign().getSession().getId(), this._clientInfo.getSdkVersion());
+
+        if (clickThroughURL) {
+            return this._request.followRedirectChain(clickThroughURL).then((url: string) => {
+                if (platform === Platform.IOS) {
+                    this._nativeBridge.UrlScheme.open(url);
+                } else if (this._nativeBridge.getPlatform() === Platform.ANDROID) {
+                    this._nativeBridge.Intent.launch({
+                        'action': 'android.intent.action.VIEW',
+                        'uri': url
+                    });
+                }
+            });
+        }
+        return Promise.reject(new Error('There is no clickthrough URL for video or companion'));
+    }
+
+    public onVastEndScreenClose(): void {
+        this._adUnit.hide();
+    }
+
+    public onKeyEvent(keyCode: number): void {
+        if (keyCode === KeyCode.BACK && this._adUnit.isShowing() && !this._adUnit.isActive()) {
+            this._adUnit.hide();
+        }
+    }
+
+    public onVastEndScreenShow(): void {
+        this._adUnit.sendCompanionTrackingEvent(this._adUnit.getCampaign().getSession().getId(), this._clientInfo.getSdkVersion());
+    }
+}
