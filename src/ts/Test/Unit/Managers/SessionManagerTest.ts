10c096255bec9bcd2fe90aee966b671dd30ec134
Ville Orkas <villeo@unity3d.com>
2017-10-11 18:40:28 +0300

    Dummy commit

--- src/ts/Test/Unit/Managers/SessionManagerTest.ts
+++ src/ts/Test/Unit/Managers/SessionManagerTest.ts
@@ -197,7 +197,7 @@ describe('SessionManagerTest', () => {
 
     xit('Retry failed event', () => {
         const url: string = 'https://www.example.net/retry_event';
-        const data: string = 'Retry test';
+        const data: string = 'Retry testing';
         const sessionId: string = 'abcd-1234';
         const eventId: string = '5678-efgh';
 
