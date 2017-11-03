d59b9ce1a8470751f700ed2fa78ca0a22eb0b058
Jon Appelberg <ujappelbe@users.noreply.github.com>
2017-11-03 20:29:38 +0200

    Git had issues merging Browser.ts properly, fixed.

--- src/ts/Browser.ts
+++ src/ts/Browser.ts
@@ -119,116 +119,116 @@ const setIosDeviceInfo = () => {
     });
 };
 
-const abGroupElement = <HTMLInputElement>window.parent.document.getElementById('abGroup');
-const campaignIdElement = <HTMLInputElement>window.parent.document.getElementById('campaignId');
-const countryElement = <HTMLInputElement>window.parent.document.getElementById('country');
-const platformElement = <HTMLInputElement>window.parent.document.getElementById('platform');
-const gameIdElement = <HTMLInputElement>window.parent.document.getElementById('gameId');
-const testModeElement = <HTMLInputElement>window.parent.document.getElementById('testMode');
-const autoSkipElement = <HTMLInputElement>window.parent.document.getElementById('autoSkip');
-const initializeButton = <HTMLButtonElement>window.parent.document.getElementById('initialize');
-const campaignResponseElement = <HTMLInputElement>window.parent.document.getElementById('campaignResponse');
+if(window.parent !== window) {
+    const abGroupElement = <HTMLInputElement>window.parent.document.getElementById('abGroup');
+    const campaignIdElement = <HTMLInputElement>window.parent.document.getElementById('campaignId');
+    const countryElement = <HTMLInputElement>window.parent.document.getElementById('country');
+    const platformElement = <HTMLInputElement>window.parent.document.getElementById('platform');
+    const gameIdElement = <HTMLInputElement>window.parent.document.getElementById('gameId');
+    const testModeElement = <HTMLInputElement>window.parent.document.getElementById('testMode');
+    const autoSkipElement = <HTMLInputElement>window.parent.document.getElementById('autoSkip');
+    const initializeButton = <HTMLButtonElement>window.parent.document.getElementById('initialize');
+    const campaignResponseElement = <HTMLInputElement>window.parent.document.getElementById('campaignResponse');
 
-const initialize = () => {
-    abGroupElement.disabled = true;
-    campaignIdElement.disabled = true;
-    countryElement.disabled = true;
-    platformElement.disabled = true;
-    gameIdElement.disabled = true;
-    testModeElement.disabled = true;
-    autoSkipElement.disabled = true;
-    initializeButton.disabled = true;
+    const initialize = () => {
+        abGroupElement.disabled = true;
+        campaignIdElement.disabled = true;
+        countryElement.disabled = true;
+        platformElement.disabled = true;
+        gameIdElement.disabled = true;
+        testModeElement.disabled = true;
+        autoSkipElement.disabled = true;
+        initializeButton.disabled = true;
 
-    const publicStorage: any = {
-        test: {}
-    };
-
-    if(abGroupElement.value.length) {
-        publicStorage.test.abGroup = {
-            value: parseInt(abGroupElement.value, 10),
-            ts: Date.now()
+        const publicStorage: any = {
+            test: {}
         };
-    }
-    if(campaignIdElement.value.length) {
-        publicStorage.test.campaignId = {
-            value: campaignIdElement.value,
-            ts: Date.now()
-        };
-    }
-    if(countryElement.value.length) {
-        publicStorage.test.country = {
-            value: countryElement.value,
-            ts: Date.now()
-        };
-    }
-    if(autoSkipElement.checked) {
-        publicStorage.test.autoSkip = {
-            value: true,
-            ts: Date.now()
-        };
-    }
-    if(campaignResponseElement.value.length) {
-        CampaignManager.setCampaignResponse(campaignResponseElement.value);
-    }
 
-    window.sessionStorage.clear();
-    window.sessionStorage.setItem('PUBLIC', JSON.stringify(publicStorage));
+        if(abGroupElement.value.length) {
+            publicStorage.test.abGroup = {
+                value: parseInt(abGroupElement.value, 10),
+                ts: Date.now()
+            };
+        }
+        if(campaignIdElement.value.length) {
+            publicStorage.test.campaignId = {
+                value: campaignIdElement.value,
+                ts: Date.now()
+            };
+        }
+        if(countryElement.value.length) {
+            publicStorage.test.country = {
+                value: countryElement.value,
+                ts: Date.now()
+            };
+        }
+        if(autoSkipElement.checked) {
+            publicStorage.test.autoSkip = {
+                value: true,
+                ts: Date.now()
+            };
+        }
+        if(campaignResponseElement.value.length) {
+            CampaignManager.setCampaignResponse(campaignResponseElement.value);
+        }
 
-    setClientInfo();
+        window.sessionStorage.clear();
+        window.sessionStorage.setItem('PUBLIC', JSON.stringify(publicStorage));
 
-    // tslint:disable:no-console
-    const listener: IUnityAdsListener = {
-        onUnityAdsReady: (placement: string) => {
-            console.log('onUnityAdsReady: ' + placement);
-            const placementButton = <HTMLButtonElement>window.parent.document.getElementById(placement);
-            if(typeof location !== 'undefined' && location.search.indexOf('headless=1') !== -1) {
-                UnityAds.show(placement);
-            } else {
-                const placementButtonlistener = (placementButtonEvent: Event) => {
-                    placementButtonEvent.preventDefault();
-                    placementButton.disabled = true;
-                    placementButton.removeEventListener('click', placementButtonlistener, false);
+        setClientInfo();
+
+        // tslint:disable:no-console
+        const listener: IUnityAdsListener = {
+            onUnityAdsReady: (placement: string) => {
+                console.log('onUnityAdsReady: ' + placement);
+                const placementButton = <HTMLButtonElement>window.parent.document.getElementById(placement);
+                if(typeof location !== 'undefined' && location.search.indexOf('headless=1') !== -1) {
                     UnityAds.show(placement);
-                };
-                placementButton.disabled = false;
-                placementButton.addEventListener('click', placementButtonlistener, false);
+                } else {
+                    const placementButtonlistener = (placementButtonEvent: Event) => {
+                        placementButtonEvent.preventDefault();
+                        placementButton.disabled = true;
+                        placementButton.removeEventListener('click', placementButtonlistener, false);
+                        UnityAds.show(placement);
+                    };
+                    placementButton.disabled = false;
+                    placementButton.addEventListener('click', placementButtonlistener, false);
+                }
+            },
+            onUnityAdsStart: (placement: string) => {
+                console.log('onUnityAdsStart: ' + placement);
+            },
+            onUnityAdsFinish: (placement: string, state: FinishState) => {
+                console.log('onUnityAdsFinish: ' + placement + ' - ' + state);
+            },
+            onUnityAdsError: (error: UnityAdsError, message: string) => {
+                console.log('onUnityAdsError: ' + error + ' - ' + message);
+            },
+            onUnityAdsClick: (placement: string) => {
+                console.log('onUnityAdsClick: ' + placement);
+            },
+            onUnityAdsPlacementStateChanged: (placement: string, oldState: PlacementState, newState: PlacementState) => {
+                console.log('onUnityAdsPlacementStateChanged: ' + placement + ' ' + PlacementState[oldState] + ' -> ' + PlacementState[newState]);
             }
-        },
-        onUnityAdsStart: (placement: string) => {
-            console.log('onUnityAdsStart: ' + placement);
-        },
-        onUnityAdsFinish: (placement: string, state: FinishState) => {
-            console.log('onUnityAdsFinish: ' + placement + ' - ' + state);
-        },
-        onUnityAdsError: (error: UnityAdsError, message: string) => {
-            console.log('onUnityAdsError: ' + error + ' - ' + message);
-        },
-        onUnityAdsClick: (placement: string) => {
-            console.log('onUnityAdsClick: ' + placement);
-        },
-        onUnityAdsPlacementStateChanged: (placement: string, oldState: PlacementState, newState: PlacementState) => {
-            console.log('onUnityAdsPlacementStateChanged: ' + placement + ' ' + PlacementState[oldState] + ' -> ' + PlacementState[newState]);
+        };
+        // tslint:enable:no-console
+
+        switch(platformElement.value) {
+            case 'android':
+                setAndroidDeviceInfo();
+                UnityAds.initialize(Platform.ANDROID, gameIdElement.value, listener, testModeElement.checked);
+                break;
+
+            case 'ios':
+                setIosDeviceInfo();
+                UnityAds.initialize(Platform.IOS, gameIdElement.value, listener, testModeElement.checked);
+                break;
+
+            default:
+                throw new Error('Unity Ads webview init failure: no platform defined, unable to initialize native bridge');
         }
     };
-    // tslint:enable:no-console
 
-    switch(platformElement.value) {
-        case 'android':
-            setAndroidDeviceInfo();
-            UnityAds.initialize(Platform.ANDROID, gameIdElement.value, listener, testModeElement.checked);
-            break;
-
-        case 'ios':
-            setIosDeviceInfo();
-            UnityAds.initialize(Platform.IOS, gameIdElement.value, listener, testModeElement.checked);
-            break;
-
-        default:
-            throw new Error('Unity Ads webview init failure: no platform defined, unable to initialize native bridge');
-    }
-};
-
-if(window.parent !== window) {
     const getHeadlessName = 'getHeadless';
     if(window.parent[getHeadlessName]()) {
         initialize();
