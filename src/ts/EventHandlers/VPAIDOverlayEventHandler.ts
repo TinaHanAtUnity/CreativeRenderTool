f166b90ea53418b1b851be6898af73df7cbc1c43
Scott Wise <scottw@unity3d.com>
2017-11-08 17:42:38 -0800

    Send Comscore event on vpaid end (skipped) without playtime and length video ad

--- src/ts/EventHandlers/VPAIDOverlayEventHandler.ts
+++ src/ts/EventHandlers/VPAIDOverlayEventHandler.ts
@@ -3,19 +3,23 @@ import { FinishState } from 'Constants/FinishState';
 import { NativeBridge } from 'Native/NativeBridge';
 import { IVPAIDAdUnitParameters, VPAIDAdUnit } from 'AdUnits/VPAIDAdUnit';
 import { OperativeEventManager } from 'Managers/OperativeEventManager';
+import { ComScoreTrackingService } from "Utilities/ComScoreTrackingService";
 
 export class VPAIDOverlayEventHandler implements IOverlayHandler {
     private _adUnit: VPAIDAdUnit;
     private _operativeEventManager: OperativeEventManager;
+    private _comScoreTrackingService: ComScoreTrackingService;
 
     constructor(nativeBridge: NativeBridge, adUnit: VPAIDAdUnit, parameters: IVPAIDAdUnitParameters) {
         this._adUnit = adUnit;
         this._operativeEventManager = parameters.operativeEventManager;
+        this._comScoreTrackingService = parameters.comScoreTrackingService;
     }
 
     public onOverlaySkip(position: number): void {
         this._adUnit.sendTrackingEvent('skip');
         this._operativeEventManager.sendSkip(this._adUnit);
+        this.sendComscoreEvent('end', 0);
         this._adUnit.setFinishState(FinishState.SKIPPED);
         this._adUnit.hide();
     }
@@ -30,5 +34,15 @@ export class VPAIDOverlayEventHandler implements IOverlayHandler {
 
     public onOverlayPauseForTesting(paused: boolean): void {
         // EMPTY
+    }
+
+    private sendComscoreEvent(eventName: string, position: number) {
+        const sessionId = this._adUnit.getCampaign().getSession().getId();
+        const creativeId = this._adUnit.getCampaign().getCreativeId();
+        const category = this._adUnit.getCampaign().getCategory();
+        const subCategory = this._adUnit.getCampaign().getSubCategory();
+        const adDuration = 0;
+
+        this._comScoreTrackingService.sendEvent(eventName, sessionId, adDuration.toString(10), position, creativeId, category, subCategory);
     }
 }
