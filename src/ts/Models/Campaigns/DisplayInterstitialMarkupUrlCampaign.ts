4ec2ec32cf02b40959b3bcacf14a08b907be0c71
Scott Wise <scottw@unity3d.com>
2017-12-04 14:56:30 -0800

    Fix markupurl campaign

--- src/ts/Models/Campaigns/DisplayInterstitialMarkupUrlCampaign.ts
+++ src/ts/Models/Campaigns/DisplayInterstitialMarkupUrlCampaign.ts
@@ -3,21 +3,32 @@ import { Asset } from 'Models/Assets/Asset';
 import { Session } from 'Models/Session';
 import { HTML } from 'Models/Assets/HTML';
 
-interface IDisplayInterstitialCampaign extends ICampaign {
+interface IDisplayInterstitialMarkupUrlCampaign extends ICampaign {
+    markupUrl: string;
     tracking: object | undefined;
     resourceAsset: HTML | undefined;
 }
 
-export class DisplayInterstitialMarkupUrlCampaign extends Campaign<IDisplayInterstitialCampaign> {
+export class DisplayInterstitialMarkupUrlCampaign extends Campaign<IDisplayInterstitialMarkupUrlCampaign> {
     constructor(markupUrl: string, session: Session, gamerId: string, abGroup: number, cacheTTL: number | undefined, tracking?: { [eventName: string]: string[] }, adType?: string, creativeId?: string, seatId?: number, correlationId?: string) {
         super('DisplayInterstitialMarkupUrlCampaign', {
             ... Campaign.Schema,
+            markupUrl: ['string'],
             resourceAsset: ['object', 'undefined'],
             tracking: ['object', 'undefined']
         });
         if(cacheTTL) {
             this.set('willExpireAt', Date.now() + cacheTTL * 1000);
         }
+        this.set('markupUrl', markupUrl);
+        this.set('gamerId', gamerId);
+        this.set('abGroup', abGroup);
+        this.set('adType', adType || undefined);
+        this.set('correlationId', correlationId || undefined);
+        this.set('creativeId', creativeId || undefined);
+        this.set('seatId', seatId || undefined);
+        this.set('tracking', tracking || undefined);
+        this.set('session', session);
     }
 
     public getTrackingUrlsForEvent(eventName: string): string[] {
@@ -26,6 +37,10 @@ export class DisplayInterstitialMarkupUrlCampaign extends Campaign<IDisplayInter
             return tracking[eventName] || [];
         }
         return [];
+    }
+
+    public getMarkupUrl(): string {
+        return this.get('markupUrl');
     }
 
     public getOptionalAssets(): Asset[] {
