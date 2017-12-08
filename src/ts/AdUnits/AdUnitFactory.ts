ef917b11c53f852a3778ccff2464a8e04a8410b2
Scott Wise <scottw@unity3d.com>
2017-12-07 19:47:42 -0800

    Remove comma

--- src/ts/AdUnits/AdUnitFactory.ts
+++ src/ts/AdUnits/AdUnitFactory.ts
@@ -149,7 +149,7 @@ export class AdUnitFactory {
             level4: campaign.getCreativeId(),
             slicer1: parameters.clientInfo.getSdkVersionName(),
             slicer2: 'Unity placements bundle Id - not sure what that corresponds to',
-            slicer3: parameters.placement.getName(),
+            slicer3: parameters.placement.getName()
         };
 
         const moatData = {
