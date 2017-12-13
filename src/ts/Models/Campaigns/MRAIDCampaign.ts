539dc5a5c61aaaf377ccc7624445db49ce0604b5
Mikko Mäntysalmi <mikkoman@unity3d.com>
2017-12-13 12:39:58 +0200

    Download end screen assets for roll the ball playable campaign

--- src/ts/Models/Campaigns/MRAIDCampaign.ts
+++ src/ts/Models/Campaigns/MRAIDCampaign.ts
@@ -4,6 +4,7 @@ import { Image } from 'Models/Assets/Image';
 import { Asset } from 'Models/Assets/Asset';
 import { StoreName } from 'Models/Campaigns/PerformanceCampaign';
 import { Session } from 'Models/Session';
+import { CustomFeatures } from 'Utilities/CustomFeatures';
 
 export interface IMRAIDCampaign extends ICampaign {
     resourceAsset: HTML | undefined;
@@ -111,6 +112,10 @@ export class MRAIDCampaign extends Campaign<IMRAIDCampaign> {
         }
         this.set('appStoreId', campaign.appStoreId);
 
+        if(resourceUrl && CustomFeatures.isPlayableEndScreenTest(abGroup, resourceUrl)) {
+            this.set('landscapeImage', new Image('https://static.everyplay.com/impact/images/130393/4729d640d83d4a18/unityads600x800-b.jpg'));
+            this.set('portraitImage', new Image('https://static.everyplay.com/impact/images/130393/98c14b54d4c51801/unityads800x600-b.jpg'));
+        }
     }
 
     public getResourceUrl(): HTML | undefined {
