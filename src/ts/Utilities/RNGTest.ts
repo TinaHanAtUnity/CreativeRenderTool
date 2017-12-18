4cb831fe0b629bffbe1f1fd6c39aad3d1e047312
Janne Nikula <jannen@unity3d.com>
2017-12-18 17:15:16 +0200

    Use object instead of array

--- src/ts/Utilities/RNGTest.ts
+++ src/ts/Utilities/RNGTest.ts
@@ -2,14 +2,14 @@ import { Diagnostics } from 'Utilities/Diagnostics';
 
 export class RNGTest {
     public static testId(id: string) {
-        if(RNGTest._previousIds.indexOf(id) !== -1) {
+        if(RNGTest._previousIds[id]) {
             Diagnostics.trigger('duplicate_id', {
                 duplicateId: id
             });
         } else {
-            RNGTest._previousIds.push(id);
+            RNGTest._previousIds[id] = true;
         }
     }
 
-    private static _previousIds: string[] = [];
+    private static _previousIds: { [id: string]: boolean } = {};
 }
