b38d095e0258db9c9ff7c2fefcb43fb9a17e1533
Pekka Palmu <pekka.palmu@gmail.com>
2018-01-22 16:01:30 +0200

    Re-enable tests

--- src/ts/Test/Unit/Utilities/CacheTest.ts
+++ src/ts/Test/Unit/Utilities/CacheTest.ts
@@ -239,7 +239,7 @@ describe('CacheTest', () => {
         });
     });
 
-    xit('Cache one file with repeated network failures (expect to fail)', () => {
+    it('Cache one file with repeated network failures (expect to fail)', () => {
         const testUrl: string = 'http://www.example.net/test.mp4';
         let networkTriggers: number = 0;
 
@@ -260,7 +260,7 @@ describe('CacheTest', () => {
         });
     });
 
-    xit('Stop caching', () => {
+    it('Stop caching', () => {
         const testUrl: string = 'http://www.example.net/test.mp4';
 
         cacheApi.setInternet(false);
