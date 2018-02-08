d8e07f2400c1906d206ab62b97d061a42899e8a8
Egor Danchenkov <icepnyx@gmail.com>
2018-02-08 13:10:56 +0200

    Restore video overlay

--- src/ts/EventHandlers/PerformanceVideoEventHandlers.ts
+++ src/ts/EventHandlers/PerformanceVideoEventHandlers.ts
@@ -1,11 +1,23 @@
 import { PerformanceAdUnit } from 'AdUnits/PerformanceAdUnit';
+import { Overlay } from 'Views/Overlay';
 
 export class PerformanceVideoEventHandlers {
 
     public static onVideoCompleted(adUnit: PerformanceAdUnit) {
-        const endScreen = adUnit.getEndScreen();
-        if (endScreen) {
-            endScreen.show();
+        const overlay = adUnit.getOverlay();
+
+        if (overlay && overlay instanceof Overlay && overlay.getClickedState() && (overlay.getAbGroup() === 8 || overlay.getAbGroup() === 10)) {
+            /* Add .then() to comfort test not sure why...
+            * “before each” hook for “should include all operational events on Android”:
+            * Error: Timeout of 2000ms exceeded. For async tests and hooks, ensure “done()” is called; if returning a Promise, ensure it resolves.
+            * */
+            adUnit.hide().then();
+        } else {
+            const endScreen = adUnit.getEndScreen();
+
+            if (endScreen) {
+                endScreen.show();
+            }
         }
     }
 
