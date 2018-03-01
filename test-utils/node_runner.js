12e3983441ef5b8e223d9f47982bcaba7a83aeba
Ville Orkas <villeo@unity3d.com>
2018-03-01 15:26:29 +0200

    Lets try this again

--- test-utils/node_runner.js
+++ test-utils/node_runner.js
@@ -73,7 +73,7 @@ System.config({
         'text': './node_modules/systemjs-plugin-text/text.js',
         'es6-promise': './node_modules/es6-promise/dist/es6-promise.auto.js',
         'null': './test-utils/null-plugin.js',
-        'long': './node_modules/long/dist/long.js',
+        'long': './node_modules/protobufjs/node_modules/long/dist/long.js',
         'protobufjs/minimal': './node_modules/protobufjs/dist/minimal/protobuf.js'
     },
     meta: {
