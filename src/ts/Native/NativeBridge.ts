be2c86317c215cc9583c1723156ea3d1bc13b443
Pekka Palmu <pekka.palmu@gmail.com>
2018-03-20 15:26:05 +0200

    use new permissions api

--- src/ts/Native/NativeBridge.ts
+++ src/ts/Native/NativeBridge.ts
@@ -26,6 +26,7 @@ import { AndroidPreferencesApi } from 'Native/Api/AndroidPreferences';
 import { IosPreferencesApi } from 'Native/Api/IosPreferences';
 import { SensorInfoApi } from 'Native/Api/SensorInfo';
 import { PurchasingApi } from 'Native/Api/Purchasing';
+import { PermissionsApi } from 'Native/Api/Permissions';
 
 export enum CallbackStatus {
     OK,
@@ -72,6 +73,7 @@ export class NativeBridge implements INativeBridge {
     public VideoPlayer: VideoPlayerApi;
     public UrlScheme: UrlSchemeApi;
     public WebPlayer: WebPlayerApi;
+    public Permissions: PermissionsApi;
 
     private _callbackId: number = 1;
     private _callbackTable: {[key: number]: CallbackContainer<any>} = {};
@@ -118,6 +120,7 @@ export class NativeBridge implements INativeBridge {
         this.VideoPlayer = new VideoPlayerApi(this);
         this.UrlScheme = new UrlSchemeApi(this);
         this.WebPlayer = new WebPlayerApi(this);
+        this.Permissions = new PermissionsApi(this);
     }
 
     public registerCallback<T>(resolve: (value?: T | PromiseLike<T>) => void, reject: (reason?: any) => void): number {
@@ -240,6 +243,12 @@ export class NativeBridge implements INativeBridge {
 
             case EventCategory[EventCategory.WEBPLAYER]:
                 this.WebPlayer.handleEvent(event, parameters);
+                break;
+
+            case EventCategory[EventCategory.PERMISSIONS]:
+                if(this.getPlatform() === Platform.ANDROID) {
+                    this.Permissions.Android.handleEvent(event, parameters);
+                }
                 break;
 
             default:
