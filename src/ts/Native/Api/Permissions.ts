f2ee8a20f8fa8031f493f8ebf2b174a39c14b32a
Pekka Palmu <pekka.palmu@gmail.com>
2018-03-21 17:25:43 +0200

    Use iOS permission Api

--- src/ts/Native/Api/Permissions.ts
+++ src/ts/Native/Api/Permissions.ts
@@ -2,15 +2,19 @@ import { NativeApi } from 'Native/NativeApi';
 import { NativeBridge } from 'Native/NativeBridge';
 import { AndroidPermissionsApi } from 'Native/Api/AndroidPermissions';
 import { Platform } from 'Constants/Platform';
+import { IosPermissionsApi } from 'Native/Api/IosPermissions';
 
 export class PermissionsApi extends NativeApi {
     public Android: AndroidPermissionsApi;
+    public Ios: IosPermissionsApi;
 
     constructor(nativeBridge: NativeBridge) {
         super(nativeBridge, 'Permissions');
 
         if (nativeBridge.getPlatform() === Platform.ANDROID) {
             this.Android = new AndroidPermissionsApi(nativeBridge);
+        } else if(nativeBridge.getPlatform() === Platform.IOS) {
+            this.Ios = new IosPermissionsApi(nativeBridge);
         }
     }
 }
