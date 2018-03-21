3e175ad2315a6bf594208d0f18931bac45203cbf
Pekka Palmu <pekka.palmu@gmail.com>
2018-03-21 13:48:02 +0200

    Add permissions error event

--- src/ts/Native/Api/AndroidPermissions.ts
+++ src/ts/Native/Api/AndroidPermissions.ts
@@ -1,13 +1,15 @@
 import { NativeApi } from 'Native/NativeApi';
 import { NativeBridge } from 'Native/NativeBridge';
-import { Observable3 } from 'Utilities/Observable';
+import { Observable1, Observable3 } from 'Utilities/Observable';
 
 enum PermissionsEvent {
-    PERMISSIONS_RESULT
+    PERMISSIONS_RESULT,
+    PERMISSIONS_ERROR
 }
 
 export class AndroidPermissionsApi extends NativeApi {
     public readonly onPermissionsResult = new Observable3<number, string[], number[]>();
+    public readonly onPermissionsError = new Observable1<string>();
 
     constructor(nativeBridge: NativeBridge) {
         super(nativeBridge, 'Permissions');
@@ -29,6 +31,10 @@ export class AndroidPermissionsApi extends NativeApi {
         switch (event) {
             case PermissionsEvent[PermissionsEvent.PERMISSIONS_RESULT]:
                 this.onPermissionsResult.trigger(parameters[0], parameters[1], parameters[2]);
+                break;
+
+            case PermissionsEvent[PermissionsEvent.PERMISSIONS_ERROR]:
+                this.onPermissionsError.trigger(parameters[0]);
                 break;
 
             default:
