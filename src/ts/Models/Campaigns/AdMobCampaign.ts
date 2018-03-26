67f784f421b7d296c3d14db918c27153d3c1f9a4
Ross Rothenstine <rossr@unity3d.com>
2018-03-25 17:12:57 -0700

    Rudimentary caching for AdMob video

--- src/ts/Models/Campaigns/AdMobCampaign.ts
+++ src/ts/Models/Campaigns/AdMobCampaign.ts
@@ -1,16 +1,49 @@
 import { Campaign, ICampaign } from 'Models/Campaign';
 import { Asset } from 'Models/Assets/Asset';
 import { ProgrammaticCampaign, IProgrammaticCampaign } from 'Models/Campaigns/ProgrammaticCampaign';
+import { Video } from 'Models/Assets/Video';
+import { Model } from '../Model';
+
+export interface IAdMobVideo {
+    mediaFileURL: string;
+    video: Video;
+}
+
+export class AdMobVideo extends Model<IAdMobVideo> {
+    constructor(data: IAdMobVideo) {
+        super('AdMobVideo', {
+            mediaFileURL: ['string'],
+            video: ['object']
+        }, data);
+    }
+
+    public getMediaFileURL(): string {
+        return this.get('mediaFileURL');
+    }
+
+    public getVideo(): Video {
+        return this.get('video');
+    }
+
+    public getDTO() {
+        return {
+            mediaFileUrl: this.getMediaFileURL(),
+            video: this.getVideo().getDTO()
+        };
+    }
+}
 
 export interface IAdMobCampaign extends IProgrammaticCampaign {
     dynamicMarkup: string;
+    video: AdMobVideo | null;
 }
 
 export class AdMobCampaign extends ProgrammaticCampaign<IAdMobCampaign> {
     constructor(campaign: IAdMobCampaign) {
         super('AdMobCampaign', {
             ... ProgrammaticCampaign.Schema,
-            dynamicMarkup: ['string']
+            dynamicMarkup: ['string'],
+            video: ['object', 'null']
         }, campaign);
     }
 
@@ -18,8 +51,17 @@ export class AdMobCampaign extends ProgrammaticCampaign<IAdMobCampaign> {
         return this.get('dynamicMarkup');
     }
 
+    public getVideo(): AdMobVideo | null {
+        return this.get('video');
+    }
+
     public getRequiredAssets(): Asset[] {
-        return [];
+        const assets = [];
+        const video = this.get('video');
+        if (video) {
+            assets.push(video.getVideo());
+        }
+        return assets;
     }
 
     public getOptionalAssets(): Asset[] {
