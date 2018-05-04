d53bb5621c2d6afe49ba8cfdf29cc4af86575882
Shawn Hampton <shawnh@unity3d.com>
2018-05-04 15:59:34 -0700

    Adding error for when video element doesn't exist on page Update to iOS admob caching unit tests

--- src/ts/Test/Unit/Parsers/ProgrammaticAdMobParserTest.ts
+++ src/ts/Test/Unit/Parsers/ProgrammaticAdMobParserTest.ts
@@ -11,6 +11,7 @@ import ValidAdMobCampaign from 'json/campaigns/admob/ValidAdMobCampaign.json';
 import 'mocha';
 import * as sinon from 'sinon';
 import { TestFixtures } from '../TestHelpers/TestFixtures';
+import { FileId } from 'Utilities/FileId';
 
 describe('ProgrammaticAdMobParser', () => {
     const placements = ['TestPlacement'];
@@ -18,22 +19,29 @@ describe('ProgrammaticAdMobParser', () => {
     const mediaId = 'o2YMT0Cmps6xHiOwNMeCrH';
     const correlationId = '583dfda0d933a3630a53249c';
     const abGroup = 0;
+    const url = 'https://r2---sn-n4v7knll.googlevideo.com/videoplayback?id=a6e915b5b0f41a1c&itag=22&source=youtube&requiressl=yes&mm=31&mn=sn-n4v7knll&ms=au&mv=m&pl=19&ei=eo3rWuGXD8-KuAL6oLvQAQ&susc=yti&mime=video/mp4&lmt=1518153041357987&mt=1525386488&ip=4.14.109.2&ipbits=0&expire=1525415418&sparams=ip,ipbits,expire,id,itag,source,requiressl,mm,mn,ms,mv,pl,ei,susc,mime,lmt&signature=4834094C1C09F34DE9D6473658D0B1EE75DB3E10.830B2F45714128B27549A3B15E8BE3CB8EFCBE19&key=ck2';
 
     let parser: ProgrammaticAdMobParser;
     let nativeBridge: NativeBridge;
     let request: Request;
     let session: Session;
+    let setFileIdSpy: sinon.SinonSpy;
 
     beforeEach(() => {
         nativeBridge = sinon.createStubInstance(NativeBridge);
         (<any>nativeBridge.Sdk) = sinon.createStubInstance(SdkApi);
 
         request = sinon.createStubInstance(Request);
-        (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve('http://s3-us-west-1.amazonaws.com/ads-load-testing/AssetPack1/b30-400.mp4'));
+        (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(url));
 
         session = TestFixtures.getSession();
 
         parser = new ProgrammaticAdMobParser();
+        setFileIdSpy = sinon.spy(FileId, 'setFileID');
+    });
+
+    afterEach(() => {
+        setFileIdSpy.restore();
     });
 
     describe('parsing a campaign', () => {
@@ -59,6 +67,7 @@ describe('ProgrammaticAdMobParser', () => {
             describe('on Android', () => {
                 beforeEach(() => {
                     (<sinon.SinonStub>nativeBridge.getPlatform).returns(Platform.ANDROID);
+                    setFileIdSpy.resetHistory();
                     return parse(JSON.parse(ValidAdMobCampaign));
                 });
 
@@ -68,11 +77,17 @@ describe('ProgrammaticAdMobParser', () => {
                 });
 
                 it('should have valid data', validateCampaign);
+
+                it('should FileId.setFileId with a mp4 mime type', () => {
+                    assert.equal(setFileIdSpy.firstCall.args[0], url);
+                    assert.equal(setFileIdSpy.firstCall.args[1], 'G2KkvNWTNuU.mp4'); // videoID
+                });
             });
 
             describe('on iOS', () => {
                 beforeEach(() => {
                     (<sinon.SinonStub>nativeBridge.getPlatform).returns(Platform.IOS);
+                    setFileIdSpy.resetHistory();
                     return parse(JSON.parse(ValidAdMobCampaign));
                 });
 
@@ -82,6 +97,11 @@ describe('ProgrammaticAdMobParser', () => {
                 });
 
                 it('should have valid data', validateCampaign);
+
+                it('should FileId.setFileId with a mp4 mime type', () => {
+                    assert.equal(setFileIdSpy.firstCall.args[0], url);
+                    assert.equal(setFileIdSpy.firstCall.args[1], 'G2KkvNWTNuU.mp4'); // videoID
+                });
             });
         });
     });
