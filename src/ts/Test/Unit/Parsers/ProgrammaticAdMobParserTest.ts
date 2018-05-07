1dae1c7acd6595610932a5ee076536f46b31f304
Shawn Hampton <shawnh@unity3d.com>
2018-05-07 13:18:57 -0700

    Updating unit tests for android exclusion from mime type and for fallback case when mime type is not found in iOS

--- src/ts/Test/Unit/Parsers/ProgrammaticAdMobParserTest.ts
+++ src/ts/Test/Unit/Parsers/ProgrammaticAdMobParserTest.ts
@@ -20,29 +20,13 @@ describe('ProgrammaticAdMobParser', () => {
     const correlationId = '583dfda0d933a3630a53249c';
     const abGroup = 0;
     const url = 'https://r2---sn-n4v7knll.googlevideo.com/videoplayback?id=a6e915b5b0f41a1c&itag=22&source=youtube&requiressl=yes&mm=31&mn=sn-n4v7knll&ms=au&mv=m&pl=19&ei=eo3rWuGXD8-KuAL6oLvQAQ&susc=yti&mime=video/mp4&lmt=1518153041357987&mt=1525386488&ip=4.14.109.2&ipbits=0&expire=1525415418&sparams=ip,ipbits,expire,id,itag,source,requiressl,mm,mn,ms,mv,pl,ei,susc,mime,lmt&signature=4834094C1C09F34DE9D6473658D0B1EE75DB3E10.830B2F45714128B27549A3B15E8BE3CB8EFCBE19&key=ck2';
+    const urlNoMime = 'https://r2---sn-n4v7knll.googlevideo.com/videoplayback?id=a6e915b5b0f41a1c&itag=22&source=youtube&requiressl=yes&mm=31&mn=sn-n4v7knll&ms=au&mv=m&pl=19&ei=eo3rWuGXD8-KuAL6oLvQAQ&susc=yti&lmt=1518153041357987&mt=1525386488&ip=4.14.109.2&ipbits=0&expire=1525415418&sparams=ip,ipbits,expire,id,itag,source,requiressl,mm,mn,ms,mv,pl,ei,susc,mime,lmt&signature=4834094C1C09F34DE9D6473658D0B1EE75DB3E10.830B2F45714128B27549A3B15E8BE3CB8EFCBE19&key=ck2';
 
     let parser: ProgrammaticAdMobParser;
     let nativeBridge: NativeBridge;
     let request: Request;
     let session: Session;
     let setFileIdSpy: sinon.SinonSpy;
-
-    beforeEach(() => {
-        nativeBridge = sinon.createStubInstance(NativeBridge);
-        (<any>nativeBridge.Sdk) = sinon.createStubInstance(SdkApi);
-
-        request = sinon.createStubInstance(Request);
-        (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(url));
-
-        session = TestFixtures.getSession();
-
-        parser = new ProgrammaticAdMobParser();
-        setFileIdSpy = sinon.spy(FileId, 'setFileID');
-    });
-
-    afterEach(() => {
-        setFileIdSpy.restore();
-    });
 
     describe('parsing a campaign', () => {
         let campaign: AdMobCampaign;
@@ -54,7 +38,94 @@ describe('ProgrammaticAdMobParser', () => {
             });
         };
 
+        beforeEach(() => {
+            nativeBridge = sinon.createStubInstance(NativeBridge);
+            (<any>nativeBridge.Sdk) = sinon.createStubInstance(SdkApi);
+
+            request = sinon.createStubInstance(Request);
+
+            session = TestFixtures.getSession();
+
+            parser = new ProgrammaticAdMobParser();
+            setFileIdSpy = sinon.spy(FileId, 'setFileID');
+        });
+
+        afterEach(() => {
+            setFileIdSpy.restore();
+        });
+
+        describe('on Android', () => {
+
+            beforeEach(() => {
+                (<sinon.SinonStub>nativeBridge.getPlatform).returns(Platform.ANDROID);
+                setFileIdSpy.resetHistory();
+            });
+
+            describe('without a mime type in url', () => {
+
+                beforeEach(() => {
+                    (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(urlNoMime));
+                    return parse(JSON.parse(ValidAdMobCampaign));
+                });
+
+                it('should FileId.setFileId without a mp4 mime type', () => {
+                    assert.equal(setFileIdSpy.firstCall.args[0], urlNoMime);
+                    assert.equal(setFileIdSpy.firstCall.args[1], 'G2KkvNWTNuU'); // videoID
+                });
+            });
+
+            describe('with a mime type in url', () => {
+                beforeEach(() => {
+                    (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(url));
+                    return parse(JSON.parse(ValidAdMobCampaign));
+                });
+
+                it('should FileId.setFileId without a mp4 mime type', () => {
+                    assert.equal(setFileIdSpy.firstCall.args[0], url);
+                    assert.equal(setFileIdSpy.firstCall.args[1], 'G2KkvNWTNuU'); // videoID
+                });
+            });
+
+        });
+
+        describe('on iOS', () => {
+
+            beforeEach(() => {
+                (<sinon.SinonStub>nativeBridge.getPlatform).returns(Platform.IOS);
+                setFileIdSpy.resetHistory();
+            });
+
+            describe('without a mime type in url', () => {
+
+                beforeEach(() => {
+                    (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(urlNoMime));
+                    return parse(JSON.parse(ValidAdMobCampaign));
+                });
+
+                it('should not call FileId.setFileId', () => { // when no mime type should default to streaming
+                    assert.isNull(setFileIdSpy.firstCall);
+                    assert.equal(setFileIdSpy.callCount, 0);
+                });
+            });
+
+            describe('with a mime type in url', () => {
+                beforeEach(() => {
+                    (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(url));
+                    return parse(JSON.parse(ValidAdMobCampaign));
+                });
+
+                it('should FileId.setFileId with a mp4 mime type', () => {
+                    assert.equal(setFileIdSpy.firstCall.args[0], url);
+                    assert.equal(setFileIdSpy.firstCall.args[1], 'G2KkvNWTNuU.mp4'); // videoID
+                });
+            });
+        });
+
         describe('with a proper JSON payload', () => {
+
+            beforeEach(() => {
+                (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(url));
+            });
 
             const validateCampaign = () => {
                 const json = JSON.parse(ValidAdMobCampaign);
@@ -78,10 +149,6 @@ describe('ProgrammaticAdMobParser', () => {
 
                 it('should have valid data', validateCampaign);
 
-                it('should FileId.setFileId with a mp4 mime type', () => {
-                    assert.equal(setFileIdSpy.firstCall.args[0], url);
-                    assert.equal(setFileIdSpy.firstCall.args[1], 'G2KkvNWTNuU.mp4'); // videoID
-                });
             });
 
             describe('on iOS', () => {
@@ -98,10 +165,6 @@ describe('ProgrammaticAdMobParser', () => {
 
                 it('should have valid data', validateCampaign);
 
-                it('should FileId.setFileId with a mp4 mime type', () => {
-                    assert.equal(setFileIdSpy.firstCall.args[0], url);
-                    assert.equal(setFileIdSpy.firstCall.args[1], 'G2KkvNWTNuU.mp4'); // videoID
-                });
             });
         });
     });
