fc69179386eddbd61a402c5706f1e7b893d62597
Pekka Palmu <pekka.palmu@gmail.com>
2018-05-08 16:32:56 +0300

    Use VideoState enum

--- src/ts/EventHandlers/IosVideoEventHandler.ts
+++ src/ts/EventHandlers/IosVideoEventHandler.ts
@@ -1,5 +1,6 @@
 import { BaseVideoEventHandler, IVideoEventHandlerParams } from 'EventHandlers/BaseVideoEventHandler';
 import { IIosVideoEventHandler } from 'Native/Api/IosVideoPlayer';
+import { VideoState } from 'AdUnits/VideoAdUnit';
 
 export class IosVideoEventHandler extends BaseVideoEventHandler implements IIosVideoEventHandler {
 
@@ -9,7 +10,7 @@ export class IosVideoEventHandler extends BaseVideoEventHandler implements IIosV
 
     public onLikelyToKeepUp(url: string, likelyToKeepUp: boolean): void {
         const container = this._adUnit.getContainer();
-        if(!container.isPaused() && this._video.hasStarted() && likelyToKeepUp) {
+        if(!container.isPaused() && (this._adUnit.getVideoState() === VideoState.PLAYING || this._adUnit.getVideoState() === VideoState.PAUSED) && likelyToKeepUp) {
             this._nativeBridge.VideoPlayer.play();
         }
     }
