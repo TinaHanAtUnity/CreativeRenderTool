a579da43217a4e9f2b382d21dde82d83e3123bf7
Pekka Palmu <pekka.palmu@gmail.com>
2018-05-16 12:47:38 +0300

    Enable ThirdPartyEventManager to replace template values in url

--- src/ts/Managers/ThirdPartyEventManager.ts
+++ src/ts/Managers/ThirdPartyEventManager.ts
@@ -3,13 +3,13 @@ import { Request, INativeResponse } from 'Utilities/Request';
 import { DiagnosticError } from 'Errors/DiagnosticError';
 import { Analytics } from 'Utilities/Analytics';
 import { RequestError } from 'Errors/RequestError';
-import { Diagnostics } from 'Utilities/Diagnostics';
 import { Url } from 'Utilities/Url';
 
 export class ThirdPartyEventManager {
 
     private _nativeBridge: NativeBridge;
     private _request: Request;
+    private _templateValues: { [id: string]: string } = {};
 
     constructor(nativeBridge: NativeBridge, request: Request) {
         this._nativeBridge = nativeBridge;
@@ -37,6 +37,8 @@ export class ThirdPartyEventManager {
             }
         }
 
+        url = this.getUrl(url);
+
         this._nativeBridge.Sdk.logDebug('Unity Ads third party event: sending ' + event + ' event to ' + url + ' with headers ' + headers + ' (session ' + sessionId + ')');
         return this._request.get(url, headers, {
             retries: 0,
@@ -58,5 +60,26 @@ export class ThirdPartyEventManager {
             }
             return Analytics.trigger('third_party_event_failed', error);
         });
+    }
+
+    public setTemplateValues(templateValues: { [id: string]: string }): void {
+        this._templateValues = templateValues;
+    }
+
+    public setTemplateValue(key: string, value: string): void {
+        this._templateValues[key] = value;
+    }
+
+    private getUrl(url: string): string {
+        if(url) {
+            for(const key in this._templateValues) {
+                if(this._templateValues.hasOwnProperty(key)) {
+                    url = url.replace(key, this._templateValues[key]);
+
+                }
+            }
+        }
+
+        return url;
     }
 }
