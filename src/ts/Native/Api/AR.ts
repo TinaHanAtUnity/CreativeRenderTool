7afcc81cc1aa7d4210546bf9bda80c149073b8cc
Üstün Ergenoglu <ustun@unity3d.com>
2018-05-23 17:11:48 +0300

    Calculate AR frame scaling on the webview side

--- src/ts/Native/Api/AR.ts
+++ src/ts/Native/Api/AR.ts
@@ -1,6 +1,7 @@
 import { NativeApi } from 'Native/NativeApi';
 import { NativeBridge } from 'Native/NativeBridge';
 import { Observable0, Observable1, Observable2 } from 'Utilities/Observable';
+import { ARUtil, IARFrameInfo, IARFrameScale, IARPoint, IARRect } from 'Utilities/ARUtil';
 
 enum AREvent {
     AR_PLANES_ADDED,
@@ -27,6 +28,24 @@ export interface IARRunProperties {
 }
 
 export class ARApi extends NativeApi {
+    private static calculateVideoScale(frameInfo: IARFrameInfo): IARFrameScale {
+        let videoRect: IARRect = {x: 0, y: 0, width: frameInfo.videoSize.width, height: frameInfo.videoSize.height};
+        videoRect = ARUtil.transformRect(videoRect, ARUtil.invertTransform(frameInfo.transform));
+
+        let bottomRight: IARPoint = {x: frameInfo.videoSize.width, y: frameInfo.videoSize.height};
+        bottomRight = ARUtil.transformPoint(bottomRight, frameInfo.transform);
+        const videoBounds: IARRect = {x: 0, y: 0, width: bottomRight.x, height: bottomRight.y};
+        const videoAspectRatio = videoBounds.width / videoBounds.height;
+        const drawableAspectRatio = frameInfo.drawableSize.width / frameInfo.drawableSize.height;
+
+        const scaleX = frameInfo.drawableSize.width / videoRect.width;
+        const scaleY = frameInfo.drawableSize.height / videoRect.height;
+        if (videoAspectRatio < drawableAspectRatio) {
+            return {scaleX: Math.max(scaleX, scaleY), scaleY: Math.max(scaleX, scaleY)};
+        } else {
+            return {scaleX: Math.min(scaleX, scaleY), scaleY: Math.min(scaleX, scaleY)};
+        }
+    }
 
     public readonly onPlanesAdded = new Observable1<string>();
     public readonly onPlanesRemoved = new Observable1<string>();
@@ -75,7 +94,21 @@ export class ARApi extends NativeApi {
     }
 
     public advanceFrame(): Promise<void> {
-        return this._nativeBridge.invoke<void>(this._apiClass, 'advanceFrame');
+        // Get frame info, calculate scaling and then call advanceFrame
+        return this.getFrameInfo().then((frameInfo) => {
+            const scale = ARApi.calculateVideoScale(frameInfo);
+            return this.setFrameScaling(scale).then(
+                () => this._nativeBridge.invoke<void>(this._apiClass, 'advanceFrame')).catch(
+                    (error) => this._nativeBridge.Sdk.logInfo('Cannot set scaling: ' + error));
+        }).catch((error) => this._nativeBridge.Sdk.logInfo('Cannot get frame info: ' + error));
+    }
+
+    public getFrameInfo(): Promise<IARFrameInfo> {
+        return this._nativeBridge.invoke<IARFrameInfo>(this._apiClass, 'getFrameInfo');
+    }
+
+    public setFrameScaling(scale: IARFrameScale): Promise<void> {
+        return this._nativeBridge.invoke<void>(this._apiClass, 'setFrameScaling', [scale]);
     }
 
     public handleEvent(event: string, parameters: any[]): void {
---
+++ src/ts/Utilities/ARUtil.ts
@@ -0,0 +1,86 @@
+export interface IARFrameTransform {
+    a: number;
+    b: number;
+    c: number;
+    d: number;
+    tx: number;
+    ty: number;
+}
+
+export interface IARFrameScale {
+    scaleX: number;
+    scaleY: number;
+    transform?: IARFrameTransform;
+}
+
+export interface IARSize {
+    width: number;
+    height: number;
+}
+
+export interface IARFrameInfo {
+    transform: IARFrameTransform;
+    videoSize: IARSize;
+    drawableSize: IARSize;
+    interfaceOrientation: number;
+}
+
+export interface IARPoint {
+    x: number;
+    y: number;
+}
+
+export interface IARRect {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+}
+
+export class ARUtil {
+    public static invertTransform(transform: IARFrameTransform): IARFrameTransform {
+        const t = transform;
+        const determinant = t.a * t.d - t.b * t.c;
+        if (determinant === 0) {
+            return transform;
+        }
+        const invDet = 1.0 / determinant;
+
+        return {
+            a: t.d * invDet,
+            b: -t.b * invDet,
+            c: -t.c * invDet,
+            d: t.a * invDet,
+            tx: (t.c * t.ty - t.d * t.tx) * invDet,
+            ty: (t.b * t.tx - t.a * t.ty) * invDet,
+        };
+    }
+
+    public static transformPoint(point: IARPoint, t: IARFrameTransform): IARPoint {
+        return {
+            x: t.a * point.x + t.c * point.y + t.tx,
+            y: t.b * point.x + t.d * point.y + t.ty,
+        };
+    }
+
+    public static transformRect(rect: IARRect, t: IARFrameTransform): IARRect {
+        const top = rect.y;
+        const left = rect.x;
+        const right = rect.x + rect.width;
+        const bottom = rect.y + rect.height;
+
+        const topLeft = ARUtil.transformPoint({x: left, y: top}, t);
+        const topRight = ARUtil.transformPoint({x: right, y: top}, t);
+        const bottomLeft = ARUtil.transformPoint({x: left, y: bottom}, t);
+        const bottomRight = ARUtil.transformPoint({x: right, y: bottom}, t);
+
+        const minX = Math.min(Math.min(topLeft.x, topRight.x), Math.min(bottomLeft.x, bottomRight.x));
+        const maxX = Math.max(Math.max(topLeft.x, topRight.x), Math.max(bottomLeft.x, bottomRight.x));
+        const minY = Math.min(Math.min(topLeft.y, topRight.y), Math.min(bottomLeft.y, bottomRight.y));
+        const maxY = Math.max(Math.max(topLeft.y, topRight.y), Math.max(bottomLeft.y, bottomRight.y));
+
+        return {
+            x: minX, y: minY, width: maxX - minX, height: maxY - minY
+        };
+    }
+}
