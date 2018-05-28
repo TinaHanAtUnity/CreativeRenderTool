e844508b4eb561743262933dfe9f70d53ed7c106
Pekka Palmu <pekka.palmu@gmail.com>
2018-05-28 10:54:11 +0300

    Fix tests

--- src/ts/Test/Unit/AdUnits/VastAdUnitTest.ts
+++ src/ts/Test/Unit/AdUnits/VastAdUnitTest.ts
@@ -118,30 +118,6 @@ describe('VastAdUnit', () => {
 
     afterEach(() => sandbox.restore());
 
-    describe('sendTrackingEvent', () => {
-        it('should replace "%ZONE%" in the url with the placement id', () => {
-            const vast = vastCampaign.getVast();
-            const urlTemplate = 'http://foo.biz/%ZONE%/123';
-            sandbox.stub(vast, 'getTrackingEventUrls').returns([ urlTemplate ]);
-            sandbox.stub(thirdPartyEventManager, 'sendEvent').returns(null);
-            vastAdUnit.sendTrackingEvent('eventName', 'sessionId');
-
-            sinon.assert.calledOnce(<sinon.SinonSpy>thirdPartyEventManager.sendEvent);
-            sinon.assert.calledWith(<sinon.SinonSpy>thirdPartyEventManager.sendEvent, 'vast eventName', 'sessionId', 'http://foo.biz/' + placement.getId() + '/123');
-        });
-
-        it('should replace "%SDK_VERSION%" in the url with the SDK version as a query parameter', () => {
-            const urlTemplate = 'http://ads-brand-postback.unityads.unity3d.com/brands/2002/defaultVideoAndPictureZone/%ZONE%/impression/common?adSourceId=2&advertiserDomain=appnexus.com&advertisingTrackingId=49f7acaa-81f2-4887-9f3b-cd124854879c&cc=USD&creativeId=54411305&dealCode=&demandSeatId=1&fillSource=appnexus&floor=0&gamerId=5834bc21b54e3b0100f44c92&gross=0&networkId=&precomputedFloor=0&seatId=958&value=1.01&sdkVersion=%SDK_VERSION%';
-            const vast = vastCampaign.getVast();
-            sandbox.stub(vast, 'getTrackingEventUrls').returns([ urlTemplate ]);
-            sandbox.stub(thirdPartyEventManager, 'sendEvent').returns(null);
-            vastAdUnit.sendTrackingEvent('start', 'sessionId');
-
-            sinon.assert.calledOnce(<sinon.SinonSpy>thirdPartyEventManager.sendEvent);
-            sinon.assert.calledWith(<sinon.SinonSpy>thirdPartyEventManager.sendEvent, 'vast start', 'sessionId', 'http://ads-brand-postback.unityads.unity3d.com/brands/2002/defaultVideoAndPictureZone/' + placement.getId() + '/impression/common?adSourceId=2&advertiserDomain=appnexus.com&advertisingTrackingId=49f7acaa-81f2-4887-9f3b-cd124854879c&cc=USD&creativeId=54411305&dealCode=&demandSeatId=1&fillSource=appnexus&floor=0&gamerId=5834bc21b54e3b0100f44c92&gross=0&networkId=&precomputedFloor=0&seatId=958&value=1.01&sdkVersion=1234');
-        });
-    });
-
     describe('with click through url', () => {
         beforeEach(() => {
             const video = new Video('', TestFixtures.getSession());
