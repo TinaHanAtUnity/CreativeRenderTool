067f9c9a1c7258884f9e68abd49618f626bf74fa
Pekka Palmu <pekka.palmu@gmail.com>
2018-05-30 11:25:43 +0300

    Drop click event url

--- src/ts/Managers/ProgrammaticOperativeEventManager.ts
+++ src/ts/Managers/ProgrammaticOperativeEventManager.ts
@@ -1,21 +1,18 @@
 import { IOperativeEventManagerParams, OperativeEventManager } from 'Managers/OperativeEventManager';
 import { Campaign } from 'Models/Campaign';
-import { Url } from 'Utilities/Url';
 
 export class ProgrammaticOperativeEventManager extends OperativeEventManager {
     public static setTestBaseUrl(baseUrl: string): void {
-        ProgrammaticOperativeEventManager.VideoEventBaseUrl = baseUrl + '/mobile/gamers';
-        ProgrammaticOperativeEventManager.ClickEventBaseUrl = baseUrl + '/mobile/campaigns';
+        ProgrammaticOperativeEventManager.VideoEventBaseUrl = baseUrl + '/events/v2/brand/video';
     }
 
     private static VideoEventBaseUrl: string = 'https://adserver.unityads.unity3d.com/events/v2/brand/video';
-    private static ClickEventBaseUrl: string = 'https://adserver.unityads.unity3d.com/mobile/campaigns';
 
     constructor(params: IOperativeEventManagerParams<Campaign>) {
         super(params);
     }
 
-    protected createVideoEventUrl(type: string): string {
+    protected createVideoEventUrl(type: string): string | undefined {
         return [
             ProgrammaticOperativeEventManager.VideoEventBaseUrl,
             type,
@@ -24,21 +21,7 @@ export class ProgrammaticOperativeEventManager extends OperativeEventManager {
         ].join('/');
     }
 
-    protected createClickEventUrl(): string {
-        let url: string | undefined;
-        let parameters: any;
-
-        url = [
-            ProgrammaticOperativeEventManager.ClickEventBaseUrl,
-            this._campaign.getId(),
-            'click',
-            this._campaign.getGamerId(),
-        ].join('/');
-        parameters = {
-            gameId: this._clientInfo.getGameId(),
-            redirect: false
-        };
-
-        return Url.addParameters(url, parameters);
+    protected createClickEventUrl(): string | undefined {
+        return undefined;
     }
 }
