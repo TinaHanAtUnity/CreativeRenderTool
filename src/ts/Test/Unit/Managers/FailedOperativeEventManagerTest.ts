0d74c0ac9771baaa9106e8dce4792c1204fca9ef
Pekka Palmu <pekka.palmu@gmail.com>
2018-06-12 12:09:24 +0300

    Structure tests better

--- src/ts/Test/Unit/Managers/FailedOperativeEventManagerTest.ts
+++ src/ts/Test/Unit/Managers/FailedOperativeEventManagerTest.ts
@@ -30,101 +30,101 @@ describe('FailedOperativeEventManagerTest', () => {
         request = new Request(nativeBridge, wakeUpManager);
     });
 
-    it('Sending failed event', () => {
-        sinon.stub(request, 'post').callsFake(() => {
-            return Promise.resolve();
-        });
-        sinon.stub(nativeBridge.Storage, 'get').callsFake(() => {
-            return Promise.resolve({url: 'http://test.url', data: '{\"testdata\": \"test\"}'});
-        });
-        sinon.stub(nativeBridge.Storage, 'delete').callsFake(() => {
-            return Promise.resolve();
-        });
-        sinon.stub(nativeBridge.Storage, 'write').callsFake(() => {
-            return Promise.resolve();
-        });
-
-        const manager = new FailedOperativeEventManager('12345', '12345');
-        return manager.sendFailedEvent(nativeBridge, request, true).then(() => {
-            sinon.assert.calledOnce(<sinon.SinonSpy>request.post);
-            sinon.assert.calledWith(<sinon.SinonSpy>request.post, 'http://test.url', '{\"testdata\": \"test\"}');
-            sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.get);
-            sinon.assert.calledWith(<sinon.SinonSpy>nativeBridge.Storage.get, StorageType.PRIVATE, 'session.12345.operative.12345');
-            sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.delete);
-            sinon.assert.calledWith(<sinon.SinonSpy>nativeBridge.Storage.delete, StorageType.PRIVATE, 'session.12345.operative.12345');
-            sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.write);
-        });
-    });
-
-    it('Storing failed event', () => {
-        sinon.stub(nativeBridge.Storage, 'set').callsFake(() => {
-            return Promise.resolve();
-        });
-        sinon.stub(nativeBridge.Storage, 'write').callsFake(() => {
-            return Promise.resolve();
+    describe('Handling failed events', () => {
+        beforeEach(() => {
+            sinon.stub(nativeBridge.Storage, 'getKeys').callsFake(() => {
+                return Promise.resolve(['event1', 'event2']);
+            });
+            sinon.stub(request, 'post').callsFake(() => {
+                return Promise.resolve();
+            });
+            sinon.stub(nativeBridge.Storage, 'get').callsFake(() => {
+                return Promise.resolve({url: 'http://test.url', data: '{\"testdata\": \"test\"}'});
+            });
+            sinon.stub(nativeBridge.Storage, 'delete').callsFake(() => {
+                return Promise.resolve();
+            });
+            sinon.stub(nativeBridge.Storage, 'write').callsFake(() => {
+                return Promise.resolve();
+            });
         });
 
-        const manager = new FailedOperativeEventManager('12345', '12345');
-        return manager.storeFailedEvent(nativeBridge, {test1: 'test1', test2: 'test2'}).then(() => {
-            sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.set);
-            sinon.assert.calledWith(<sinon.SinonSpy>nativeBridge.Storage.set, StorageType.PRIVATE, 'session.12345.operative.12345', {test1: 'test1', test2: 'test2'});
-            sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.write);
-        });
-    });
+        describe('Resending', () => {
+            describe('Performance events', () => {
+                it('Should send single event', () => {
+                    const manager = new FailedOperativeEventManager('12345', '12345');
+                    return manager.sendFailedEvent(nativeBridge, request, true).then(() => {
+                        sinon.assert.calledOnce(<sinon.SinonSpy>request.post);
+                        sinon.assert.calledWith(<sinon.SinonSpy>request.post, 'http://test.url', '{\"testdata\": \"test\"}');
+                        sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.get);
+                        sinon.assert.calledWith(<sinon.SinonSpy>nativeBridge.Storage.get, StorageType.PRIVATE, 'session.12345.operative.12345');
+                        sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.delete);
+                        sinon.assert.calledWith(<sinon.SinonSpy>nativeBridge.Storage.delete, StorageType.PRIVATE, 'session.12345.operative.12345');
+                        sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.write);
+                    });
+                });
 
-    it('Sending failed events', () => {
-        sinon.stub(request, 'post').callsFake(() => {
-            return Promise.resolve();
-        });
-        sinon.stub(nativeBridge.Storage, 'getKeys').callsFake(() => {
-            return Promise.resolve(['event1', 'event2']);
-        });
-        sinon.stub(nativeBridge.Storage, 'get').callsFake(() => {
-            return Promise.resolve({url: 'http://test.url', data: '{testdata: \'test\'}'});
-        });
-        sinon.stub(nativeBridge.Storage, 'delete').callsFake(() => {
-            return Promise.resolve();
-        });
-        sinon.stub(nativeBridge.Storage, 'write').callsFake(() => {
-            return Promise.resolve();
+                it('Should send multiple events', () => {
+                    const manager = new FailedOperativeEventManager('12345');
+                    return manager.sendFailedEvents(nativeBridge, request).then(() => {
+                        sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.getKeys);
+                        sinon.assert.calledTwice(<sinon.SinonSpy>nativeBridge.Storage.get);
+                        sinon.assert.calledTwice(<sinon.SinonSpy>nativeBridge.Storage.delete);
+                        assert.equal((<sinon.SinonSpy>nativeBridge.Storage.delete).getCall(0).args[1], 'session.12345.operative.event1');
+                        assert.equal((<sinon.SinonSpy>nativeBridge.Storage.delete).getCall(1).args[1], 'session.12345.operative.event2');
+                        sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.write);
+                    });
+                });
+
+                it('Should not send event without eventId', () => {
+                    const manager = new FailedOperativeEventManager('12345');
+                    return manager.sendFailedEvent(nativeBridge, request, true).then(() => {
+                        sinon.assert.notCalled(<sinon.SinonSpy>nativeBridge.Storage.get);
+                        sinon.assert.notCalled(<sinon.SinonSpy>request.post);
+                        sinon.assert.notCalled(<sinon.SinonSpy>nativeBridge.Storage.write);
+                        sinon.assert.notCalled(<sinon.SinonSpy>nativeBridge.Storage.delete);
+                    });
+                });
+            });
+
+            describe('Xpromo events', () => {
+                it('Single event', () => {
+                    (<sinon.SinonStub>nativeBridge.Storage.get).restore();
+                    sinon.stub(nativeBridge.Storage, 'get').callsFake(() => {
+                        return Promise.resolve({kafkaType: 'test.kafka.type', data: '{\"testdata\": \"test\"}'});
+                    });
+
+                    HttpKafka.setRequest(request);
+
+                    const manager = new FailedXpromoOperativeEventManager('12345', '12345');
+                    return manager.sendFailedEvent(nativeBridge, request, true).then(() => {
+                        sinon.assert.calledOnce(<sinon.SinonSpy>request.post);
+                        sinon.assert.calledWith(<sinon.SinonSpy>request.post, 'https://httpkafka.unityads.unity3d.com/v1/events');
+                        sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.get);
+                        sinon.assert.calledWith(<sinon.SinonSpy>nativeBridge.Storage.get, StorageType.PRIVATE, 'session.12345.xpromooperative.12345');
+                        sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.delete);
+                        sinon.assert.calledWith(<sinon.SinonSpy>nativeBridge.Storage.delete, StorageType.PRIVATE, 'session.12345.xpromooperative.12345');
+                        sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.write);
+                    });
+                });
+            });
         });
 
-        const manager = new FailedOperativeEventManager('12345', 'none');
-        return manager.sendFailedEvents(nativeBridge, request).then(() => {
-            sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.getKeys);
-            sinon.assert.calledTwice(<sinon.SinonSpy>nativeBridge.Storage.get);
-            sinon.assert.calledTwice(<sinon.SinonSpy>nativeBridge.Storage.delete);
-            assert.equal((<sinon.SinonSpy>nativeBridge.Storage.delete).getCall(0).args[1], 'session.12345.operative.event1');
-            assert.equal((<sinon.SinonSpy>nativeBridge.Storage.delete).getCall(1).args[1], 'session.12345.operative.event2');
-            sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.write);
-        });
-    });
+        describe('Storing', () => {
+            beforeEach(() => {
+                sinon.stub(nativeBridge.Storage, 'set').callsFake(() => {
+                    return Promise.resolve();
+                });
+            });
 
-    it('Sending failed Xpromo events', () => {
-        sinon.stub(request, 'post').callsFake(() => {
-            return Promise.resolve();
-        });
-        sinon.stub(nativeBridge.Storage, 'get').callsFake(() => {
-            return Promise.resolve({kafkaType: 'test.kafka.type', data: '{\"testdata\": \"test\"}'});
-        });
-        sinon.stub(nativeBridge.Storage, 'delete').callsFake(() => {
-            return Promise.resolve();
-        });
-        sinon.stub(nativeBridge.Storage, 'write').callsFake(() => {
-            return Promise.resolve();
-        });
-
-        HttpKafka.setRequest(request);
-
-        const manager = new FailedXpromoOperativeEventManager('12345', '12345');
-        return manager.sendFailedEvent(nativeBridge, request, true).then(() => {
-            sinon.assert.calledOnce(<sinon.SinonSpy>request.post);
-            sinon.assert.calledWith(<sinon.SinonSpy>request.post, 'https://httpkafka.unityads.unity3d.com/v1/events');
-            sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.get);
-            sinon.assert.calledWith(<sinon.SinonSpy>nativeBridge.Storage.get, StorageType.PRIVATE, 'session.12345.xpromooperative.12345');
-            sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.delete);
-            sinon.assert.calledWith(<sinon.SinonSpy>nativeBridge.Storage.delete, StorageType.PRIVATE, 'session.12345.xpromooperative.12345');
-            sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.write);
+            it('Single event', () => {
+                const manager = new FailedOperativeEventManager('12345', '12345');
+                return manager.storeFailedEvent(nativeBridge, {test1: 'test1', test2: 'test2'}).then(() => {
+                    sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.set);
+                    sinon.assert.calledWith(<sinon.SinonSpy>nativeBridge.Storage.set, StorageType.PRIVATE, 'session.12345.operative.12345', {test1: 'test1', test2: 'test2'});
+                    sinon.assert.calledOnce(<sinon.SinonSpy>nativeBridge.Storage.write);
+                });
+            });
         });
     });
 });
