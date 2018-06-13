acf92c3201583c3665d81c23a7b4a3a62209fc6a
Scott Wise <scottw@unity3d.com>
2018-06-13 13:46:21 -0700

    Add richards test gameid

--- src/ts/Utilities/CustomFeatures.ts
+++ src/ts/Utilities/CustomFeatures.ts
@@ -8,7 +8,7 @@ export class CustomFeatures {
     }
 
     public static isMixedPlacementExperiment(gameId: string): boolean {
-        return gameId === '1543512' || gameId === '1003628';
+        return gameId === '1543512' || gameId === '1003628' || gameId === '1543460';
     }
 
     public static isPlayableConfigurationEnabled(originalResourceUrl: string) {
