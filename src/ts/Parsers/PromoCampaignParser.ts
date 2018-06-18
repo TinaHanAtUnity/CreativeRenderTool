2343e687d5d5d32ef1abbf2a8d8ce219e888d513
Scott Wise <scottw@unity3d.com>
2018-06-18 13:27:18 -0700

    Minor change for allowskip in promo

--- src/ts/Parsers/PromoCampaignParser.ts
+++ src/ts/Parsers/PromoCampaignParser.ts
@@ -38,7 +38,7 @@ export class PromoCampaignParser extends CampaignParser {
                         additionalTrackingEvents: response.getTrackingUrls() ? response.getTrackingUrls() : undefined,
                         dynamicMarkup: promoJson.dynamicMarkup,
                         creativeAsset: new HTML(promoJson.creativeUrl, session),
-                        allowSkip: promoJson.allowSkip
+                        allowSkip: false
                     };
 
                     const promoCampaign = new PromoCampaign(promoCampaignParams);
