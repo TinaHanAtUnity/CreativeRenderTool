a0cc2229aa2aeca7a8ca1d6992b116d8ba7199bf
Shawn Hampton <shawnh@unity3d.com>
2018-07-13 11:43:52 -0700

    UADS-849 : TSLINT no-use-before-declare re-enabled
    Changing the order of declaration so that things are declared before they are used.
    ABGroup has a circular dependency that is now fixed.
    
    Unit tests updated as well.
    
    All unit tests pass.

---
+++ src/ts/Test/Unit/Jaeger/JaegerUtilitiesTest.ts
@@ -0,0 +1,49 @@
+import 'mocha';
+import * as sinon from 'sinon';
+import { assert } from 'chai';
+import { JaegerUtilities } from 'Jaeger/JaegerUtilities';
+
+describe('JaegerUtilitiesTest', () => {
+    const stubbedDateTimestamp: number = 3333;
+
+    describe('generate timestamp', () => {
+        beforeEach(() => {
+            sinon.stub(Date, 'now').returns(stubbedDateTimestamp);
+        });
+
+        afterEach(() => {
+            (<sinon.SinonStub>Date.now).restore();
+        });
+
+        it('should return stubbedDateTimestamp * 1000', () => {
+            assert.equal(JaegerUtilities.genTimestamp(), 3333000);
+            assert.equal(JaegerUtilities.genTimestamp(), stubbedDateTimestamp * 1000);
+        });
+
+    });
+
+    describe('on stripQueryAndFragment', () => {
+        const tests: Array<{
+            input: string,
+            output: string
+        }> = [{
+            input: 'http://google.com/test?key=value&hello=world',
+            output: 'http://google.com/test'
+        }, {
+            input: 'http://google.com/test#key=value&hello=world',
+            output: 'http://google.com/test'
+        }, {
+            input: 'http://google.com/test?key=value&hello=world#more=things,4&to=test',
+            output: 'http://google.com/test'
+        }, {
+            input: 'http://google.com/test#more=things,4&to=test?key=value&hello=world',
+            output: 'http://google.com/test'
+        }];
+        tests.forEach((t) => {
+            it(`stripQueryAndFragment should ouput "${t.output}" when input is "${t.input}"`, () => {
+                const urlString = JaegerUtilities.stripQueryAndFragment(t.input);
+                assert.equal(urlString, t.output);
+            });
+        });
+    });
+});
