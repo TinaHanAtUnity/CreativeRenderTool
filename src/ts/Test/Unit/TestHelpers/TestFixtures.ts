641ff4f233571460089bbc91cbfc6ae291eea448
Anna Cho <annac@unity3d.com>
2018-08-01 15:17:19 -0700

    update vast video impression event unit test

--- src/ts/Test/Unit/TestHelpers/TestFixtures.ts
+++ src/ts/Test/Unit/TestHelpers/TestFixtures.ts
@@ -262,7 +262,7 @@ export class TestFixtures {
             useWebViewUserAgentForTracking: false,
             buyerId: 'buyerId',
             trackingUrls: {},
-            impressionUrls: [],
+            impressionUrls: vast.getImpressionUrls(),
             isMoatEnabled: true
         };
     }
