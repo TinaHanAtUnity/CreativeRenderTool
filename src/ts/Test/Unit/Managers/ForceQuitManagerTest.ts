4ea6470ead8f2700698a45c3d6e3ebef951b112c
Tuomas Harju <tuomash@unity3d.com>
2018-08-17 11:34:49 +0300

    fix force_quit deletion

--- src/ts/Test/Unit/Managers/ForceQuitManagerTest.ts
+++ src/ts/Test/Unit/Managers/ForceQuitManagerTest.ts
@@ -83,19 +83,35 @@ describe('ForceQuitManagerTest', () => {
 
     describe('destroyForceQuitKey', () => {
         it('should delete the force quit key', () => {
-            deleteStub.resolves(true);
+            deleteStub.resolves();
+            writeStub.resolves();
             return forceQuitManager.destroyForceQuitKey().then((res) => {
                 sinon.assert.calledOnce(deleteStub);
                 sinon.assert.calledWith(deleteStub, StorageType.PRIVATE, ForceQuitKey);
+                sinon.assert.calledOnce(writeStub);
+                sinon.assert.calledWith(writeStub, StorageType.PRIVATE);
                 assert.isTrue(res, 'Return value did not equal true');
             });
         });
 
-        it('should fail to delete the force quit key', () => {
-            deleteStub.rejects(undefined);
+        it('should return false if deleting the force quit key fails', () => {
+            deleteStub.rejects();
             return forceQuitManager.destroyForceQuitKey().then((res) => {
                 sinon.assert.calledOnce(deleteStub);
                 sinon.assert.calledWith(deleteStub, StorageType.PRIVATE, ForceQuitKey);
+                sinon.assert.notCalled(writeStub);
+                assert.isFalse(res, 'Return value was not false');
+            });
+        });
+
+        it('should return false if committing the force quit key delete fails', () => {
+            deleteStub.resolves();
+            writeStub.rejects();
+            return forceQuitManager.destroyForceQuitKey().then((res) => {
+                sinon.assert.calledOnce(deleteStub);
+                sinon.assert.calledWith(deleteStub, StorageType.PRIVATE, ForceQuitKey);
+                sinon.assert.calledOnce(writeStub);
+                sinon.assert.calledWith(writeStub, StorageType.PRIVATE);
                 assert.isFalse(res, 'Return value was not false');
             });
         });
