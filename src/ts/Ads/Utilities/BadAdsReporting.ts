07976a56d4686814c5230c974431a50324480f90
Andrew Konecny <andrewko@unity3d.com>
2018-09-10 18:28:26 -0700

    Finalize initial set of BadAReasons

--- src/ts/Ads/Utilities/BadAdsReporting.ts
+++ src/ts/Ads/Utilities/BadAdsReporting.ts
@@ -2,13 +2,11 @@ import { Campaign } from 'Ads/Models/Campaign';
 import { Diagnostics } from 'Core/Utilities/Diagnostics';
 
 export enum BadAdReason {
-    BLACK_SCREEN = 'Showing only a black screen',
-    BLACK_SCREEN_PARTIAL = 'Showing a mostly black screen with some surrounding pictures',
-    SOUND_NOT_MUTED = 'Not respecting my device\'s mute status',
-    OFFENSIVE = 'Very offensive to me',
-    VIDEO_NOT_REWARDING = 'Not rewarding me after I watch it',
-    DISLIKE_ADS = 'Annoying because I dislike ads',
-    OTHER = 'Being reported for a different reason than the options above'
+    OFFENSIVE = 'Very offensive',
+    NEVER_STARTED = 'Ad did not start',
+    WONT_END = 'Ad will not close',
+    MALFORMED = 'Ad does not look right',
+    OTHER = 'Other'
 }
 
 export class BadAdsReporting {
