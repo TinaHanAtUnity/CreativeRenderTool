e40f64ff0bd2062ac98a5d0d083bf63f5b0e2a5e
Ville Orkas <villeo@unity3d.com>
2018-10-18 16:59:25 +0300

    Port rest of AdMob tests

---
+++ test/AdMob/AFMABridgeTest.ts
@@ -0,0 +1,160 @@
+import { AFMABridge, AFMAEvents, IAFMAHandler } from 'AdMob/Views/AFMABridge';
+import { Orientation } from 'Ads/AdUnits/Containers/AdUnitContainer';
+import { NativeBridge } from 'Core/Native/Bridge/NativeBridge';
+import 'mocha';
+import * as sinon from 'sinon';
+import { TestFixtures } from 'TestHelpers/TestFixtures';
+import { Backend } from '../../src/ts/Backend/Backend';
+import { ICoreApi } from '../../src/ts/Core/ICore';
+import { Platform } from '../../src/ts/Core/Constants/Platform';
+
+[Platform.ANDROID, Platform.IOS].forEach(platform => {
+    describe('AFMABridge', () => {
+
+        let handler: IAFMAHandler;
+        let afmaBridge: AFMABridge;
+        let iframe: HTMLIFrameElement;
+        let backend: Backend;
+        let nativeBridge: NativeBridge;
+        let core: ICoreApi;
+
+        beforeEach(() => {
+            backend = TestFixtures.getBackend(platform);
+            nativeBridge = TestFixtures.getNativeBridge(platform, backend);
+            core = TestFixtures.getCoreApi(nativeBridge);
+            handler = {
+                onAFMAClose: sinon.spy(),
+                onAFMAOpenURL: sinon.spy(),
+                onAFMADisableBackButton: sinon.spy(),
+                onAFMAClick: sinon.spy(),
+                onAFMAFetchAppStoreOverlay: sinon.spy(),
+                onAFMAForceOrientation: sinon.spy(),
+                onAFMAGrantReward: sinon.spy(),
+                onAFMAOpenInAppStore: sinon.spy(),
+                onAFMAOpenStoreOverlay: sinon.spy(),
+                OnAFMAVideoStart: sinon.spy(),
+                onAFMAResolveOpenableIntents: sinon.spy(),
+                onAFMATrackingEvent: sinon.spy(),
+                onAFMAClickSignalRequest: sinon.spy(),
+                onAFMAUserSeeked: sinon.spy()
+            };
+            afmaBridge = new AFMABridge(core, handler);
+            iframe = document.createElement('iframe');
+            document.body.appendChild(iframe);
+            afmaBridge.connect(iframe);
+        });
+
+        afterEach(() => {
+            document.body.removeChild(iframe);
+            afmaBridge.disconnect();
+        });
+
+        const sendEvent = (e: string, data?: any) => {
+            return () => {
+                return new Promise((res) => {
+                    window.postMessage({
+                        type: 'afma',
+                        event: e,
+                        data: data
+                    }, '*');
+                    setTimeout(res);
+                });
+            };
+        };
+
+        describe('receiving AFMA events', () => {
+            const tests = [{
+                event: AFMAEvents.OPEN_URL,
+                data: {
+                    url: 'unityads.unity3d.com'
+                },
+                verify: (data?: any) => sinon.assert.calledWith(<sinon.SinonSpy>handler.onAFMAOpenURL, data.url)
+            }, {
+                event: AFMAEvents.CLOSE,
+                verify: (data?: any) => sinon.assert.called(<sinon.SinonSpy>handler.onAFMAClose)
+            }, {
+                event: AFMAEvents.FORCE_ORIENTATION,
+                data: {
+                    orientation: 'portrait'
+                },
+                verify: (data?: any) => sinon.assert.calledWith(<sinon.SinonSpy>handler.onAFMAForceOrientation, Orientation.PORTRAIT)
+            }, {
+                event: AFMAEvents.FORCE_ORIENTATION,
+                data: {
+                    orientation: 'landscape'
+                },
+                verify: (data?: any) => sinon.assert.calledWith(<sinon.SinonSpy>handler.onAFMAForceOrientation, Orientation.LANDSCAPE)
+            }, {
+                event: AFMAEvents.VIDEO_START,
+                verify: (data?: any) => sinon.assert.called(<sinon.SinonSpy>handler.OnAFMAVideoStart)
+            }, {
+                event: AFMAEvents.CLICK,
+                data: {
+                    url: 'unityads.unity3d.com'
+                },
+                verify: (data?: any) => sinon.assert.calledWith(<sinon.SinonSpy>handler.onAFMAClick, data.url)
+            }, {
+                event: AFMAEvents.GRANT_REWARD,
+                verify: () => sinon.assert.called(<sinon.SinonSpy>handler.onAFMAGrantReward)
+            }, {
+                event: AFMAEvents.DISABLE_BACK_BUTTON,
+                data: {
+                    disabled: true
+                },
+                verify: (data?: any) => sinon.assert.calledWith(<sinon.SinonSpy>handler.onAFMADisableBackButton, data.disabled)
+            }, {
+                event: AFMAEvents.OPEN_STORE_OVERLAY,
+                data: {
+                    url: 'itunes://com.unity3d.ads'
+                },
+                verify: (data?: any) => sinon.assert.calledWith(<sinon.SinonSpy>handler.onAFMAOpenStoreOverlay, data.url)
+            }, {
+                event: AFMAEvents.OPEN_IN_APP_STORE,
+                data: {
+                    productId: 'com.unity3d.ads',
+                    url: 'itunes://com.unity3d.ads'
+                },
+                verify: (data?: any) => sinon.assert.calledWith(<sinon.SinonSpy>handler.onAFMAOpenInAppStore, data.productId, data.url)
+            }, {
+                event: AFMAEvents.FETCH_APP_STORE_OVERLAY,
+                data: {
+                    productId: 'com.unity3d.ads'
+                },
+                verify: (data?: any) => sinon.assert.calledWith(<sinon.SinonSpy>handler.onAFMAFetchAppStoreOverlay, data.productId)
+            }, {
+                event: AFMAEvents.OPEN_INTENTS_REQUEST,
+                data: {
+                    id: 1,
+                    intents: [{
+                        id: '1',
+                        packageName: 'com.unity3d.ads.foo'
+                    }]
+                },
+                verify: (data?: any) => sinon.assert.calledWith(<sinon.SinonSpy>handler.onAFMAResolveOpenableIntents, data)
+            }, {
+                event: AFMAEvents.TRACKING,
+                data: {
+                    event: 'foo'
+                },
+                verify: (data?: any) => sinon.assert.calledWith(<sinon.SinonSpy>handler.onAFMATrackingEvent, data.event)
+            }, {
+                event: AFMAEvents.GET_CLICK_SIGNAL,
+                data: {
+                    start: {x: 1, y: 1},
+                    end: {x: 2, y: 2}
+                },
+                verify: (data?: any) => sinon.assert.calledWith(<sinon.SinonSpy>handler.onAFMAClickSignalRequest, data)
+            }, {
+                event: AFMAEvents.USER_SEEKED,
+                verify: (data?: any) => sinon.assert.calledWith(<sinon.SinonSpy>handler.onAFMAUserSeeked)
+            }];
+
+            for(const test of tests) {
+                describe(`${test.event} AFMA event`, () => {
+                    beforeEach(sendEvent(test.event, test.data));
+                    it(`should handle the ${test.event} event`, () => test.verify(test.data));
+                });
+            }
+        });
+    });
+});
---
+++ test/AdMob/ProgrammaticAdMobParserTest.ts
@@ -0,0 +1,206 @@
+import { AdMobCampaign } from 'AdMob/Models/AdMobCampaign';
+import { ProgrammaticAdMobParser } from 'AdMob/Parsers/ProgrammaticAdMobParser';
+import { AuctionResponse } from 'Ads/Models/AuctionResponse';
+import { Session } from 'Ads/Models/Session';
+import { assert } from 'chai';
+import { Platform } from 'Core/Constants/Platform';
+import { NativeBridge } from 'Core/Native/Bridge/NativeBridge';
+import { SdkApi } from 'Core/Native/Sdk';
+import { FileId } from 'Core/Utilities/FileId';
+import { RequestManager } from 'Core/Managers/RequestManager';
+import ValidAdMobCampaign from 'json/campaigns/admob/ValidAdMobCampaign.json';
+import 'mocha';
+import * as sinon from 'sinon';
+import { TestFixtures } from 'TestHelpers/TestFixtures';
+import { Backend } from '../../src/ts/Backend/Backend';
+import { ICoreApi } from '../../src/ts/Core/ICore';
+
+[Platform.ANDROID, Platform.IOS].forEach(platform => {
+    describe('ProgrammaticAdMobParser', () => {
+        const placements = ['TestPlacement'];
+        const mediaId = 'o2YMT0Cmps6xHiOwNMeCrH';
+        const correlationId = '583dfda0d933a3630a53249c';
+        const url = 'https://r2---sn-n4v7knll.googlevideo.com/videoplayback?id=a6e915b5b0f41a1c&itag=22&source=youtube&requiressl=yes&mm=31&mn=sn-n4v7knll&ms=au&mv=m&pl=19&ei=eo3rWuGXD8-KuAL6oLvQAQ&susc=yti&mime=video/mp4&lmt=1518153041357987&mt=1525386488&ip=4.14.109.2&ipbits=0&expire=1525415418&sparams=ip,ipbits,expire,id,itag,source,requiressl,mm,mn,ms,mv,pl,ei,susc,mime,lmt&signature=4834094C1C09F34DE9D6473658D0B1EE75DB3E10.830B2F45714128B27549A3B15E8BE3CB8EFCBE19&key=ck2';
+        const urlNoMime = 'https://r2---sn-n4v7knll.googlevideo.com/videoplayback?id=a6e915b5b0f41a1c&itag=22&source=youtube&requiressl=yes&mm=31&mn=sn-n4v7knll&ms=au&mv=m&pl=19&ei=eo3rWuGXD8-KuAL6oLvQAQ&susc=yti&lmt=1518153041357987&mt=1525386488&ip=4.14.109.2&ipbits=0&expire=1525415418&sparams=ip,ipbits,expire,id,itag,source,requiressl,mm,mn,ms,mv,pl,ei,susc,mime,lmt&signature=4834094C1C09F34DE9D6473658D0B1EE75DB3E10.830B2F45714128B27549A3B15E8BE3CB8EFCBE19&key=ck2';
+
+        let parser: ProgrammaticAdMobParser;
+        let backend: Backend;
+        let nativeBridge: NativeBridge;
+        let core: ICoreApi;
+        let request: RequestManager;
+        let session: Session;
+        let setFileIdSpy: sinon.SinonSpy;
+
+        describe('parsing a campaign', () => {
+            let campaign: AdMobCampaign;
+
+            const parse = (data: any) => {
+                const response = new AuctionResponse(placements, data, mediaId, correlationId);
+                return parser.parse(platform, core, request, response, session).then((parsedCampaign) => {
+                    campaign = <AdMobCampaign>parsedCampaign;
+                });
+            };
+
+            beforeEach(() => {
+                backend = TestFixtures.getBackend(platform);
+                nativeBridge = TestFixtures.getNativeBridge(platform, backend);
+                core = TestFixtures.getCoreApi(nativeBridge);
+                (<any>core.Sdk) = sinon.createStubInstance(SdkApi);
+
+                request = sinon.createStubInstance(RequestManager);
+
+                session = TestFixtures.getSession();
+
+                parser = new ProgrammaticAdMobParser();
+                setFileIdSpy = sinon.spy(FileId, 'setFileID');
+                (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(url));
+            });
+
+            afterEach(() => {
+                setFileIdSpy.restore();
+            });
+
+            if(platform === Platform.ANDROID) {
+                describe('on Android', () => {
+
+                    beforeEach(() => {
+                        sinon.stub(nativeBridge, 'getPlatform').returns(Platform.ANDROID);
+                    });
+
+                    describe('without a mime type in url', () => {
+
+                        beforeEach(() => {
+                            (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(urlNoMime));
+                            return parse(JSON.parse(ValidAdMobCampaign));
+                        });
+
+                        it('should FileId.setFileId without a mp4 mime type', () => {
+                            sinon.assert.calledWith(setFileIdSpy, urlNoMime, 'G2KkvNWTNuU');
+                        });
+                    });
+
+                    describe('with a mime type in url', () => {
+                        beforeEach(() => {
+                            (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(url));
+                            return parse(JSON.parse(ValidAdMobCampaign));
+                        });
+
+                        it('should FileId.setFileId without a mp4 mime type', () => {
+                            sinon.assert.calledWith(setFileIdSpy, url, 'G2KkvNWTNuU');
+                        });
+                    });
+
+                    describe('should cache', () => {
+
+                        beforeEach(() => {
+                            (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(url));
+                            return parse(JSON.parse(ValidAdMobCampaign));
+                        });
+
+                        afterEach(() => {
+                            setFileIdSpy.resetHistory();
+                        });
+
+                        it('and call FileId.setFileId', () => {
+                            sinon.assert.calledOnce(setFileIdSpy);
+                            sinon.assert.calledWith(setFileIdSpy, url, 'G2KkvNWTNuU');
+                        });
+                    });
+
+                });
+            }
+
+            if(platform === Platform.IOS) {
+                describe('on iOS', () => {
+
+                    beforeEach(() => {
+                        sinon.stub(nativeBridge, 'getPlatform').returns(Platform.IOS);
+                    });
+
+                    describe('without a mime type in url', () => {
+
+                        beforeEach(() => {
+                            (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(urlNoMime));
+                            return parse(JSON.parse(ValidAdMobCampaign));
+                        });
+
+                        it('should not call FileId.setFileId', () => { // when no mime type should default to streaming
+                            sinon.assert.notCalled(setFileIdSpy);
+                        });
+                    });
+
+                    describe('with a mime type in url', () => {
+                        beforeEach(() => {
+                            (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(url));
+                            return parse(JSON.parse(ValidAdMobCampaign));
+                        });
+
+                        it('should FileId.setFileId with a mp4 mime type', () => {
+                            sinon.assert.calledWith(setFileIdSpy, url, 'G2KkvNWTNuU.mp4');
+                        });
+                    });
+
+                    describe('should cache', () => {
+
+                        beforeEach(() => {
+                            (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(url));
+                            return parse(JSON.parse(ValidAdMobCampaign));
+                        });
+
+                        afterEach(() => {
+                            setFileIdSpy.resetHistory();
+                        });
+
+                        it('and call FileId.setFileId', () => {
+                            sinon.assert.calledOnce(setFileIdSpy);
+                            sinon.assert.calledWith(setFileIdSpy, url, 'G2KkvNWTNuU.mp4');
+                        });
+                    });
+                });
+            }
+
+            describe('with a proper JSON payload', () => {
+
+                const validateCampaign = () => {
+                    const json = JSON.parse(ValidAdMobCampaign);
+                    assert.isNotNull(campaign);
+                    assert.equal(campaign.getDynamicMarkup(), json.content, 'Markup is not equal');
+                    assert.equal(campaign.getSession(), session, 'Session is not equal');
+                    assert.deepEqual(campaign.getTrackingUrls(), json.trackingUrls, 'Tracking URLs are not equal');
+                };
+
+                describe('on Android', () => {
+                    beforeEach(() => {
+                        sinon.stub(nativeBridge, 'getPlatform').returns(Platform.ANDROID);
+                        setFileIdSpy.resetHistory();
+                        return parse(JSON.parse(ValidAdMobCampaign));
+                    });
+
+                    it('should have a video cached from the AdMob ad', () => {
+                        const assets = campaign.getOptionalAssets();
+                        assert.lengthOf(assets, 1, 'Video is not contained within campaign');
+                    });
+
+                    it('should have valid data', validateCampaign);
+
+                });
+
+                describe('on iOS', () => {
+                    beforeEach(() => {
+                        sinon.stub(nativeBridge, 'getPlatform').returns(Platform.IOS);
+                        setFileIdSpy.resetHistory();
+                        return parse(JSON.parse(ValidAdMobCampaign));
+                    });
+
+                    it('should have a video cached from the AdMobAd', () => {
+                        const assets = campaign.getOptionalAssets();
+                        assert.lengthOf(assets, 1, 'Video is not contained within campaign');
+                    });
+
+                    it('should have valid data', validateCampaign);
+
+                });
+            });
+        });
+    });
+});
