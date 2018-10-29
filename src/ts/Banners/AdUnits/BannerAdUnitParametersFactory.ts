b93aa3dd7c68f1d01a39d978729ef068be65b6aa
Ross Rothenstine <rossr@unity3d.com>
2018-10-29 15:09:11 -0700

    Refactor BannerAdUnitParametersFactory to remove unused parameters
    This resolves UADS-1090

--- src/ts/Banners/AdUnits/BannerAdUnitParametersFactory.ts
+++ src/ts/Banners/AdUnits/BannerAdUnitParametersFactory.ts
@@ -18,87 +18,33 @@ import { DeviceInfo } from 'Core/Models/DeviceInfo';
 import { NativeBridge } from 'Core/Native/Bridge/NativeBridge';
 import { Request } from 'Core/Utilities/Request';
 import { StorageBridge } from 'Core/Utilities/StorageBridge';
+import { IBannerAdUnitParameters } from './BannerAdUnit';
+import { BannerCampaign } from 'Banners/Models/BannerCampaign';
 
 export class BannerAdUnitParametersFactory {
 
     private _nativeBridge: NativeBridge;
     private _request: Request;
-    private _metadataManager: MetaDataManager;
-    private _coreConfig: CoreConfiguration;
-    private _adsConfig: AdsConfiguration;
-    private _container: AdUnitContainer;
-    private _deviceInfo: DeviceInfo;
     private _clientInfo: ClientInfo;
-    private _sessionManager: SessionManager;
-    private _focusManager: FocusManager;
-    private _adMobSignalFactory: AdMobSignalFactory;
     private _webPlayerContainer: WebPlayerContainer;
-    private _gdprManager: GdprManager;
-    private _programmaticTrackingService: ProgrammaticTrackingService;
-    private _storageBridge: StorageBridge;
 
-    constructor(nativeBridge: NativeBridge, request: Request, metadataManager: MetaDataManager, coreConfig: CoreConfiguration, adsConfig: AdsConfiguration, container: AdUnitContainer, deviceInfo: DeviceInfo, clientInfo: ClientInfo, sessionManager: SessionManager, focusManager: FocusManager, adMobSignalFactory: AdMobSignalFactory, gdprManager: GdprManager, webPlayerContainer: WebPlayerContainer, programmaticTrackingService: ProgrammaticTrackingService, storageBridge: StorageBridge) {
+    constructor(nativeBridge: NativeBridge, request: Request, clientInfo: ClientInfo, webPlayerContainer: WebPlayerContainer) {
         this._nativeBridge = nativeBridge;
         this._request = request;
-        this._metadataManager = metadataManager;
-        this._coreConfig = coreConfig;
-        this._adsConfig = adsConfig;
-        this._container = container;
-        this._deviceInfo = deviceInfo;
         this._clientInfo = clientInfo;
-        this._sessionManager = sessionManager;
-        this._focusManager = focusManager;
-        this._adMobSignalFactory = adMobSignalFactory;
-        this._gdprManager = gdprManager;
         this._webPlayerContainer = webPlayerContainer;
-        this._programmaticTrackingService = programmaticTrackingService;
-        this._storageBridge = storageBridge;
     }
 
-    public create(campaign: Campaign, placement: Placement, options: any): Promise<IAdUnitParameters<Campaign>> {
-        return Promise.all([
-            this.getDeviceOrientation()
-        ]).then(([orientation]) => {
-            return {
-                forceOrientation: orientation,
-                webPlayerContainer: this._webPlayerContainer,
-                focusManager: this._focusManager,
-                container: this._container,
-                deviceInfo: this._deviceInfo,
-                clientInfo: this._clientInfo,
-                thirdPartyEventManager: new ThirdPartyEventManager(this._nativeBridge, this._request, {
-                    '%ZONE%': placement.getId(),
-                    '%SDK_VERSION%': this._clientInfo.getSdkVersion().toString()
-                }),
-                operativeEventManager: OperativeEventManagerFactory.createOperativeEventManager({
-                    nativeBridge: this._nativeBridge,
-                    request: this._request,
-                    metaDataManager: this._metadataManager,
-                    sessionManager: this._sessionManager,
-                    clientInfo: this._clientInfo,
-                    deviceInfo: this._deviceInfo,
-                    coreConfig: this._coreConfig,
-                    adsConfig: this._adsConfig,
-                    storageBridge: this._storageBridge,
-                    campaign: campaign
-                }),
-                placement: placement,
-                campaign: campaign,
-                coreConfig: this._coreConfig,
-                adsConfig: this._adsConfig,
-                request: this._request,
-                options: options,
-                adMobSignalFactory: this._adMobSignalFactory,
-                gdprManager: this._gdprManager,
-                programmaticTrackingService: this._programmaticTrackingService
-            };
+    public create(campaign: BannerCampaign, placement: Placement, options: any): Promise<IBannerAdUnitParameters> {
+        return Promise.resolve({
+            placement,
+            campaign,
+            clientInfo: this._clientInfo,
+            thirdPartyEventManager: new ThirdPartyEventManager(this._nativeBridge, this._request, {
+                '%ZONE%': placement.getId(),
+                '%SDK_VERSION%': this._clientInfo.getSdkVersion().toString()
+            }),
+            webPlayerContainer: this._webPlayerContainer
         });
-    }
-
-    private getDeviceOrientation(): Promise<Orientation> {
-        return Promise.all([
-            this._deviceInfo.getScreenWidth(),
-            this._deviceInfo.getScreenHeight()
-        ]).then(([width, height]) => width >= height ? Orientation.LANDSCAPE : Orientation.LANDSCAPE);
     }
 }
