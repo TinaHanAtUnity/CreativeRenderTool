939510200b934f796c6f82be3a13750d2e0ff150
Ross Rothenstine <rossr@unity3d.com>
2018-10-30 13:24:30 -0700

    Fix import path

--- src/ts/Banners/AdUnits/BannerAdUnitParametersFactory.ts
+++ src/ts/Banners/AdUnits/BannerAdUnitParametersFactory.ts
@@ -1,25 +1,11 @@
-import { AdMobSignalFactory } from 'AdMob/Utilities/AdMobSignalFactory';
-import { IAdUnitParameters } from 'Ads/AdUnits/AbstractAdUnit';
-import { AdUnitContainer, Orientation } from 'Ads/AdUnits/Containers/AdUnitContainer';
-import { GdprManager } from 'Ads/Managers/GdprManager';
-import { OperativeEventManagerFactory } from 'Ads/Managers/OperativeEventManagerFactory';
-import { SessionManager } from 'Ads/Managers/SessionManager';
 import { ThirdPartyEventManager } from 'Ads/Managers/ThirdPartyEventManager';
-import { AdsConfiguration } from 'Ads/Models/AdsConfiguration';
-import { Campaign } from 'Ads/Models/Campaign';
 import { Placement } from 'Ads/Models/Placement';
-import { ProgrammaticTrackingService } from 'Ads/Utilities/ProgrammaticTrackingService';
 import { WebPlayerContainer } from 'Ads/Utilities/WebPlayer/WebPlayerContainer';
-import { FocusManager } from 'Core/Managers/FocusManager';
-import { MetaDataManager } from 'Core/Managers/MetaDataManager';
+import { BannerCampaign } from 'Banners/Models/BannerCampaign';
 import { ClientInfo } from 'Core/Models/ClientInfo';
-import { CoreConfiguration } from 'Core/Models/CoreConfiguration';
-import { DeviceInfo } from 'Core/Models/DeviceInfo';
 import { NativeBridge } from 'Core/Native/Bridge/NativeBridge';
 import { Request } from 'Core/Utilities/Request';
-import { StorageBridge } from 'Core/Utilities/StorageBridge';
-import { IBannerAdUnitParameters } from './BannerAdUnit';
-import { BannerCampaign } from 'Banners/Models/BannerCampaign';
+import { IBannerAdUnitParameters } from 'Banners/AdUnits/BannerAdUnit';
 
 export class BannerAdUnitParametersFactory {
 
