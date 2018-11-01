ed4bc4782ab884e7b237d93eb3eb2ffa2dbabaf4
Antti Moilanen <anttim@unity3d.com>
2018-11-01 13:46:37 +0200

    Fix broken things after rebase and only limit the test to close icon skip.

--- src/ts/Ads/Views/NewVideoOverlay.ts
+++ src/ts/Ads/Views/NewVideoOverlay.ts
@@ -6,7 +6,7 @@ import { Localization } from 'Core/Utilities/Localization';
 import { Template } from 'Core/Utilities/Template';
 
 import NewVideoOverlayTemplate from 'html/NewVideoOverlay.html';
-import { ExitSkipIconTest, ABGroup, NextSkipIconTest, CloseSkipIconTest } from 'Core/Models/ABGroup';
+import { ABGroup, CloseSkipIconTest } from 'Core/Models/ABGroup';
 
 export class NewVideoOverlay extends AbstractVideoOverlay implements IPrivacyHandler {
     private _localization: Localization;
@@ -141,10 +141,6 @@ export class NewVideoOverlay extends AbstractVideoOverlay implements IPrivacyHan
 
         if (CustomFeatures.isCheetahGame(this._gameId) || CloseSkipIconTest.isValid(this._abGroup)) {
             this._skipButtonElement.classList.add('close-icon-skip');
-        } else if (ExitSkipIconTest.isValid(this._abGroup)) {
-            this._skipButtonElement.classList.add('exit-icon-skip');
-        } else if (NextSkipIconTest.isValid(this._abGroup)) {
-            this._skipButtonElement.classList.add('next-icon-skip');
         } else {
             this._skipButtonElement.classList.add('skip-icon-skip');
         }
