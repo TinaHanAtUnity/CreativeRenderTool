d083a42a98850b419f3a0cf20e0666340dba8a11
Scott Wise <scottw@unity3d.com>
2018-11-06 11:59:05 -0800

    Remove array based testing :D

--- test/Unit/Views/MraidEventBridge/MraidIFrameEventBridgeTest.ts
+++ test/Unit/Views/MraidEventBridge/MraidIFrameEventBridgeTest.ts
@@ -36,64 +36,7 @@ describe('MraidIframeEventBridge', () => {
     });
 
     describe('receiving MRAID events', () => {
-        const tests = [{
-            msg: {
-                type: MRAIDEvents.OPEN,
-                url: 'unityads.unity3d.com'
-            },
-            verify: (msg?: any) => sinon.assert.calledWith(<sinon.SinonSpy>handler.onOpen, msg.url)
-        }, {
-            msg: {
-                type: MRAIDEvents.CLOSE
-            },
-            verify: (msg?: any) => sinon.assert.called(<sinon.SinonSpy>handler.onClose)
-        }, {
-            msg: {
-                type: MRAIDEvents.ORIENTATION,
-                properties: {
-                    allowOrientationChange: true,
-                    forceOrientation: 'portrait'
-                }
-            },
-            verify: (msg?: any) => sinon.assert.calledWith(<sinon.SinonSpy>handler.onSetOrientationProperties, true, Orientation.PORTRAIT)
-        }, {
-            msg: {
-                type: MRAIDEvents.ORIENTATION,
-                properties: {
-                    allowOrientationChange: true,
-                    forceOrientation: 'landscape'
-                }
-            },
-            verify: (msg?: any) => sinon.assert.calledWith(<sinon.SinonSpy>handler.onSetOrientationProperties, true, Orientation.LANDSCAPE)
-        }, {
-            msg: {
-                type: MRAIDEvents.LOADED
-            },
-            verify: (msg?: any) => sinon.assert.called(<sinon.SinonSpy>handler.onLoaded)
-        }, {
-            msg: {
-                type: MRAIDEvents.ANALYTICS_EVENT,
-                event: 'x',
-                eventData: 'y'
-            },
-            verify: (msg?: any) => sinon.assert.calledWith(<sinon.SinonSpy>handler.onAnalyticsEvent, msg.event, msg.eventData)
-        }, {
-            msg: {
-                type: MRAIDEvents.STATE_CHANGE,
-                state: 'test'
-            },
-            verify: (msg?: any) => sinon.assert.calledWith(<sinon.SinonSpy>handler.onStateChange, msg.state)
-        }, {
-            msg: {
-                type: MRAIDEvents.SEND_STATS,
-                totalTime: 20,
-                playTime: 10,
-                frameCount: 200
-            },
-            verify: (msg?: any) => sinon.assert.calledWith(<sinon.SinonSpy>handler.onSendStats, msg.totalTime, msg.playTime, msg.frameCount)
-        }];
-
-        describe(`${tests[0].msg.type} MRAID event`, () => {
+        describe(`${MRAIDEvents.OPEN} MRAID event`, () => {
             const sendEvent = (e: string, data?: any) => {
                 return () => {
                     return new Promise((res) => {
@@ -105,11 +48,13 @@ describe('MraidIframeEventBridge', () => {
                     });
                 };
             };
-            beforeEach(sendEvent(tests[0].msg.type, tests[0].msg.url));
-            it(`should handle the ${tests[0].msg.type} event`, () => tests[0].verify(tests[0].msg));
+            beforeEach(sendEvent(MRAIDEvents.OPEN, 'unityads.unity3d.com'));
+            it(`should handle the Mraid ${MRAIDEvents.OPEN} event`, () => {
+                sinon.assert.calledWith(<sinon.SinonSpy>handler.onOpen, 'unityads.unity3d.com');
+            });
         });
 
-        describe(`${tests[4].msg.type} MRAID event`, () => {
+        describe(`${MRAIDEvents.LOADED} MRAID event`, () => {
             const sendEvent = (e: string, data?: any) => {
                 return () => {
                     return new Promise((res) => {
@@ -120,11 +65,13 @@ describe('MraidIframeEventBridge', () => {
                     });
                 };
             };
-            beforeEach(sendEvent(tests[4].msg.type));
-            it(`should handle the ${tests[4].msg.type} event`, () => tests[4].verify(tests[4].msg));
+            beforeEach(sendEvent(MRAIDEvents.LOADED));
+            it(`should handle the ${MRAIDEvents.LOADED} event`, () => {
+                sinon.assert.called(<sinon.SinonSpy>handler.onLoaded);
+            });
         });
 
-        describe(`${tests[5].msg.type} MRAID event`, () => {
+        describe(`${MRAIDEvents.ANALYTICS_EVENT} MRAID event`, () => {
             const sendEvent = (e: string, event: any, eventData: any) => {
                 return () => {
                     return new Promise((res) => {
@@ -137,11 +84,13 @@ describe('MraidIframeEventBridge', () => {
                     });
                 };
             };
-            beforeEach(sendEvent(tests[5].msg.type, tests[5].msg.event, tests[5].msg.eventData));
-            it(`should handle the ${tests[5].msg.type} event`, () => tests[5].verify(tests[5].msg));
+            beforeEach(sendEvent(MRAIDEvents.ANALYTICS_EVENT, 'x', 'y'));
+            it(`should handle the ${MRAIDEvents.ANALYTICS_EVENT} event`, () => {
+                sinon.assert.calledWith(<sinon.SinonSpy>handler.onAnalyticsEvent, 'x', 'y');
+            });
         });
 
-        describe(`${tests[6].msg.type} MRAID event`, () => {
+        describe(`${MRAIDEvents.STATE_CHANGE} MRAID event`, () => {
             const sendEvent = (e: string, data?: any) => {
                 return () => {
                     return new Promise((res) => {
@@ -153,12 +102,14 @@ describe('MraidIframeEventBridge', () => {
                     });
                 };
             };
-            beforeEach(sendEvent(tests[6].msg.type, tests[6].msg.state));
-            it(`should handle the ${tests[6].msg.type} event`, () => tests[6].verify(tests[6].msg));
+            beforeEach(sendEvent(MRAIDEvents.STATE_CHANGE, 'test'));
+            it(`should handle the ${MRAIDEvents.STATE_CHANGE} event`, () => {
+                sinon.assert.calledWith(<sinon.SinonSpy>handler.onStateChange, 'test');
+            });
         });
 
-        describe(`${tests[7].msg.type} MRAID event`, () => {
-            const sendEvent7 = (e: string, totalTime: any, playTime: any, frameCount: any) => {
+        describe(`${MRAIDEvents.SEND_STATS} MRAID event`, () => {
+            const sendEvent = (e: string, totalTime: any, playTime: any, frameCount: any) => {
                 return () => {
                     return new Promise((res) => {
                         window.postMessage({
@@ -171,11 +122,13 @@ describe('MraidIframeEventBridge', () => {
                     });
                 };
             };
-            beforeEach(sendEvent7(tests[7].msg.type, tests[7].msg.totalTime, tests[7].msg.playTime, tests[7].msg.frameCount));
-            it(`should handle the ${tests[7].msg.type} event`, () => tests[7].verify(tests[7].msg));
+            beforeEach(sendEvent(MRAIDEvents.SEND_STATS, 20, 10, 200));
+            it(`should handle the ${MRAIDEvents.SEND_STATS} event`, () => {
+                sinon.assert.calledWith(<sinon.SinonSpy>handler.onSendStats, 20, 10, 200);
+            });
         });
 
-        describe(`${tests[2].msg.type} MRAID event`, () => {
+        describe(`${MRAIDEvents.ORIENTATION} MRAID event`, () => {
             const sendEvent = (e: string, data?: any) => {
                 return () => {
                     return new Promise((res) => {
@@ -187,11 +140,13 @@ describe('MraidIframeEventBridge', () => {
                     });
                 };
             };
-            beforeEach(sendEvent(tests[2].msg.type, tests[2].msg.properties));
-            it(`should handle the ${tests[2].msg.type} event`, () => tests[2].verify(tests[2].msg));
+            beforeEach(sendEvent(MRAIDEvents.ORIENTATION, {allowOrientationChange: true, forceOrientation: 'portrait'}));
+            it(`should handle the ${MRAIDEvents.ORIENTATION} event`, () => {
+                sinon.assert.calledWith(<sinon.SinonSpy>handler.onSetOrientationProperties, true, Orientation.PORTRAIT);
+            });
         });
 
-        describe(`${tests[3].msg.type} MRAID event`, () => {
+        describe(`${MRAIDEvents.ORIENTATION} MRAID event`, () => {
             const sendEvent = (e: string, data?: any) => {
                 return () => {
                     return new Promise((res) => {
@@ -203,11 +158,13 @@ describe('MraidIframeEventBridge', () => {
                     });
                 };
             };
-            beforeEach(sendEvent(tests[3].msg.type, tests[3].msg.properties));
-            it(`should handle the ${tests[3].msg.type} event`, () => tests[3].verify(tests[3].msg));
+            beforeEach(sendEvent(MRAIDEvents.ORIENTATION, {allowOrientationChange: true, forceOrientation: 'landscape'}));
+            it(`should handle the ${MRAIDEvents.ORIENTATION} event`, () => {
+                sinon.assert.calledWith(<sinon.SinonSpy>handler.onSetOrientationProperties, true, Orientation.LANDSCAPE);
+            });
         });
 
-        describe(`${tests[1].msg.type} MRAID event`, () => {
+        describe(`${MRAIDEvents.CLOSE} MRAID event`, () => {
             const sendEvent = (e: string, data?: any) => {
                 return () => {
                     return new Promise((res) => {
@@ -218,8 +175,10 @@ describe('MraidIframeEventBridge', () => {
                     });
                 };
             };
-            beforeEach(sendEvent(tests[1].msg.type));
-            it(`should handle the ${tests[1].msg.type} event`, () => tests[1].verify(tests[1].msg));
+            beforeEach(sendEvent(MRAIDEvents.CLOSE));
+            it(`should handle the ${MRAIDEvents.CLOSE} event`, () => {
+                sinon.assert.called(<sinon.SinonSpy>handler.onClose);
+            });
         });
     });
 });
