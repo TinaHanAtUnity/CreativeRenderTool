a31d6c8f70780eab340e16f4cb436bd015fe247d
Tuomas Harju <tuomash@unity3d.com>
2018-11-20 16:26:37 +0200

    add GamePrivacy to AdsConfiguration

---
+++ test/Unit/Ads/PrivacyTest.ts
@@ -0,0 +1,24 @@
+import 'mocha';
+import { assert } from 'chai';
+
+import { GamePrivacy, PrivacyMethod } from 'Ads/Models/Privacy';
+
+describe('GamePrivacyTests', () => {
+    it('should be disabled if PrivacyMethod.DISABLED', () => {
+        const gamePrivacy = new GamePrivacy({ method: PrivacyMethod.DISABLED});
+        assert.isFalse(gamePrivacy.isEnabled());
+        assert.equal(gamePrivacy.getVersion(), 0);
+    });
+
+    it('should be disabled if unknown method', () => {
+        const gamePrivacy = new GamePrivacy({ method: 'unknown' });
+        assert.isFalse(gamePrivacy.isEnabled());
+    });
+
+    it('should be enabled if PrivacyMethod.UNITY_CONSENT', () => {
+        const gamePrivacy = new GamePrivacy({ method: PrivacyMethod.UNITY_CONSENT});
+        assert.isTrue(gamePrivacy.isEnabled());
+        assert.equal(gamePrivacy.getMethod(), PrivacyMethod.UNITY_CONSENT);
+        assert.equal(gamePrivacy.getVersion(), 20181106);
+    });
+});
