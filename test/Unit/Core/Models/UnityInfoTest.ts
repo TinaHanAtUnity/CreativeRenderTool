7c9be30c0186fbe386b012765866d689d3f8ee8d
Janne Nikula <jannen@unity3d.com>
2018-11-22 18:16:40 +0200

    More parametrization to tests

--- test/Unit/Core/Models/UnityInfoTest.ts
+++ test/Unit/Core/Models/UnityInfoTest.ts
@@ -1,70 +1,59 @@
 import 'mocha';
 import { assert } from 'chai';
-import { Backend } from 'Backend/Backend';
-import { ICoreApi } from 'Core/ICore';
-import { NativeBridge } from 'Core/Native/Bridge/NativeBridge';
 import { TestFixtures } from 'TestHelpers/TestFixtures';
 import { Platform } from 'Core/Constants/Platform';
 import { UnityInfo } from 'Core/Models/UnityInfo';
 
-describe('UnityInfoTest', () => {
-    const appId: string = 'test.app';
-    const userId: string = '123456acbdef';
-    const sessionId: string = '12345';
+class TestHelper {
+    public static getAnalyticsRunningTest(platform: Platform): Promise<void> {
+        const unityInfo = TestHelper.getUnityInfo(platform, true);
 
-    let backend: Backend;
-    let nativeBridge: NativeBridge;
-    let core: ICoreApi;
-    let unityInfo: UnityInfo;
-
-    describe('on Android', () => {
-        beforeEach(() => {
-            backend = TestFixtures.getBackend(Platform.ANDROID);
-            nativeBridge = TestFixtures.getNativeBridge(Platform.ANDROID, backend);
-            core = TestFixtures.getCoreApi(nativeBridge);
-            unityInfo = new UnityInfo(Platform.ANDROID, core);
+        return unityInfo.fetch(TestHelper._appId).then(() => {
+            assert.equal(unityInfo.getAnalyticsUserId(), TestHelper._userId, 'analytics user id was not successfully fetched (' + platform + ')');
+            assert.equal(unityInfo.getAnalyticsSessionId(), TestHelper._sessionId, 'analytics session id was not successfully fetched (' + platform + ')');
         });
+    }
 
+    public static getAnalyticsNotRunningTest(platform: Platform): Promise<void> {
+        const unityInfo = TestHelper.getUnityInfo(platform, false);
+
+        return unityInfo.fetch(TestHelper._appId).then(() => {
+            assert.isUndefined(unityInfo.getAnalyticsUserId(), 'analytics user id was defined when engine is not running (' + platform + ')');
+            assert.isUndefined(unityInfo.getAnalyticsSessionId(), 'analytics session id was defined when engine is not running (' + platform + ')');
+        });
+    }
+
+    private static _appId: string = 'test.app';
+    private static _userId: string = '123456acbdef';
+    private static _sessionId: string = '12345';
+
+    private static getUnityInfo(platform: Platform, analyticsRunning: boolean): UnityInfo {
+        const backend = TestFixtures.getBackend(platform);
+        backend.Api.Preferences.setUnityEngineRunning(analyticsRunning);
+        const nativeBridge = TestFixtures.getNativeBridge(platform, backend);
+        const core = TestFixtures.getCoreApi(nativeBridge);
+        return new UnityInfo(platform, core);
+    }
+}
+
+describe('UnityInfoTest', () => {
+    describe('on Android', () => {
         it('should return undefined identifiers when Unity engine is not running', () => {
-            backend.Api.Preferences.setUnityEngineRunning(false);
-            return unityInfo.fetch(appId).then(() => {
-                assert.isUndefined(unityInfo.getAnalyticsUserId(), 'analytics user id was defined when engine is not running (Android)');
-                assert.isUndefined(unityInfo.getAnalyticsSessionId(), 'analytics session id was defined when engine is not running (Android)');
-            });
+            return TestHelper.getAnalyticsNotRunningTest(Platform.ANDROID);
         });
 
         it('should return Unity Analytics identifiers when Unity engine is running', () => {
-            backend.Api.Preferences.setUnityEngineRunning(true);
-            return unityInfo.fetch(appId).then(() => {
-                assert.equal(unityInfo.getAnalyticsUserId(), userId, 'analytics user id was not successfully fetched (Android)');
-                assert.equal(unityInfo.getAnalyticsSessionId(), sessionId, 'analytics session id was not successfully fetched (Android)');
-            });
+            return TestHelper.getAnalyticsRunningTest(Platform.ANDROID);
         });
     });
 
     describe('on iOS', () => {
-        beforeEach(() => {
-            backend = TestFixtures.getBackend(Platform.IOS);
-            nativeBridge = TestFixtures.getNativeBridge(Platform.IOS, backend);
-            core = TestFixtures.getCoreApi(nativeBridge);
-            unityInfo = new UnityInfo(Platform.IOS, core);
-        });
-
         it('should return undefined identifiers when Unity engine is not running', () => {
-            backend.Api.Preferences.setUnityEngineRunning(false);
-            return unityInfo.fetch(appId).then(() => {
-                assert.isUndefined(unityInfo.getAnalyticsUserId(), 'analytics user id was defined when engine is not running (iOS)');
-                assert.isUndefined(unityInfo.getAnalyticsSessionId(), 'analytics session id was defined when engine is not running (iOS)');
-            });
+            return TestHelper.getAnalyticsNotRunningTest(Platform.IOS);
         });
 
         it('should return Unity Analytics identifiers when Unity engine is running', () => {
-            backend.Api.Preferences.setUnityEngineRunning(true);
-            return unityInfo.fetch(appId).then(() => {
-                assert.equal(unityInfo.getAnalyticsUserId(), userId, 'analytics user id was not successfully fetched (iOS)');
-                assert.equal(unityInfo.getAnalyticsSessionId(), sessionId, 'analytics session id was not successfully fetched (iOS)');
-            });
+            return TestHelper.getAnalyticsRunningTest(Platform.IOS);
         });
-
     });
 });
