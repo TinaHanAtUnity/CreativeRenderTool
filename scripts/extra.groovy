8d6c495b55dca904c37312225bc7feba2f9d857c
Igor Lunev <iglunev@gmail.com>
2018-11-27 18:22:02 +0200

    Launch tests and get results

--- scripts/extra.groovy
+++ scripts/extra.groovy
@@ -1,5 +1,6 @@
 import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
 
+// TODO: move to sharedLibs
 def waitWebviewDeployed(webviewBranch) {
     timeout(15) {
         def CONFIG_URL = "https://config.unityads.unity3d.com/webview/${webviewBranch}/test/config.json"
@@ -11,137 +12,166 @@ def waitWebviewDeployed(webviewBranch) {
     }
 }
 
-def downloadArtifacts(job) {
-    try {
-        withCredentials([file(credentialsId: 'ADS_SDK_ARTIFACT_BUCKET_KEY', variable: 'BUCKET_KEY')])
-        {
-            sh """#!/bin/bash
-            if [ ! -e ${HOME}/google-cloud-sdk/.installed.dummy ]; then
-              rm -rf ${HOME}/google-cloud-sdk;
-              curl https://sdk.cloud.google.com | bash > /dev/null;
-              touch ${HOME}/google-cloud-sdk/.installed.dummy;
-            fi
-            echo 'source $HOME/google-cloud-sdk/path.bash.inc' > ~/.bashrc
-            source ~/google-cloud-sdk/path.bash.inc
-            gcloud auth activate-service-account --key-file $BUCKET_KEY
-
-            gsutil cp -r gs://unity-ads-jenkins-artifacts/$job ./results/
-            gsutil rm -r gs://unity-ads-jenkins-artifacts/$job
-            """
-        }
-    } catch (e) {
-        echo "Failed to download artifacts from $job due to error: $e"
-    }
-}
-
 def main() {
     commitId = sh(returnStdout: true, script: "git log --format=\"%H\" -n 1").trim()
-    def webviewBranch = env.BRANCH_NAME + '/' + commitId
-    def webviewDeployed = false
+    webviewBranch = env.BRANCH_NAME + '/' + commitId
+
+    //TODO: use if clause below instead before pushing to master
     //if (env.BRANCH_NAME =~ /^PR-/) {
     if (env.BRANCH_NAME =~ /feature\/jenkinsfile/) {
         stage('Wait for webview deployment') {
-            try {
-                waitWebviewDeployed(webviewBranch)
-                webviewDeployed = true
-            } catch (FlowInterruptedException interruptEx) {
-                currentBuild.result = 'ABORTED'
-                echo("Webview '${webviewBranch}' didn\'t get deployed")
-            }
+            parallel (
+                'checkout_helpers': {
+                    dir('sharedLibs') {
+                        checkout(
+                            [$class: 'GitSCM', branches: [[name: 'master']],
+                            userRemoteConfigs: [[credentialsId: 'applifier-readonly-jenkins-bot',
+                            url: 'https://github.com/Applifier/unity-ads-sdk-tests.git']]]
+                        )
+                        script {
+                            sharedLibs = load 'sharedLibs.groovy'
+                        }
+                    }
+                }
+
+                'wait': {
+                    try {
+                        waitWebviewDeployed(webviewBranch)
+                    } catch (FlowInterruptedException interruptEx) {
+                        currentBuild.result = 'ABORTED'
+                        echo("Webview '${webviewBranch}' didn\'t get deployed")
+                    }
+                }
+            )
         }
 
         stage('Run tests') {
-            if (webviewDeployed) {
-                try {
-                    dir('results') {
-                        //TODO: use if clause below instead before pushing to master
-                        //if (env.BRANCH_NAME =~ /^staging/) {
-                        if (env.BRANCH_NAME =~ /feature\/jenkinsfile/) {
-                            parallel (
-                                // run hybrid tests for staging branches
-                                'Android hybrid tests': {
-                                    def _jobName = 'ads-sdk-hybrid-test-android'
-                                    def _build = build(
-                                      job: "Applifier/unity-ads-sdk-tests/$_jobName",
-                                      propagate: false,
-                                      wait: true,
-                                      parameters: [
-                                        string(name: 'WEBVIEW_BRANCH', value: webviewBranch),
-                                      ]
-                                    )
+            try {
+                dir('results') {
+                    //TODO: use if clause below instead before pushing to master
+                    //if (env.BRANCH_NAME =~ /^staging/) {
+                    // run deployment tests
+                    if (env.BRANCH_NAME =~ /feature\/jenkinsfile/) {
+                        //TODO: use the line below before pushing to master
+                        //def nativeBranch = env.BRANCH_NAME.replace("staging/", "");
+                        nativeBranch = "master"
+                        parallel (
+                            'android-hybrid-tests': {
+                                def jobName = 'ads-sdk-hybrid-test-android'
+                                def build_ = build(
+                                  job: "Applifier/unity-ads-sdk-tests/$jobName",
+                                  propagate: false,
+                                  wait: true,
+                                  parameters: [
+                                    string(name: 'WEBVIEW_BRANCH', value: webviewBranch),
+                                  ]
+                                )
 
-                                    def artifactFolder = "$_jobName-$_build.number"
-
-                                    downloadArtifacts(artifactFolder)
-                                },
-
-                                'iOS hybrid tests': {
-                                    def _jobName = 'ads-sdk-hybrid-test-ios'
-                                    def _build = build(
-                                      job: "Applifier/unity-ads-sdk-tests/$_jobName",
-                                      propagate: false,
-                                      wait: true,
-                                      parameters: [
-                                        string(name: 'WEBVIEW_BRANCH', value: webviewBranch),
-                                      ],
-                                    )
-
-                                    def artifactFolder = "$_jobName-$_build.number"
-
-                                    downloadArtifacts(artifactFolder)
-                                },
-                                // run deployment tests for staging branches
-                                // TODO: add iOS system tests
-                                'System tests': {
-                                    //TODO: uncomment the line below before pushing to master
-                                    //def nativeBranch = env.BRANCH_NAME.replace("staging/", "");
-                                    def nativeBranch = "master"
-                                    build(
-                                      job: "Applifier/unity-ads-sdk-tests/ads-sdk-systest-android/",
-                                      propagate: false,
-                                      wait: true,
-                                      parameters: [
-                                        string(name: 'WEBVIEW_BRANCH', value: env.BRANCH_NAME),
-                                        string(name: 'UNITY_ADS_ANDROID_BRANCH', value: nativeBranch),
-                                        booleanParam(name: 'RUN_TD_TESTS', value: false)
-                                      ],
-                                    )
+                                def artifactFolder = "$jobName/$build_.number"
+                                dir(jobName) {
+                                    sharedLibs.downloadFromGcp("$artifactFolder/*")
                                 }
-                            )
-                        } else {
-                            // run hybrid tests for all pull requests
-                            echo "This is a placeholder for running hybrid tests on PR, branch: ${env.BRANCH_NAME}"
-                            // parallel (
-                            //     // run hybrid tests for staging branches
-                            //     'Android hybrid tests': {
-                            //         build(
-                            //           job: "Applifier/unity-ads-sdk-tests/ads-sdk-hybrid-test-android/",
-                            //           propagate: true,
-                            //           wait: true,
-                            //           parameters: [
-                            //             string(name: 'WEBVIEW_BRANCH', value: webviewBranch),
-                            //           ]
-                            //         )
-                            //     },
-                            //
-                            //     'iOS hybrid tests': {
-                            //         build(
-                            //           job: "Applifier/unity-ads-sdk-tests/ads-sdk-hybrid-test-ios/",
-                            //           propagate: true,
-                            //           wait: true,
-                            //           parameters: [
-                            //             string(name: 'WEBVIEW_BRANCH', value: webviewBranch),
-                            //           ],
-                            //         )
-                            //     }
-                            // )
-                        }
+                                sharedLibs.removeFromGcp(artifactFolder)
+                            },
+
+                            'ios-hybrid-tests': {
+                                def jobName = 'ads-sdk-hybrid-test-ios'
+                                def build_ = build(
+                                  job: "Applifier/unity-ads-sdk-tests/$jobName",
+                                  propagate: false,
+                                  wait: true,
+                                  parameters: [
+                                    string(name: 'WEBVIEW_BRANCH', value: webviewBranch),
+                                  ],
+                                )
+
+                                def artifactFolder = "$jobName/$build_.number"
+                                dir(jobName) {
+                                    sharedLibs.downloadFromGcp("$artifactFolder/*")
+                                }
+                                sharedLibs.removeFromGcp(artifactFolder)
+                            },
+
+                            //NOTE: system tests results are not being fetched intentionally
+                            //since tests on real devices can be flaky and should not
+                            //cause any noise on GitHub. Results should instead be checked
+                            //manually
+                            'android-system-tests': {
+                                build(
+                                  job: "Applifier/unity-ads-sdk-tests/ads-sdk-systest-ios/",
+                                  propagate: false,
+                                  wait: false,
+                                  parameters: [
+                                    string(name: 'WEBVIEW_BRANCH', value: env.BRANCH_NAME),
+                                    string(name: 'UNITY_ADS_ANDROID_BRANCH', value: nativeBranch),
+                                    //TODO: enable tests before pushing to master
+                                    booleanParam(name: 'RUN_TD_TESTS', value: false)
+                                  ],
+                                )
+                            }
+
+                            'ios-system-tests': {
+                                build(
+                                  job: "Applifier/unity-ads-sdk-tests/ads-sdk-systest-android/",
+                                  propagate: false,
+                                  wait: false,
+                                  parameters: [
+                                    string(name: 'WEBVIEW_BRANCH', value: env.BRANCH_NAME),
+                                    string(name: 'UNITY_ADS_ANDROID_BRANCH', value: nativeBranch),
+                                    //TODO: enable tests before pushing to master
+                                    booleanParam(name: 'RUN_TD_TESTS', value: false)
+                                  ],
+                                )
+                            }
+                        )
+
+                    // run hybrid tests for all pull requests
+                    } else {
+                        echo "Tests triggered for PR, branch: ${env.BRANCH_NAME}"
+                        parallel (
+                            // run hybrid tests for staging branches
+                            'android-hybrid-tests': {
+                                def jobName = 'ads-sdk-hybrid-test-android'
+                                def build_ = build(
+                                  job: "Applifier/unity-ads-sdk-tests/$jobName",
+                                  propagate: false,
+                                  wait: true,
+                                  parameters: [
+                                    string(name: 'WEBVIEW_BRANCH', value: webviewBranch),
+                                  ]
+                                )
+
+                                def artifactFolder = "$jobName/$build_.number"
+                                dir(jobName) {
+                                    sharedLibs.downloadFromGcp("$artifactFolder/*")
+                                }
+                                sharedLibs.removeFromGcp(artifactFolder)
+                            },
+
+                            'ios-hybrid-tests': {
+                                def jobName = 'ads-sdk-hybrid-test-ios'
+                                def build_ = build(
+                                  job: "Applifier/unity-ads-sdk-tests/$jobName",
+                                  propagate: false,
+                                  wait: true,
+                                  parameters: [
+                                    string(name: 'WEBVIEW_BRANCH', value: webviewBranch),
+                                  ],
+                                )
+
+                                def artifactFolder = "$jobName/$build_.number"
+                                dir(jobName) {
+                                    sharedLibs.downloadFromGcp("$artifactFolder/*")
+                                }
+                                sharedLibs.removeFromGcp(artifactFolder)
+                            }
+                        )
+
                     }
-                } finally {
-                    sh "pwd && ls -la"
-                    archiveArtifacts artifacts: "results/**", fingerprint: true
-                    step ([$class: "JUnitResultArchiver", testResults: "results/**/*.xml"])
                 }
+            } finally {
+                archiveArtifacts artifacts: "results/**", fingerprint: true
+                step ([$class: "JUnitResultArchiver", testResults: "results/**/*.xml"])
             }
         }
     }
