7dead882ebe1074ba835f696326d8caa9793011e
Kalle Manninen <kallem@unity3d.com>
2018-11-28 15:17:25 +0200

    Revert "Merge pull request #7181 from Applifier/feature/AUI-158_GreenButton_AB"
    This reverts commit 051709e3e9493ed308d3bd083748fa83956f5f2f, reversing
    changes made to b6ca4ebbb79bb888b80c1dd6f54c39955465bde0.

--- src/ts/Ads/Views/EndScreen.ts
+++ src/ts/Ads/Views/EndScreen.ts
@@ -6,7 +6,7 @@ import { CustomFeatures } from 'Ads/Utilities/CustomFeatures';
 import { AbstractPrivacy, IPrivacyHandler } from 'Ads/Views/AbstractPrivacy';
 import {
     ABGroup,
-    NativeGreenEndScreenButtonColorTest
+    GreenEndScreenButtonColorTest
 } from 'Core/Models/ABGroup';
 import { Localization } from 'Core/Utilities/Localization';
 import { View } from 'Core/Views/View';
@@ -126,8 +126,8 @@ export abstract class EndScreen extends View<IEndScreenHandler> implements IPriv
     }
 
     private overrideButtonColor(ctaButtonColor: string | undefined): string | undefined {
-        if (NativeGreenEndScreenButtonColorTest.isValid(this._abGroup)) {
-            return '#A4C639';
+        if (GreenEndScreenButtonColorTest.isValid(this._abGroup)) {
+            return '#83CD0C';
         }
         return ctaButtonColor;
     }
