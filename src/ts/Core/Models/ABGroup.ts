21c2e18ea4943d055e8034d1a970555f8f75d7b2
Kalle Manninen <kallem@unity3d.com>
2018-11-29 12:17:08 +0200

    Enabled AB test for Android back button skip

--- src/ts/Core/Models/ABGroup.ts
+++ src/ts/Core/Models/ABGroup.ts
@@ -45,4 +45,4 @@ export const FakeDisabledABTest = new DisabledABTest(16, 17);
 export const FPSCollectionTest = new ABTest(16);
 export const NextSkipIconTest = new ABTest(5, 6);
 export const AuctionV5Test = new DisabledABTest(15);
-export const AndroidBackButtonSkipTest = new DisabledABTest(7, 8);
+export const AndroidBackButtonSkipTest = new ABTest(7, 8);
