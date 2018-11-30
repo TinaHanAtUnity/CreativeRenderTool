ff762e527eeb9f3beb612b1d8a64f1cb8a163dd7
Ville Orkas <villeo@unity3d.com>
2018-11-30 12:43:41 +0200

    Update to TS 3.2

--- src/ts/Core/Utilities/DOMUtils.ts
+++ src/ts/Core/Utilities/DOMUtils.ts
@@ -2,17 +2,17 @@ export class DOMUtils {
 
     // This implementation is taken from https://developer.mozilla.org/en/docs/Web/API/DOMParser
 
-    public static parseFromString(markup: string, type: string): Document {
+    public static parseFromString(markup: string, type: SupportedType): Document {
         if (/^\s*text\/html\s*(?:;|$)/i.test(type)) {
             const doc = document.implementation.createHTMLDocument('');
             if (markup.toLowerCase().indexOf('<!doctype') > -1) {
-                doc.documentElement!.innerHTML = markup;
+                doc.documentElement.innerHTML = markup;
             } else {
                 doc.body.innerHTML = markup;
             }
             return doc;
         } else {
-            return DOMUtils.nativeParse.apply(new DOMParser(), arguments);
+            return DOMUtils.nativeParse.apply(new DOMParser(), [markup, type]);
         }
     }
 
