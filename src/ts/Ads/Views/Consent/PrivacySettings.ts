79716bd487882d6167db3e0586a5bade5524dd35
Mikko Mäntysalmi <mikkoman@unity3d.com>
2018-12-04 16:10:57 +0200

    wip PrivacySettings

--- src/ts/Ads/Views/Consent/PrivacySettings.ts
+++ src/ts/Ads/Views/Consent/PrivacySettings.ts
@@ -5,18 +5,71 @@ import { Platform } from 'Core/Constants/Platform';
 import { Template } from 'Core/Utilities/Template';
 
 import PrivacySettingsTemplate from 'html/consent/PrivacySettings.html';
+import { PrivacyRowItemContainer } from 'Ads/Views/Consent/PrivacyRowItemContainer';
+import { PersonalizationCheckboxGroup } from 'Ads/Views/Consent/PersonalizationCheckboxGroup';
+
+enum ViewState {
+    INITIAL,
+    PERSONALIZATION,
+    DATA,
+    REPORT,
+    BUILD_INFO
+}
 
 export class PrivacySettings extends AbstractPrivacy {
 
     private _gdprManager: GdprManager;
+    private _privacyRowItemContainer: PrivacyRowItemContainer;
+    private _personalizationCheckBoxGroup: PersonalizationCheckboxGroup;
 
-    constructor(platform: Platform, campaign: Campaign,
-                gdprManager: GdprManager, gdprEnabled: boolean,
-                isCoppaCompliant: boolean) {
-        super(platform, isCoppaCompliant, gdprEnabled, 'consent-privacy');
+    constructor(platform: Platform, gdprManager: GdprManager, campaign?: Campaign,
+                gdprEnabled?: boolean,
+                isCoppaCompliant?: boolean) {
+        super(platform, isCoppaCompliant || false, gdprEnabled || false, 'privacy-settings');
 
         this._template = new Template(PrivacySettingsTemplate);
-        this._bindings = [];
+        this._bindings = [
+            {
+                event: 'click',
+                listener: (event: Event) => this.onBackButtonEvent(event),
+                selector: '.back-button'
+            },
+            {
+                event: 'click',
+                listener: (event: Event) => this.onPersonalizationButtonEvent(event),
+                selector: '.personalization-button'
+            },
+            {
+                event: 'click',
+                listener: (event: Event) => this.onViewDataButtonEvent(event),
+                selector: '.view-data-button'
+            },
+            {
+                event: 'click',
+                listener: (event: Event) => this.onReportAdButtonEvent(event),
+                selector: '.report-ad-button'
+            },
+            {
+                event: 'click',
+                listener: (event: Event) => this.onBuildInfoButtonEvent(event),
+                selector: '.build-info-button'
+            }
+        ];
+
+        this._privacyRowItemContainer = new PrivacyRowItemContainer({ platform: platform, gdprManager: gdprManager });
+        this._personalizationCheckBoxGroup = new PersonalizationCheckboxGroup(platform);
+    }
+
+    public render(): void {
+        super.render();
+
+        this._privacyRowItemContainer.render();
+        (<HTMLElement>this._container.querySelector('.info-container')).appendChild(this._privacyRowItemContainer.container());
+
+        this._personalizationCheckBoxGroup.render();
+        (<HTMLElement>this._container.querySelector('.checkbox-group-container')).appendChild(this._personalizationCheckBoxGroup.container());
+
+        this.showView(ViewState.INITIAL);
     }
 
     protected onCloseEvent(event: Event): void {
@@ -28,6 +81,74 @@ export class PrivacySettings extends AbstractPrivacy {
     protected onPrivacyEvent(event: Event): void {
         event.preventDefault();
         this._handlers.forEach(handler => handler.onPrivacy((<HTMLLinkElement>event.target).href));
+    }
+
+    private onBackButtonEvent(event: Event) {
+        event.preventDefault();
+
+        this.showView(ViewState.INITIAL);
+    }
+
+    private onPersonalizationButtonEvent(event: Event) {
+        event.preventDefault();
+
+        this.showView(ViewState.PERSONALIZATION);
+    }
+
+    private onViewDataButtonEvent(event: Event) {
+        event.preventDefault();
+
+        this.showView(ViewState.DATA);
+    }
+
+    private onReportAdButtonEvent(event: Event) {
+        event.preventDefault();
+
+        this.showView(ViewState.REPORT);
+    }
+
+    private onBuildInfoButtonEvent(event: Event) {
+        event.preventDefault();
+
+        this.showView(ViewState.BUILD_INFO);
+    }
+
+    private showView(viewState: ViewState) {
+        let classToAdd: string;
+
+        switch (viewState) {
+            case ViewState.INITIAL:
+                classToAdd = 'initial';
+                break;
+            case ViewState.PERSONALIZATION:
+                classToAdd = 'personalization';
+                break;
+            case ViewState.DATA:
+                classToAdd = 'data';
+                break;
+            case ViewState.REPORT:
+                classToAdd = 'report';
+                break;
+            case ViewState.BUILD_INFO:
+                classToAdd = 'build-info';
+                break;
+            default:
+                classToAdd = 'initial';
+        }
+
+        const states = ['initial', 'personalization', 'data', 'report', 'build-info'];
+        states.forEach(state => {
+            if (state === classToAdd) {
+                this.container().classList.add(classToAdd);
+
+                // todo: needed to set the length of the line connecting main and sub checkboxes
+                if (state === 'personalization') {
+                    this._personalizationCheckBoxGroup.show();
+                }
+            } else {
+                this.container().classList.remove(state);
+            }
+        });
     }
 
 }
