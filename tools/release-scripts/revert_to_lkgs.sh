db634ecbe345dd80e8019b688ec5b52cb3a87598
Andrew Konecny <andrewko@unity3d.com>
2019-01-07 14:13:37 -0800

    Update revert script to use release file

--- tools/release-scripts/revert_to_lkgs.sh
+++ tools/release-scripts/revert_to_lkgs.sh
@@ -14,15 +14,16 @@ fi
 echo "Proceeding in five seconds."
 sleep 5
 
-releases=$(git tag -l | grep LKG | grep -v ios | grep -v '2.2.0-beta1' | grep -v '2.2.1-preview.1' | sed 's/LKG_//')
+releases="release-scripts/releases.txt"
 
 git pull --tags
-for release in $releases; do
+while IFS= read -r release
+    do
     git checkout $release
     git reset --hard "LKG_$release"
     git push -f
     sleep 2
-done
+done <"$releases"
 
 curl -X POST -H 'Content-type: application/json' --data '{"text":"The latest Ads SDK Webview Deployment has been manually reverted. :slightly_frowning_face:"}' https://hooks.slack.com/services/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
 curl -X POST -H 'Content-type: application/json' --data '{"text":"The latest Ads SDK Webview Deployment has been manually reverted. :slightly_frowning_face:"}' https://hooks.slack.com/services/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
