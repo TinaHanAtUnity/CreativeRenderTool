f31698ad2507d0e28cf526d4b8b44223d0f5674a
Andrew Konecny <andrewko@unity3d.com>
2019-01-14 11:03:05 -0800

    Enable auction v5 on group 15

--- src/ts/Core/Models/ABGroup.ts
+++ src/ts/Core/Models/ABGroup.ts
@@ -43,6 +43,6 @@ export const FakeDisabledABTest = new DisabledABTest(16, 17);
 
 // Add actual A/B tests below
 
-export const AuctionV5Test = new DisabledABTest(15, 16, 17);
+export const AuctionV5Test = new ABTest(15);
 export const VastParsingStrictTest = new DisabledABTest(); // Testing new vast parser logic
 export const ByteDanceCTATest = new ABTest(17);
