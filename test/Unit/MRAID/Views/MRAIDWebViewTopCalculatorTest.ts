68cac8e5d67a7dbb586602e328d884ae20a86418
Scott Wise <scottw@unity3d.com>
2019-01-15 11:23:53 -0800

    clean up tests

--- test/Unit/MRAID/Views/MRAIDWebViewTopCalculatorTest.ts
+++ test/Unit/MRAID/Views/MRAIDWebViewTopCalculatorTest.ts
@@ -3,97 +3,88 @@ import { assert } from 'chai';
 import { Platform } from 'Core/Constants/Platform';
 import { TestFixtures } from 'TestHelpers/TestFixtures';
 import { MRAIDWebViewTopCalculator } from 'MRAID/Views/MRAIDWebViewTopCalculator';
-import { AndroidDeviceInfo } from 'Core/Models/AndroidDeviceInfo';
-import { IosDeviceInfo } from 'Core/Models/IosDeviceInfo';
 
 describe('MRAIDWebViewTopCalculator', () => {
-    let platform: Platform;
+    describe('When device is Android', () => {
+        let mraidViewSizer: MRAIDWebViewTopCalculator;
 
-    describe('Getting Top position', () => {
-        describe('When device is Android', () => {
-            let mraidViewSizer: MRAIDWebViewTopCalculator;
-            let deviceInfo: AndroidDeviceInfo;
+        beforeEach(() => {
+            const platform = Platform.ANDROID;
+            const backend = TestFixtures.getBackend(platform);
+            const nativeBridge = TestFixtures.getNativeBridge(platform, backend);
+            const core = TestFixtures.getCoreApi(nativeBridge);
 
-            beforeEach(() => {
-                platform = Platform.ANDROID;
-                const backend = TestFixtures.getBackend(platform);
-                const nativeBridge = TestFixtures.getNativeBridge(platform, backend);
-                const core = TestFixtures.getCoreApi(nativeBridge);
-
-                deviceInfo = TestFixtures.getAndroidDeviceInfo(core);
-                mraidViewSizer = new MRAIDWebViewTopCalculator(deviceInfo, platform);
-            });
-
-            it('should scale for landscape', () => {
-                const width = 800;
-                const height = 600;
-
-                assert.equal(mraidViewSizer.getTopPosition(width, height), 48);
-            });
-
-            it('should scale for portrait', () => {
-                const width = 600;
-                const height = 800;
-
-                assert.equal(mraidViewSizer.getTopPosition(width, height), 40);
-            });
+            const deviceInfo = TestFixtures.getAndroidDeviceInfo(core);
+            mraidViewSizer = new MRAIDWebViewTopCalculator(deviceInfo, platform);
         });
 
-        xdescribe('When device is iPhone', () => {
-            let mraidViewSizer: MRAIDWebViewTopCalculator;
-            let deviceInfo: IosDeviceInfo;
+        it('should scale for landscape', () => {
+            const width = 800;
+            const height = 600;
 
-            beforeEach(() => {
-                platform = Platform.IOS;
-                const backend = TestFixtures.getBackend(platform);
-                const nativeBridge = TestFixtures.getNativeBridge(platform, backend);
-                const core = TestFixtures.getCoreApi(nativeBridge);
-
-                deviceInfo = TestFixtures.getIosDeviceInfo(core);
-                mraidViewSizer = new MRAIDWebViewTopCalculator(deviceInfo, platform);
-            });
-
-            it('should scale for landscape', () => {
-                const width = 800;
-                const height = 600;
-
-                assert.equal(mraidViewSizer.getTopPosition(width, height), 66);
-            });
-            it('should scale for portrait', () => {
-                const width = 600;
-                const height = 800;
-
-                assert.equal(mraidViewSizer.getTopPosition(width, height), 48);
-            });
+            assert.equal(mraidViewSizer.getTopPosition(width, height), 48);
         });
 
-        xdescribe('When device is iPhoneX', () => {
-            let mraidViewSizer: MRAIDWebViewTopCalculator;
-            let deviceInfo: IosDeviceInfo;
+        it('should scale for portrait', () => {
+            const width = 600;
+            const height = 800;
 
-            beforeEach(() => {
-                platform = Platform.IOS;
-                const backend = TestFixtures.getBackend(platform);
-                const nativeBridge = TestFixtures.getNativeBridge(platform, backend);
-                const core = TestFixtures.getCoreApi(nativeBridge);
+            assert.equal(mraidViewSizer.getTopPosition(width, height), 40);
+        });
+    });
 
-                deviceInfo = TestFixtures.getIosDeviceInfo(core);
-                mraidViewSizer = new MRAIDWebViewTopCalculator(deviceInfo, platform);
-            });
+    describe('When device is iPhone', () => {
+        let mraidViewSizer: MRAIDWebViewTopCalculator;
 
-            it('should scale for landscape', () => {
-                const width = 812;
-                const height = 375;
+        beforeEach(() => {
+            const platform = Platform.IOS;
+            const backend = TestFixtures.getBackend(platform);
+            const nativeBridge = TestFixtures.getNativeBridge(platform, backend);
+            const core = TestFixtures.getCoreApi(nativeBridge);
 
-                assert.equal(mraidViewSizer.getTopPosition(width, height), 41.25);
-            });
+            const deviceInfo = TestFixtures.getIosDeviceInfo(core);
+            mraidViewSizer = new MRAIDWebViewTopCalculator(deviceInfo, platform);
+        });
 
-            it('should scale for portrait', () => {
-                const width = 375;
-                const height = 812;
+        it('should scale for landscape', () => {
+            const width = 800;
+            const height = 600;
 
-                assert.equal(Math.round(mraidViewSizer.getTopPosition(width, height)), 89);
-            });
+            assert.equal(mraidViewSizer.getTopPosition(width, height), 66);
+        });
+        it('should scale for portrait', () => {
+            const width = 600;
+            const height = 800;
+
+            assert.equal(mraidViewSizer.getTopPosition(width, height), 48);
+        });
+    });
+
+    describe('When device is iPhoneX', () => {
+        let mraidViewSizer: MRAIDWebViewTopCalculator;
+
+        beforeEach(() => {
+            const platform = Platform.IOS;
+            const backend = TestFixtures.getBackend(platform);
+            const nativeBridge = TestFixtures.getNativeBridge(platform, backend);
+            const core = TestFixtures.getCoreApi(nativeBridge);
+
+            const deviceInfo = TestFixtures.getIosDeviceInfo(core);
+            mraidViewSizer = new MRAIDWebViewTopCalculator(deviceInfo, platform);
+        });
+
+        it('should scale for landscape', () => {
+            const width = 812;
+            const height = 375;
+
+            assert.equal(mraidViewSizer.getTopPosition(width, height), 41.25);
+        });
+
+        it('should scale for portrait', () => {
+            const width = 375;
+            const height = 812;
+
+            assert.equal(Math.round(mraidViewSizer.getTopPosition(width, height)), 89);
         });
     });
 });
