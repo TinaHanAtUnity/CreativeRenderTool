ec3c3d4d25befa5c78d77b8241bfac5902a911c7
Anna Cho <annac@unity3d.com>
2019-01-30 15:31:42 -0800

    empty string check of urls in strict parser

--- src/ts/VAST/Utilities/VastParserStrict.ts
+++ src/ts/VAST/Utilities/VastParserStrict.ts
@@ -309,18 +309,22 @@ export class VastParserStrict {
         const clickThroughElement = this.getFirstNodeWithName(creativeElement, VastNodeName.CLICK_THROUGH);
         if (clickThroughElement) {
             const url = this.parseNodeText(clickThroughElement);
-            creative.setVideoClickThroughURLTemplate(url);
+            if (url && url.length > 0) {
+                creative.setVideoClickThroughURLTemplate(url);
+            }
         }
 
         this.getNodesWithName(creativeElement, VastNodeName.CLICK_TRACKING).forEach((element: HTMLElement) => {
             const url = this.parseNodeText(element);
-            creative.addVideoClickTrackingURLTemplate(url);
+            if (url && url.length > 0) {
+                creative.addVideoClickTrackingURLTemplate(url);
+            }
         });
 
         this.getNodesWithName(creativeElement, VastNodeName.TRACKING).forEach((element: HTMLElement) => {
             const url = this.parseNodeText(element);
             const eventName = element.getAttribute(VastAttributeNames.EVENT);
-            if (eventName) {
+            if (eventName && url && url.length > 0) {
                 creative.addTrackingEvent(eventName, url);
             }
         });
@@ -380,12 +384,15 @@ export class VastParserStrict {
 
         const companionClickThroughElement = this.getFirstNodeWithName(companionAdElement, VastNodeName.COMPANION_CLICK_THROUGH);
         if (companionClickThroughElement) {
-            companionAd.setCompanionClickThroughURLTemplate(this.parseNodeText(companionClickThroughElement));
+            const companionClickThroughUrl = this.parseNodeText(companionClickThroughElement);
+            if (companionClickThroughUrl && companionClickThroughUrl.length > 0) {
+                companionAd.setCompanionClickThroughURLTemplate(companionClickThroughUrl);
+            }
         }
 
         this.getNodesWithName(companionAdElement, VastNodeName.COMPANION_CLICK_TRACKING).forEach((element: HTMLElement) => {
             const companionClickTrackingUrl = this.parseNodeText(element);
-            if (companionClickTrackingUrl) {
+            if (companionClickTrackingUrl && companionClickTrackingUrl.length > 0) {
                 companionAd.addCompanionClickTrackingURLTemplate(companionClickTrackingUrl);
             }
         });
