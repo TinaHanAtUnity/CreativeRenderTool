3b72149821e79fc7302a1c5aeee972443b5bcc3c
Andrew Konecny <andrewko@unity3d.com>
2019-02-01 14:05:16 -0800

    flub 2

--- tools/release-scripts/Deployment/update_lkgs.sh
+++ tools/release-scripts/Deployment/update_lkgs.sh
@@ -48,10 +48,10 @@ EOF
 )
 
     # ads-sdk slack channel
-    curl -X POST -H 'Content-type: application/json' --data $slackjson https://hooks.slack.com/services/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
+    curl -X POST -H 'Content-type: application/json' --data "$slackjson" https://hooks.slack.com/services/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
 
     # ads-deploys slack channel
-    curl -X POST -H 'Content-type: application/json' --data $slackjson https://hooks.slack.com/services/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
+    curl -X POST -H 'Content-type: application/json' --data "$slackjson" https://hooks.slack.com/services/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
 
     git checkout master
     ./tools/fire_deploy_json.sh
