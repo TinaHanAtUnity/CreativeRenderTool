fb385d3cda510c43c125f3c103f1340d3d46dab7
Janne Nikula <jannen@unity3d.com>
2019-02-12 20:02:32 +0200

    Add types to iOS native API objects

--- src/ts/Store/Native/iOS/Products.ts
+++ src/ts/Store/Native/iOS/Products.ts
@@ -9,10 +9,39 @@ enum IosStoreEvent {
     RECEIVED_TRANSACTION
 }
 
+export interface IApplePayment {
+    productId: string;
+    numberOfItems: number;
+}
+
+export interface IAppleTransaction {
+    productId: string;
+    transactionState: number;
+    transactionDate: string;
+    transactionIdentifier: string;
+    hasOriginalTransaction: boolean;
+    receipt: string;
+    payment: IApplePayment;
+}
+
+export interface IApplePriceLocale {
+    countryCode: string;
+    currencyCode: string;
+    currencySymbol: string;
+}
+
+export interface IAppleProduct {
+    downloadable: boolean;
+    localizedTitle: string;
+    localizedDescription: string;
+    price: number;
+    priceLocale: IApplePriceLocale;
+}
+
 export class ProductsApi extends NativeApi {
     public readonly onProductRequestErrorNoProducts = new Observable1<number>();
-    public readonly onProductRequestComplete = new Observable2<number, unknown>(); // todo: typing
-    public readonly onTransaction = new Observable1<unknown>(); // todo: typing
+    public readonly onProductRequestComplete = new Observable2<number, { [productId: string]: IAppleProduct }>();
+    public readonly onTransaction = new Observable1<IAppleTransaction[]>();
 
     constructor(nativeBridge: NativeBridge) {
         super(nativeBridge, 'Products', ApiPackage.STORE, EventCategory.STORE);
@@ -37,7 +66,7 @@ export class ProductsApi extends NativeApi {
     public handleEvent(event: string, parameters: unknown[]): void {
         switch(event) {
             case IosStoreEvent[IosStoreEvent.PRODUCT_REQUEST_COMPLETE]:
-                this.onProductRequestComplete.trigger(<number>parameters[0], parameters[1]);
+                this.onProductRequestComplete.trigger(<number>parameters[0], <{ [productId: string]: IAppleProduct }>parameters[1]);
                 break;
 
             case IosStoreEvent[IosStoreEvent.PRODUCT_REQUEST_ERROR_NO_PRODUCTS]:
@@ -45,7 +74,7 @@ export class ProductsApi extends NativeApi {
                 break;
 
             case IosStoreEvent[IosStoreEvent.RECEIVED_TRANSACTION]:
-                this.onTransaction.trigger(parameters[0]);
+                this.onTransaction.trigger(<IAppleTransaction[]>parameters[0]);
                 break;
 
             default:
