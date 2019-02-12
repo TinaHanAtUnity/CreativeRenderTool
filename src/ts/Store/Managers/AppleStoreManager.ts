9686e5279c6dfdb26dc97fdfea84c2c6fa5997f6
Janne Nikula <jannen@unity3d.com>
2019-02-12 21:10:20 +0200

    Send iOS store transactions to SDK diagnostics for testing

--- src/ts/Store/Managers/AppleStoreManager.ts
+++ src/ts/Store/Managers/AppleStoreManager.ts
@@ -1,19 +1,45 @@
 import { StoreManager } from 'Store/Managers/StoreManager';
 import { ICore } from 'Core/ICore';
 import { IStoreApi } from 'Store/IStore';
+import { AppleProduct } from 'Store/Utilities/AppleProduct';
+import { IAppleTransaction } from 'Store/Native/iOS/Products';
+import { StoreTransaction } from 'Store/Models/StoreTransaction';
 
 export class AppleStoreManager extends StoreManager {
+    private _appleProduct: AppleProduct;
+
     constructor(core: ICore, store: IStoreApi) {
         super(core, store);
+
+        this._appleProduct = new AppleProduct(store);
     }
 
     public startTracking(): void {
-        this._store.iOS!.Products.onTransaction.subscribe((data: unknown) => this.onTransaction(data));
+        this._store.iOS!.Products.onTransaction.subscribe((data) => this.onTransaction(data));
 
         this._store.iOS!.Products.startTransactionObserver();
     }
 
-    private onTransaction(data: unknown): void {
-        this._core.Api.Sdk.logInfo('IOS TRANSACTION: ' + JSON.stringify(data));
+    private onTransaction(data: IAppleTransaction[]): void {
+        this._core.Api.Sdk.logInfo('IOS TRANSACTION: ' + JSON.stringify(data)); // todo: remove debug logging before merging to master
+
+        for(const transaction of data) {
+            this.processTransaction(transaction);
+        }
+    }
+
+    private processTransaction(transaction: IAppleTransaction) {
+        const timestamp = Date.now();
+
+        // todo: add some error handling
+        if(transaction.productId && transaction.receipt) {
+            this._appleProduct.getProductInfo(transaction.productId).then(product => {
+                if(product.price && product.priceLocale.currencyCode) {
+                    const storeTransaction = new StoreTransaction(timestamp, transaction.productId, product.price, product.priceLocale.currencyCode, transaction.receipt);
+
+                    this.sendTransaction(storeTransaction);
+                }
+            });
+        }
     }
 }
