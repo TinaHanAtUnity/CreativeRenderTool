3412329fdda971efd02afe59a6b8ac79b165b503
Andrew Konecny <andrewko@unity3d.com>
2019-02-20 10:23:56 -0800

    Add group 16 to the experiment

--- src/ts/Core/Models/ABGroup.ts
+++ src/ts/Core/Models/ABGroup.ts
@@ -42,7 +42,7 @@ export const FakeEnabledABTest = new ABTest(16, 17);
 export const FakeDisabledABTest = new DisabledABTest(16, 17);
 
 // Add actual A/B tests below
-export const AuctionV5Test = new ABTest(15);
+export const AuctionV5Test = new ABTest(15, 16);
 export const VastParsingStrictTest = new DisabledABTest();
 export const WebPlayerMRAIDTest = new DisabledABTest();
 export const CTAClickHandlingTest = new ABTest(17);
