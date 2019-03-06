f53d963b21db4905b0d10d66e91f18a09f4c148e
Antti Moilanen <anttim@unity3d.com>
2019-03-06 14:11:12 +0200

    Release install now button for rewarded ads.
    This puts out the 'Install Now' button shown in video overlay in rewarded ads out for the full network except two groups 5 and 6. We want to track the performance of the feature bit longer and compare it to the two groups that doesn't have it.

--- src/ts/Core/Models/ABGroup.ts
+++ src/ts/Core/Models/ABGroup.ts
@@ -45,4 +45,6 @@ export const FakeDisabledABTest = new DisabledABTest(16, 17);
 export const AuctionV5Test = new ABTest(15, 18);
 export const VastParsingStrictTest = new ABTest(17);
 export const WebPlayerMRAIDTest = new DisabledABTest();
-export const InstallInRewardedVideos = new ABTest(7, 8);
+
+// Two hold out groups that should not get the install now button in rewarded videos
+export const HoldOutInstallInRewardedVideos = new ABTest(5, 6);
