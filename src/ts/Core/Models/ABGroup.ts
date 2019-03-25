0f5ebfe5753ef4edf435a1b3ac3b09c62d31ce21
Antti Moilanen <anttim@unity3d.com>
2019-03-25 10:50:32 +0200

    Revert "Merge pull request #8614 from Applifier/feature/accessible-mraid-close-button"
    This reverts commit a83322600cd41c789189c9c9a36ef75c18b887cb, reversing
    changes made to 134e8b5e54290a6684a3fd2ca5b8b838d03f3063.

--- src/ts/Core/Models/ABGroup.ts
+++ src/ts/Core/Models/ABGroup.ts
@@ -45,7 +45,7 @@ export const FakeDisabledABTest = new DisabledABTest(16, 17);
 export const ConsentAltTitle = new ABTest(9, 10);
 export const AuctionV5Test = new ABTest(15, 18);
 export const WebPlayerMRAIDTest = new DisabledABTest();
+
 // Two hold out groups that should not get the install now button in rewarded videos
 export const HoldOutInstallInRewardedVideos = new ABTest(5, 6);
-export const AccessibleMRAIDCloseButtonTest = new ABTest(7, 8);
 export const VastStrictAdMobTest = new ABTest(14, 17);
