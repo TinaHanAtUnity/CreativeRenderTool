cecd1f2c80c711340faa929a19a8f43c8cdd9051
Anna Cho <annac@unity3d.com>
2019-03-27 11:07:28 -0700

    using ProgrammaictVastParserStrict

--- src/ts/VPAID/Parsers/ProgrammaticVPAIDParser.ts
+++ src/ts/VPAID/Parsers/ProgrammaticVPAIDParser.ts
@@ -4,12 +4,12 @@ import { Session } from 'Ads/Models/Session';
 import { ICore } from 'Core/ICore';
 import { Vast } from 'VAST/Models/Vast';
 import { VastMediaFile } from 'VAST/Models/VastMediaFile';
-import { ProgrammaticVastParser } from 'VAST/Parsers/ProgrammaticVastParser';
+import { ProgrammaticVastParserStrict } from 'VAST/Parsers/ProgrammaticVastParser';
 import { IVPAIDCampaign, VPAIDCampaign } from 'VPAID/Models/VPAIDCampaign';
 import { VPAIDParser } from 'VPAID/Utilities/VPAIDParser';
 import { CampaignContentTypes } from 'Ads/Utilities/CampaignContentTypes';
 
-export class ProgrammaticVPAIDParser extends ProgrammaticVastParser {
+export class ProgrammaticVPAIDParser extends ProgrammaticVastParserStrict {
 
     public static ContentType = CampaignContentTypes.ProgrammaticVpaid;
 
