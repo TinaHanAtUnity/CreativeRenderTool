8192617d10d199a89c8f904d95db3353799a473d
Kirill Likhobabich <kirill.likhobabich@unity3d.com>
2019-05-02 16:00:54 +0300

    fixed ab groups that's match master branch

--- src/ts/Core/Models/ABGroup.ts
+++ src/ts/Core/Models/ABGroup.ts
@@ -69,7 +69,3 @@ export const ConsentCTATest = new ABTest(9, 10);
 export const AuctionV5Test = new ABTest(15, 18);
 export const WebPlayerMRAIDTest = new DisabledABTest();
 export const OpenMeasurementTest = new DisabledABTest();
-
-export const SkipUnderTimerExperiment = new ZyngaFilteredABTest(7, 8);
-export const iOSCrashTest = new ABTest(15);
-export const DisableBackupCampaignsTest = new ABTest(17, 18);
