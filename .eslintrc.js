86da7c351f92c43654feed76161554bddd0b7a5e
Shawn Hampton <shawnh@unity3d.com>
2019-05-13 11:46:41 -0700

    Squashed commit of the following:
    commit 82d69f95c1875f35d136e6d1b3696cb332ee22ca
    Merge: 6b9fe31bb 23b95c160
    Author: Shawn Hampton <shawnh@unity3d.com>
    Date:   Mon May 13 11:40:32 2019 -0700
    
        Merge branch 'master' into investigation/eslint
    
    commit 6b9fe31bbdae81be78a054de08e841e0848fb3a5
    Author: Shawn Hampton <shawnh@unity3d.com>
    Date:   Mon May 13 11:38:04 2019 -0700
    
        Cleanup json file and accidental changes
    
    commit ba5c12a4b37142edf6f9a8a1d5eef4a7202c8f1b
    Author: Shawn Hampton <shawnh@unity3d.com>
    Date:   Mon May 13 11:12:11 2019 -0700
    
        Disabling more rules that break
    
    commit f8a777693794e752db8be2140da2b1f65e5b1414
    Author: Shawn Hampton <shawnh@unity3d.com>
    Date:   Mon May 13 10:50:10 2019 -0700
    
        Remove changes to test files
    
    commit 0b5fe1ad5c086b7d3a8e596bd9d607324727fcaf
    Author: Shawn Hampton <shawnh@unity3d.com>
    Date:   Mon May 13 10:48:40 2019 -0700
    
        Revert "All fixes"
    
        This reverts commit 8898f01ec1fa79aa0884147abe1a81d567101573.
    
    commit 6b4628b25d2ab1f1f8c46487b27c362485302654
    Author: Shawn Hampton <shawnh@unity3d.com>
    Date:   Mon May 13 10:08:18 2019 -0700
    
        Linting tests
    
    commit ff48fdb34b062c6399fed75c0258eaedd543d604
    Author: Shawn Hampton <shawnh@unity3d.com>
    Date:   Fri May 10 15:28:17 2019 -0700
    
        Changing `make lint` to run eslint instead of tslint
    
    commit 8898f01ec1fa79aa0884147abe1a81d567101573
    Author: Shawn Hampton <shawnh@unity3d.com>
    Date:   Fri May 10 14:47:42 2019 -0700
    
        All fixes
    
    commit 4c146808b9210c20fc9dbdbae5f306043a366a40
    Author: Shawn Hampton <shawnh@unity3d.com>
    Date:   Fri May 10 14:46:41 2019 -0700
    
        Disable prettier
    
    commit 9be446f7dd12192c35b764ba9494ad5ebbf27a45
    Author: Shawn Hampton <shawnh@unity3d.com>
    Date:   Fri May 10 14:31:01 2019 -0700
    
        Lint rules that we fail minus prettier
    
    commit 31d9ae3309e59ae0a613f686a28c444f1597a664
    Author: Shawn Hampton <shawnh@unity3d.com>
    Date:   Thu May 9 15:49:43 2019 -0700
    
        Changing recommended rules
    
    commit 576932a486b14dd5753b4a88eaac94bc2aea4033
    Author: Shawn Hampton <shawnh@unity3d.com>
    Date:   Thu May 9 15:28:38 2019 -0700
    
        Adding on save linting and prettier
    
    commit 4da2e2eb1260c2ec06ca4b10429d3753b8396c64
    Merge: f51b785c6 8f5fc6958
    Author: Shawn Hampton <shawnh@unity3d.com>
    Date:   Thu May 9 15:00:16 2019 -0700
    
        Merge branch 'investigation/eslint' of github.com:Applifier/unity-ads-webview into investigation/eslint
    
    commit f51b785c6e69dc861d2f43273abc0358dfe1f2c0
    Author: Shawn Hampton <shawnh@unity3d.com>
    Date:   Mon May 6 17:07:34 2019 -0700
    
        Add eslint for typescript
    
    commit 8f5fc69580c1b09fb6dd9d04292a641a7e0eaa75
    Author: Shawn Hampton <shawnh@unity3d.com>
    Date:   Mon May 6 17:07:34 2019 -0700
    
        Add eslint for typescript

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    // 'prettier/@typescript-eslint', // TODO Re-Enable prettier
+    // 'plugin:prettier/recommended' // TODO Re-Enable prettier
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    '@typescript-eslint/interface-name-prefix': ['off', 'always'], // TODO
+    '@typescript-eslint/explicit-function-return-type': ['off'], // TODO
+    '@typescript-eslint/explicit-member-accessibility': ['off'], // TODO
+    '@typescript-eslint/no-non-null-assertion': ['off'], // TODO
+    '@typescript-eslint/no-angle-bracket-type-assertion': ['off'], // TODO
+    'camelcase': ['off'], // TODO
+    '@typescript-eslint/camelcase': ['off'], // TODO
+    '@typescript-eslint/no-empty-interface': ['off'], // TODO
+    '@typescript-eslint/no-object-literal-type-assertion': ['off'], // TODO
+    '@typescript-eslint/no-unused-vars': ['off'], // TODO
+    '@typescript-eslint/no-explicit-any': ['off'], // TODO
+    '@typescript-eslint/type-annotation-spacing': ['off'], // TODO
+    '@typescript-eslint/indent': ['off'], // TODO
+    '@typescript-eslint/prefer-interface': ['off'], // TODO
+    '@typescript-eslint/no-inferrable-types': ['off'], // TODO
+    '@typescript-eslint/ban-types': ['off'], // TODO
+    '@typescript-eslint/no-use-before-define': ['off'], // TODO
+  }
+}
--- Makefile
+++ Makefile
@@ -8,6 +8,7 @@ BIN := node_modules/.bin
 TYPESCRIPT := $(BIN)/tsc
 REMAP_ISTANBUL := $(BIN)/remap-istanbul
 TSLINT := $(BIN)/tslint
+ESLINT := $(BIN)/eslint
 STYLUS := $(BIN)/stylus
 ROLLUP := $(BIN)/rollup
 STYLINT := $(BIN)/stylint
@@ -242,11 +243,26 @@ $(TEST_BUILD_DIR)/CoverageBundle.js: $(TEST_BUILD_DIR)/Unit.js $(TS_TARGETS) $(H
 clean:
 	rm -rf $(BUILD_DIR)/*
 
+lint-ts: 
+	$(TSLINT) --project tsconfig.json $(TS_SOURCES)
+
+lint-es:
+	$(ESLINT) $(TS_SOURCES)
+
+lint-es-test:
+	$(ESLINT) $(TESTS)
+
+lint-es-fix:
+	$(ESLINT) --fix $(TS_SOURCES)
+
+lint-es-test-fix:
+	$(ESLINT) --fix $(TESTS)
+
 lint:
 	parallel --ungroup ::: \
 		"$(STYLINT) $(SOURCE_DIR)/styl -c stylintrc.json" \
-		"$(TSLINT) --project tsconfig.json $(TS_SOURCES)" \
-		"$(TSLINT) --project tsconfig.json --config test/tslint.json $(TESTS)"
+		"$(ESLINT) $(TS_SOURCES)" \
+		"$(ESLINT) $(TESTS)"
 
 lint-fix:
 	parallel --ungroup ::: \
