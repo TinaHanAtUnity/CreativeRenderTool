ba6caf52eac5fdc348f18cac58af45d95c0697ea
Fritz Huie <fritzhuie@gmail.com>
2019-05-16 16:36:57 -0700

    enable whitelist

--- src/ts/Ads/Ads.ts
+++ src/ts/Ads/Ads.ts
@@ -83,6 +83,7 @@ import { AbstractAdUnitParametersFactory } from 'Ads/AdUnits/AdUnitParametersFac
 import { RefreshManager } from 'Ads/Managers/RefreshManager';
 import { LoadManager } from 'Ads/Managers/LoadManager';
 import { MediationMetaData } from 'Core/Models/MetaData/MediationMetaData';
+import { ClientInfo } from 'Core/Models/ClientInfo';
 
 export class Ads implements IAds {
 
@@ -252,10 +253,8 @@ export class Ads implements IAds {
             this.CampaignManager = new CampaignManager(this._core.NativeBridge.getPlatform(), this._core.Api, this._core.Config, this.Config, this.AssetManager, this.SessionManager, this.AdMobSignalFactory, this._core.RequestManager, this._core.ClientInfo, this._core.DeviceInfo, this._core.MetaDataManager, this._core.CacheBookkeeping, this.ContentTypeHandlerManager, this._core.JaegerManager, this.BackupCampaignManager);
 
             if(this._loadApiEnabled) {
-                this._core.Api.Sdk.logInfo(`LOAD API ENABLED LOAD MANAGER LOADING`);
                 this.RefreshManager = new LoadManager(this._core.NativeBridge.getPlatform(), this._core.Api, this._core.Config, this.Api, this.Config, this.CampaignManager, this._core.ClientInfo, this._core.FocusManager);
             } else {
-                this._core.Api.Sdk.logInfo(`LOAD API NOT ENABLED CAMPAIGN MANAGER LOADING`);
                 this.RefreshManager = new CampaignRefreshManager(this._core.NativeBridge.getPlatform(), this._core.Api, this._core.Config, this.Api, this._core.WakeUpManager, this.CampaignManager, this.Config, this._core.FocusManager, this.SessionManager, this._core.ClientInfo, this._core.RequestManager, this._core.CacheManager);
             }
 
@@ -568,8 +567,7 @@ export class Ads implements IAds {
         return this._core.MetaDataManager.fetch(MediationMetaData).then((mediation) => {
             if(mediation) {
                 const loadEnabled = mediation.isMetaDataLoadEnabled();
-                this._core.Api.Sdk.logInfo(`LOAD ENABLED = ${loadEnabled}`);
-                if(loadEnabled) {
+                if(loadEnabled && CustomFeatures.isWhiteListedForLoadApi(this._core.ClientInfo.getGameId())) {
                     this._loadApiEnabled = true;
                 }
             }
