71a634d4c01986fd2a4eaf600ed023b4616e6472
Andrew Konecny <andrewko@unity3d.com>
2019-05-21 13:08:27 -0700

    Correctly convert types

--- src/ts/Ads/Utilities/CampaignContentType.ts
+++ src/ts/Ads/Utilities/CampaignContentType.ts
@@ -20,11 +20,9 @@ export class ContentType {
     public static getCampaignParseError(contentType: string): CampaignParseError {
         let errorType = CampaignParseError.UnknownParseError;
         const parseError = `parse_campaign_${contentType.replace(/[\/-]/g, '_')}_error`;
-        Object.values(CampaignContentType).forEach((value: string) => {
-            if (value.valueOf() === contentType && parseError in Object.values(CampaignParseError)) {
-                errorType = <CampaignParseError>parseError;
-            }
-        });
+        if (Object.values(CampaignContentType).includes(contentType) && Object.values(CampaignParseError).includes(parseError)) {
+            errorType = <CampaignParseError>parseError;
+        }
         return errorType;
     }
 }
