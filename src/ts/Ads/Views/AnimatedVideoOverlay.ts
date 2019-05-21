a2ab91c24ad6bfdd6191a6116a127e36e098983c
Kirill Likhobabich <kirill.likhobabich@unity3d.com>
2019-05-21 16:37:00 +0300

    more descriptive variable name

--- src/ts/Ads/Views/AnimatedVideoOverlay.ts
+++ src/ts/Ads/Views/AnimatedVideoOverlay.ts
@@ -4,14 +4,14 @@ import { VastCampaign } from 'VAST/Models/VastCampaign';
 import { GoogleStore } from 'Store/Utilities/GoogleStore';
 
 export class AnimatedVideoOverlay extends VideoOverlay implements IPrivacyHandlerView {
-  protected _initialised: boolean;
+  protected _isEndScreenVisible: boolean;
 
   public setVideoProgress(value: number): void {
     super.setVideoProgress(value);
     const timerCount = Math.ceil((this._videoDuration - this._videoProgress) / 1000);
     const endScreen = window.document.getElementById('end-screen');
-    if (timerCount <= 1 && !this._initialised) {
-      this._initialised = true;
+    if (timerCount <= 1 && !this._isEndScreenVisible) {
+      this._isEndScreenVisible = true;
       if (endScreen) {
         endScreen.style.visibility = 'visible';
         endScreen.classList.add('on-show');
