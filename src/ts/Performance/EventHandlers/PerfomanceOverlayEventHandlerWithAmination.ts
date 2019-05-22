a2bf39c2f052ef26d115e41bd8ae626ee7aa7606
Kirill Likhobabich <kirill.likhobabich@unity3d.com>
2019-05-22 12:01:12 +0300

    PerfomanceOverlayEventHandlerWithAmination update
    - Rename onOverlayAnimation > showEndScreenWithAnimation
    - Remove the unesessary whichTransitionEvent() for browser prefix checking. As the default event listener is supported on minimal required versions.
    - super.onOverlaySkip instead of this.onOverlaySkip method.

--- src/ts/Performance/EventHandlers/PerfomanceOverlayEventHandlerWithAmination.ts
+++ src/ts/Performance/EventHandlers/PerfomanceOverlayEventHandlerWithAmination.ts
@@ -5,40 +5,23 @@ import { ThirdPartyEventManager, TrackingEvent } from 'Ads/Managers/ThirdPartyEv
 export class PerfomanceOverlayEventHandlerWithAmination extends PerformanceOverlayEventHandler {
     protected _performanceAdUnit: PerformanceAdUnit;
 
-    private whichTransitionEvent(): string {
-        const transitions: {[index: string]: string} = {
-            'transition'      : 'transitionend',
-            'OTransition'     : 'oTransitionEnd',
-            'MozTransition'   : 'transitionend',
-            'WebkitTransition': 'webkitTransitionEnd'
-        };
-        let transition;
-        for (const key in transitions) {
-            if (transitions.hasOwnProperty(key)) {
-                transition = transitions[key];
-            }
-        }
-        return transition ? transition : 'transitionend';
-    }
-
-    public onOverlayAnimation (position: number): void {
+    public showEndScreenWithAnimation (position: number): void {
         const gameBackgroundContainer = <HTMLElement>document.getElementsByClassName('game-background-container')[0];
         const endScreen = this._performanceAdUnit.getEndScreen();
         const endScreenElement = document.getElementById('end-screen');
-        const transitionEvent = this.whichTransitionEvent();
         this._ads.VideoPlayer.pause();
         if (endScreenElement && gameBackgroundContainer) {
             endScreenElement.style.visibility = 'visible';
-            endScreenElement.classList.add('on-show');
-            gameBackgroundContainer.addEventListener(transitionEvent, () => {
-                this.onOverlaySkip(position);
+            gameBackgroundContainer.addEventListener('transitionend', () => {
+                super.onOverlaySkip(position);
                 if (endScreen) {
                     endScreen.show();
                 }
                 this._performanceAdUnit.onFinish.trigger();
             });
+            endScreenElement.classList.add('on-show');
         } else {
-            this.onOverlaySkip(position);
+            super.onOverlaySkip(position);
             if (endScreen) {
                 endScreen.show();
             }
@@ -50,7 +33,7 @@ export class PerfomanceOverlayEventHandlerWithAmination extends PerformanceOverl
         if (this._placement.skipEndCardOnClose()) {
             super.onOverlayClose();
         } else {
-            this.onOverlayAnimation(position);
+            this.showEndScreenWithAnimation(position);
         }
         this._performanceAdUnit.sendTrackingEvent(TrackingEvent.SKIP);
     }
