8abeeb93f1db223f539826ed42e43214215a6a1c
Antti Moilanen <anttim@unity3d.com>
2019-05-22 09:33:37 +0300

    Removed unneeded redefinition of IVideoOverlayParameters.

--- src/ts/Ads/Views/VideoOverlayCTAV2.ts
+++ src/ts/Ads/Views/VideoOverlayCTAV2.ts
@@ -1,26 +1,10 @@
 import { VideoOverlay, IVideoOverlayParameters } from 'Ads/Views/VideoOverlay';
 import { Campaign } from 'Ads/Models/Campaign';
 import { AbstractPrivacy, IPrivacyHandlerView } from 'Ads/Views/AbstractPrivacy';
-import { Platform } from 'Core/Constants/Platform';
-import { IAdsApi } from 'Ads/IAds';
-import { DeviceInfo } from 'Core/Models/DeviceInfo';
-import { ClientInfo } from 'Core/Models/ClientInfo';
-import { CoreConfiguration } from 'Core/Models/CoreConfiguration';
-import { Placement } from 'Ads/Models/Placement';
 import VideoOverlayTemplateCTAV2 from 'html/VideoOverlayCTAV2.html';
 import { Template } from 'Core/Utilities/Template';
 import { PerformanceCampaign } from 'Performance/Models/PerformanceCampaign';
 import { XPromoCampaign } from 'XPromo/Models/XPromoCampaign';
-
-export interface IVideoOverlayParameters<T extends Campaign> {
-  platform: Platform;
-  ads: IAdsApi;
-  deviceInfo: DeviceInfo;
-  clientInfo: ClientInfo;
-  campaign: T;
-  coreConfig: CoreConfiguration;
-  placement: Placement;
-}
 
 export class VideoOverlayCTAV2 extends VideoOverlay implements IPrivacyHandlerView {
   constructor(parameters: IVideoOverlayParameters<Campaign>, privacy: AbstractPrivacy, showGDPRBanner: boolean, showPrivacyDuringVideo: boolean) {
