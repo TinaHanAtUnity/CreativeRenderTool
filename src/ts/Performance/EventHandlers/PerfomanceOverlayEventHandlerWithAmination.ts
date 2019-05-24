cf0dde82ec7a404cd6d970ada9d66b15b2d68e02
Kirill Likhobabich <kirill.likhobabich@unity3d.com>
2019-05-24 13:28:32 +0300

    Implement logic for skipping animation over download
    - override onOverlayDownload method
    - set some parent class methods as protected

--- src/ts/Performance/EventHandlers/PerfomanceOverlayEventHandlerWithAmination.ts
+++ src/ts/Performance/EventHandlers/PerfomanceOverlayEventHandlerWithAmination.ts
@@ -1,5 +1,7 @@
 import { PerformanceOverlayEventHandler } from 'Performance/EventHandlers/PerformanceOverlayEventHandler';
 import { PerformanceAdUnit } from 'Performance/AdUnits/PerformanceAdUnit';
+import { IVideoOverlayDownloadParameters } from 'Ads/EventHandlers/OverlayEventHandlerWithDownloadSupport';
+import { TrackingEvent } from 'Ads/Managers/ThirdPartyEventManager';
 
 export class PerfomanceOverlayEventHandlerWithAmination extends PerformanceOverlayEventHandler {
     protected _performanceAdUnit: PerformanceAdUnit;
@@ -15,10 +17,28 @@ export class PerfomanceOverlayEventHandlerWithAmination extends PerformanceOverl
                     super.onOverlaySkip(position);
                 });
             });
-            endScreenElement.classList.add('on-show');
+            endScreenElement.classList.add('on-show-animation');
         } else {
             super.onOverlaySkip(position);
         }
+    }
+
+    public onOverlayDownload(parameters: IVideoOverlayDownloadParameters): void {
+        this.setCallButtonEnabled(false);
+        if (!parameters.skipEnabled) {
+            // This is for the install now button test in rewarded ad video overlay.
+            const operativeEventParams = this.getOperativeEventParams(parameters);
+            this._operativeEventManager.sendThirdQuartile(operativeEventParams);
+            this._operativeEventManager.sendView(operativeEventParams);
+        }
+        this._storeHandler.onDownload(parameters);
+        if (parameters.skipEnabled) {
+            // there we calling super method becouse we need to skip
+            // animation stage when the download button is clicked.
+            super.onOverlaySkip(parameters.videoProgress);
+        }
+        this.setCallButtonEnabled(true);
+        this._performanceAdUnit.sendTrackingEvent(TrackingEvent.CLICK);
     }
 
     public onOverlaySkip(position: number): void {
