3e786fe0f66e40a9d47d16773c16679b8a569d88
Miley Hollenberg <miley.hollenberg@unity3d.com>
2019-07-05 16:07:42 +0300

    Removed the constructor as it wasn't used or needed

--- src/ts/Performance/Views/PerformanceEndScreenDoubleShadowClose.ts
+++ src/ts/Performance/Views/PerformanceEndScreenDoubleShadowClose.ts
@@ -1,13 +1,6 @@
 import { PerformanceEndScreen } from 'Performance/Views/PerformanceEndScreen';
-import { IEndScreenParameters } from 'Ads/Views/EndScreen';
-import { PerformanceCampaign } from 'Performance/Models/PerformanceCampaign';
 
 export class PerformanceEndScreenDoubleShadowClose extends PerformanceEndScreen {
-
-    constructor(parameters: IEndScreenParameters, campaign: PerformanceCampaign, country?: string) {
-        super(parameters, campaign, country);
-    }
-
     public render(): void {
         super.render();
 
