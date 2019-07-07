76eb7fa09252445e68157b38f76ff2a8dbe2bd19
Sergey Yanchi <sergeyyanchi@unity3d.com>
2019-07-07 20:36:07 +0300

    updated UnityAds api

--- src/ts/Backend/UnityAds.ts
+++ src/ts/Backend/UnityAds.ts
@@ -4,10 +4,12 @@ import { Platform } from 'Core/Constants/Platform';
 import { NativeBridge } from 'Core/Native/Bridge/NativeBridge';
 import { WebView } from 'WebView';
 import { PlacementState } from 'Ads/Models/Placement';
+import { EventCategory } from 'Core/Constants/EventCategory';
+import { LoadEvent } from 'Core/Native/LoadApi';
 
 export class UnityAds {
 
-    public static initialize(platform: Platform, gameId: string, listener: IUnityAdsListener, testMode: boolean = false): Promise<void> {
+    public static initialize(platform: Platform, gameId: string, listener: IUnityAdsListener, testMode: boolean = false, loadMode: boolean = false): Promise<void> {
         let nativeBridge: NativeBridge;
         switch(platform) {
             // Setting auto batching on does not work in a "single-threaded" environment due to callbacks and events
@@ -28,17 +30,30 @@ export class UnityAds {
 
         UnityAds._backend.Api.Sdk.setGameId(gameId);
         UnityAds._backend.Api.Sdk.setTestMode(testMode);
+        UnityAds._backend.Api.Sdk.setUsePerPlacementLoad(loadMode);
 
         UnityAds._listener = listener;
 
         UnityAds._webView = new WebView(nativeBridge);
-        return UnityAds._webView.initialize();
+        return UnityAds._webView.initialize().then(() => {
+            UnityAds._initialized = true;
+            UnityAds.getBackend().sendEvent(EventCategory[EventCategory.LOAD_API], LoadEvent[LoadEvent.LOAD_PLACEMENTS], ...UnityAds._loadRequests);
+            UnityAds._loadRequests = [];
+        });
     }
 
     public static show(placement: string) {
         UnityAds._webView.show(placement, {}, () => {
             return;
         });
+    }
+
+    public static load(placement: string) {
+        if (UnityAds._initialized) {
+            UnityAds.getBackend().sendEvent(EventCategory[EventCategory.LOAD_API], LoadEvent[LoadEvent.LOAD_PLACEMENTS], placement);
+        } else {
+            UnityAds._loadRequests.push(placement);
+        }
     }
 
     public static getListener() {
@@ -51,6 +66,8 @@ export class UnityAds {
 
     public static setBackend(backend: Backend) {
         UnityAds._backend = backend;
+        UnityAds._initialized = false;
+        UnityAds._loadRequests = [];
     }
 
     public static getBackend() {
@@ -60,5 +77,7 @@ export class UnityAds {
     private static _backend: Backend;
     private static _listener: IUnityAdsListener | undefined;
     private static _webView: WebView;
+    private static _initialized: boolean = false;
+    private static _loadRequests: string[] = [];
 
 }
