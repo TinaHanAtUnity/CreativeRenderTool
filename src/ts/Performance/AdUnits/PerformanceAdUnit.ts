c7f67d810a3720d1e3d15606739a90262e142215
Kalle Manninen <kallem@unity3d.com>
2019-07-08 14:58:29 +0300

    Remove unnecessary type from union

--- src/ts/Performance/AdUnits/PerformanceAdUnit.ts
+++ src/ts/Performance/AdUnits/PerformanceAdUnit.ts
@@ -5,12 +5,11 @@ import { CampaignAssetInfo } from 'Ads/Utilities/CampaignAssetInfo';
 import { AbstractPrivacy } from 'Ads/Views/AbstractPrivacy';
 import { PerformanceCampaign } from 'Performance/Models/PerformanceCampaign';
 import { PerformanceEndScreen } from 'Performance/Views/PerformanceEndScreen';
-import { PerformanceColorTintingEndScreen } from 'Performance/Views/PerformanceColorTintingEndScreen';
 import { DownloadManager } from 'China/Managers/DownloadManager';
 import { DeviceIdManager } from 'Core/Managers/DeviceIdManager';
 
 export interface IPerformanceAdUnitParameters extends IVideoAdUnitParameters<PerformanceCampaign> {
-    endScreen: PerformanceEndScreen | PerformanceColorTintingEndScreen;
+    endScreen: PerformanceEndScreen;
     adUnitStyle?: AdUnitStyle;
     downloadManager?: DownloadManager;
     deviceIdManager?: DeviceIdManager;
@@ -18,7 +17,7 @@ export interface IPerformanceAdUnitParameters extends IVideoAdUnitParameters<Per
 
 export class PerformanceAdUnit extends VideoAdUnit<PerformanceCampaign> {
 
-    private _endScreen: PerformanceEndScreen | PerformanceColorTintingEndScreen;
+    private _endScreen: PerformanceEndScreen;
     private _privacy: AbstractPrivacy;
     private _performanceCampaign: PerformanceCampaign;
     private _thirdPartyEventManager: ThirdPartyEventManager;
@@ -61,7 +60,7 @@ export class PerformanceAdUnit extends VideoAdUnit<PerformanceCampaign> {
         return 'performance';
     }
 
-    public getEndScreen(): PerformanceEndScreen | PerformanceColorTintingEndScreen | undefined {
+    public getEndScreen(): PerformanceEndScreen | undefined {
         return this._endScreen;
     }
 
