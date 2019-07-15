aee5c5f9a19330ee4f98a61048f93d343147aab9
Andrew Konecny <andrewko@unity3d.com>
2019-07-15 15:14:40 -0700

    Track instances of losing the sessiondiagnostic by sending a regular diagnostic

--- src/ts/Ads/Utilities/SessionDiagnostics.ts
+++ src/ts/Ads/Utilities/SessionDiagnostics.ts
@@ -1,6 +1,7 @@
 import { Session } from 'Ads/Models/Session';
 import { INativeResponse } from 'Core/Managers/RequestManager';
 import { HttpKafka, KafkaCommonObjectType } from 'Core/Utilities/HttpKafka';
+import { Diagnostics } from 'Core/Utilities/Diagnostics';
 
 export interface IKafkaObject {
     [key: string]: unknown;
@@ -12,16 +13,21 @@ export interface IKafkaObject {
 export class SessionDiagnostics {
     public static trigger(type: string, error: unknown, session: Session): Promise<INativeResponse> {
 
-        const adPlan = session.getAdPlan();
-        // Don't send kafka messages with adplans greater than 1MB
-        if (adPlan && adPlan.length > 1048576) {
-            return Promise.resolve(<INativeResponse>{});
-        }
         // ElasticSearch schema generation can result in dropping errors if root values are not the same type across errors
         if (!error || typeof error !== 'object' || Array.isArray(error)) {
             error = {
                 value: error
             };
+        }
+
+        const adPlan = session.getAdPlan();
+        // Don't send kafka messages with adplans greater than 1MB
+        if (adPlan && adPlan.length > 1048576) {
+            const errorWithOldDiagnosticType = {
+                error,
+                type
+            };
+            return Diagnostics.trigger('adplan_too_large', errorWithOldDiagnosticType);
         }
 
         const kafkaObject: IKafkaObject = {
