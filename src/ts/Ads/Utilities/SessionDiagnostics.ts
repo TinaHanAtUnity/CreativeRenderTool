62629fe22e83e95e09f028df8f9bf18f939f60d4
Andrew Konecny <andrewko@unity3d.com>
2019-07-15 13:50:57 -0700

    Remove sending SessionDiagnostics with AdPlans greater than 1MB

--- src/ts/Ads/Utilities/SessionDiagnostics.ts
+++ src/ts/Ads/Utilities/SessionDiagnostics.ts
@@ -11,6 +11,12 @@ export interface IKafkaObject {
 
 export class SessionDiagnostics {
     public static trigger(type: string, error: unknown, session: Session): Promise<INativeResponse> {
+
+        const adPlan = session.getAdPlan();
+        // Don't send kafka messages with adplans greater than 1MB
+        if (adPlan && adPlan.length > 1048576) {
+            return Promise.resolve(<INativeResponse>{});
+        }
         // ElasticSearch schema generation can result in dropping errors if root values are not the same type across errors
         if (!error || typeof error !== 'object' || Array.isArray(error)) {
             error = {
@@ -21,7 +27,7 @@ export class SessionDiagnostics {
         const kafkaObject: IKafkaObject = {
             type,
             timestamp: Date.now(),
-            adPlan: session.getAdPlan() ? session.getAdPlan() : undefined
+            adPlan: adPlan
         };
         kafkaObject[type] = error;
 
