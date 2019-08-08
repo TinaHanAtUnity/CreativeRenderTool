3d8690f5121810f26c4d5ca99303fc9bba1f0267
Fritz Huie <fritzhuie@gmail.com>
2019-08-08 16:08:45 -0700

    update IosUtils to include rotation

--- src/ts/Ads/Utilities/IosUtils.ts
+++ src/ts/Ads/Utilities/IosUtils.ts
@@ -1,17 +1,20 @@
+import { Orientation } from 'Ads/AdUnits/Containers/AdUnitContainer';
+
 export class IosUtils {
 
-    public static isAppSheetBroken(osVersion: string, model: string): boolean {
-        if (!model.match(/ipad/i) && osVersion.match(/^12\.[0-9]/)) {
-            return true;
-        } else if (!model.match(/ipad/i) && osVersion.match(/^11\.[0-4]/)) {
-            return true;
+    public static isAppSheetBroken(osVersion: string, model: string, orientation: Orientation): boolean {
+        if(!model.match(/ipad/i) && orientation == Orientation.LANDSCAPE) {
+            return true; //iPhone + Landscape = crash
         } else if (osVersion.match(/^8\.[0-3]/)) {
-            return true;
-        } else if (osVersion.match(/^7\.[0-9]/)) {
-            return true;
+            return true; //iOS 8 not supported
+        }else if (osVersion.match(/^7\.[0-9]/)) {
+            return true; //iOS 7 not supported
         } else {
             return false;
         }
+
+        // to disable iOS 12: osVersion.match(/^12\.[0-9]/)
+        // to disable iOS 11: osVersion.match(/^11\.[0-4]/)
     }
 
     public static hasVideoStallingApi(osVersion: string): boolean {
