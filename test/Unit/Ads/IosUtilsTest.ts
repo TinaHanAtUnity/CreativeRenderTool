bd0dcc868dcf28d742fd65d751a9af18a0d7b9dd
Fritz Huie <fritzhuie@gmail.com>
2019-08-08 18:06:22 -0700

    update tests

--- test/Unit/Ads/IosUtilsTest.ts
+++ test/Unit/Ads/IosUtilsTest.ts
@@ -5,12 +5,12 @@ import { assert } from 'chai';
 
 const version8 = ['8.0', '8.1', '8.2', '8.3', '8.0.3', '8.0.scott', '8.2ok', '8.01', '8.0scott'];
 const version7 = ['7.0', '7.0.5', '7.1', '7.2', '7.6', '7.9', '7.9.10', '7.01','7.2ok', '7.1scott'];
-const invalidStrings = ['8', '8..1', '8!1', '8.!1', '8.', '8.4', 'scott'];
 const version11 = ['11.0', '11.1.1', '11.2', '11.2.5', '11.3', '11.4', '11.0', '11.1.1', '11.2.5', '11.3', '11.4'];
 const version12 = ['12.0', '12.1.1', '12.2', '12.5.5', '12.0', '12.1.1', '12.2', '12.5.5'];
+const unknownVersion = ['8', '8..1', '8!1', '8.!1', '8.', '8.4', 'scott'];
 
-const invalidVersions = [version7, version8, invalidStrings];
-const validVersions = [version11, version12];
+const unsupportedVersions = [version7, version8];
+const supportedVersions = [version11, version12, unknownVersion];
 
 const iPhone = 'iPhone8,1'
 const iPad = 'iPad6,7'
@@ -18,20 +18,20 @@ const iPad = 'iPad6,7'
 describe('IosUtilsTest', () => {
     it('isAppSheetBroken should only return true for invalid iOS versions or iPhone + PORTRAIT mode', () => {
 
-        validVersions.forEach(version => {
+        supportedVersions.forEach(version => {
             version.forEach(vString => {
                 assert.isFalse(IosUtils.isAppSheetBroken(vString, iPhone, Orientation.PORTRAIT), 'Should return false for valid versions on iphone in PORTRAIT');
                 assert.isFalse(IosUtils.isAppSheetBroken(vString, iPad, Orientation.PORTRAIT), 'Should return false for valid versions on iPad in PORTRAIT');
                 assert.isFalse(IosUtils.isAppSheetBroken(vString, iPad, Orientation.LANDSCAPE), 'Should return false for valid versions on iPad in LANDSCAPE');
                 
-                //iphone in LANDSCAPE should always return true: Not supported
+                //iphone in LANDSCAPE not supported
                 assert.isTrue(IosUtils.isAppSheetBroken(vString, iPhone, Orientation.LANDSCAPE), 'Should return true for valid versions on iphone in LANDSCAPE');
             });
         });
     });
 
     it('isAppSheetBroken should return true for all invalid iOS versions', () => {
-        invalidVersions.forEach(version => {
+        unsupportedVersions.forEach(version => {
             version.forEach(vString => {
                 assert.isTrue(IosUtils.isAppSheetBroken(vString, iPhone, Orientation.PORTRAIT), 'Should return true for invalid versions on iphone in PORTRAIT');
                 assert.isTrue(IosUtils.isAppSheetBroken(vString, iPad, Orientation.PORTRAIT), 'Should return true for invalid versions on iPad in PORTRAIT');
