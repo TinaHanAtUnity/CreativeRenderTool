17d65311db0ef7849e4c562556457a1c4eddc943
Fritz Huie <fritzhuie@gmail.com>
2019-08-13 13:52:05 -0700

    lint

--- test/Unit/Ads/IosUtilsTest.ts
+++ test/Unit/Ads/IosUtilsTest.ts
@@ -4,7 +4,7 @@ import 'mocha';
 import { assert } from 'chai';
 
 const version8 = ['8.0', '8.1', '8.2', '8.3', '8.0.3', '8.0.scott', '8.2ok', '8.01', '8.0scott'];
-const version7 = ['7.0', '7.0.5', '7.1', '7.2', '7.6', '7.9', '7.9.10', '7.01','7.2ok', '7.1scott'];
+const version7 = ['7.0', '7.0.5', '7.1', '7.2', '7.6', '7.9', '7.9.10', '7.01', '7.2ok', '7.1scott'];
 const version11 = ['11.0', '11.1.1', '11.2', '11.2.5', '11.3', '11.4', '11.0', '11.1.1', '11.2.5', '11.3', '11.4'];
 const version12 = ['12.0', '12.1.1', '12.2', '12.5.5', '12.0', '12.1.1', '12.2', '12.5.5'];
 const unknownVersion = ['8', '8..1', '8!1', '8.!1', '8.', '8.4', 'scott'];
@@ -17,7 +17,6 @@ const iPad = 'iPad6,7';
 
 describe('IosUtilsTest', () => {
     it('isAppSheetBroken should only return true for invalid iOS versions or iPhone + PORTRAIT mode', () => {
-
         supportedVersions.forEach(version => {
             version.forEach(vString => {
                 assert.isFalse(IosUtils.isAppSheetBroken(vString, iPhone, Orientation.PORTRAIT), 'Should return false for valid versions on iphone in PORTRAIT');
