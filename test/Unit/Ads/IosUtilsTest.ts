597866fb9b3e0c7ca1653f809a2dbdfd57b989f8
Fritz Huie <fritzhuie@users.noreply.github.com>
2019-08-15 15:59:02 -0700

    update test

--- test/Unit/Ads/IosUtilsTest.ts
+++ test/Unit/Ads/IosUtilsTest.ts
@@ -7,7 +7,7 @@ const version8 = ['8.0', '8.1', '8.2', '8.3', '8.0.3', '8.0.scott', '8.2ok', '8.
 const version7 = ['7.0', '7.0.5', '7.1', '7.2', '7.6', '7.9', '7.9.10', '7.01', '7.2ok', '7.1scott'];
 const version11 = ['11.0', '11.1.1', '11.2', '11.2.5', '11.3', '11.4', '11.0', '11.1.1', '11.2.5', '11.3', '11.4'];
 const version12 = ['12.0', '12.1.1', '12.2', '12.5.5', '12.0', '12.1.1', '12.2', '12.5.5'];
-const unknownVersion = ['8', '8..1', '8!1', '8.!1', '8.', '8.4', 'scott'];
+const unknownVersion = ['8', '8..1', '8!1', '8.!1', '8.', '8.5', 'scott'];
 
 const unsupportedVersions = [version7, version8];
 const supportedVersions = [version11, version12, unknownVersion];
