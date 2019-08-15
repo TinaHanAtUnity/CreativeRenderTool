c5cbcfb919ea3fc3fd903a6eb3a108c58def50b2
Antti Moilanen <anttim@unity3d.com>
2019-08-15 12:55:57 +0300

    Revert "Merge pull request #10751 from Applifier/feature/revert-query-cta-experiment"
    This reverts commit 83332e71cd212d7ca298c9a7c43714632b77089e, reversing
    changes made to e06870da4e440ef5e604c87e0e67b7717d8353b3.

--- src/ts/Core/Models/ABGroup.ts
+++ src/ts/Core/Models/ABGroup.ts
@@ -69,3 +69,4 @@ export const ConsentTest = new ABTest(9, 10);
 export const OpenMeasurementTest = new DisabledABTest();
 export const IAPAutoLoggingTest = new DisabledABTest();
 export const SliderEndCardExperiment = new DisabledABTest();
+export const QueryCTATest = new ZyngaFilteredABTest(7, 8);
