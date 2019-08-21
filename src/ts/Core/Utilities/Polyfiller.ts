0e70090982ea6076efcd104bfcc5744a62874197
Andrew Konecny <andrewko@unity3d.com>
2019-08-21 16:03:10 -0700

    Add Polyfill Utility and Tests

---
+++ src/ts/Core/Utilities/Polyfiller.ts
@@ -0,0 +1,7 @@
+export class Polyfiller {
+    public static getObjectValuesFunction(): (object: {}) => unknown[] {
+        return (object) => {
+            return Object.keys(object).map((values) => object[<keyof typeof object>values]);
+        };
+    }
+}
