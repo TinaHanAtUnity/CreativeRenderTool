8bf47b62f2c2b788e019f409ef081cf5b5cc8f03
Scott Wise <scottw@unity3d.com>
2019-09-18 16:52:47 -0700

    cancel tests in abgroup

--- test/Unit/VAST/VastParserStrictTest.ts
+++ test/Unit/VAST/VastParserStrictTest.ts
@@ -96,7 +96,7 @@ describe('VastParserStrict', () => {
         });
 
         context('for IAS', () => {
-            it('should replace adsafeprotected urls with vastpixel3 and include correct header for IAS', () => {
+            xit('should replace adsafeprotected urls with vastpixel3 and include correct header for IAS', () => {
                 const wrappedVAST = WrappedVastIAS;
                 sinon.stub(request, 'get').resolves();
 
@@ -107,7 +107,7 @@ describe('VastParserStrict', () => {
                 sinon.assert.calledWith(<sinon.SinonStub>request.get, newUrl, headers, {retries: 2, retryDelay: 10000, followRedirects: true, retryWithConnectionEvents: false});
             });
 
-            it('should splice bundleID as first url query param', () => {
+            xit('should splice bundleID as first url query param', () => {
                 const wrappedVAST = WrappedVastIAS;
                 sinon.stub(request, 'get').resolves();
 
