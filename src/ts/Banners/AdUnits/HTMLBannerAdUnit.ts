fbce94920eeeaee883bfe2370024b1b52e1d90aa
Tina Han <yunduan@unity3d.com>
2019-09-23 14:14:13 -0700

    Make leaveApplication event to be consistent with click event

--- src/ts/Banners/AdUnits/HTMLBannerAdUnit.ts
+++ src/ts/Banners/AdUnits/HTMLBannerAdUnit.ts
@@ -38,6 +38,7 @@ export abstract class HTMLBannerAdUnit implements IBannerAdUnit {
 
     private _clickEventsSent = false;
     private _impressionEventsSent = false;
+    private _leaveApplicationEventTriggered = false;
 
     constructor(parameters: IBannerAdUnitParameters) {
         this._platform = parameters.platform;
@@ -103,14 +104,20 @@ export abstract class HTMLBannerAdUnit implements IBannerAdUnit {
             }
             if (this._platform === Platform.IOS) {
                 this._core.iOS!.UrlScheme.open(url).then(() => {
-                    this._bannerNativeApi.BannerListenerApi.sendLeaveApplicationEvent(this._bannerAdViewId);
+                    if (!this._leaveApplicationEventTriggered) {
+                        this._leaveApplicationEventTriggered = true;
+                        this._bannerNativeApi.BannerListenerApi.sendLeaveApplicationEvent(this._bannerAdViewId);
+                    }
                 });
             } else if (this._platform === Platform.ANDROID) {
                 this._core.Android!.Intent.launch({
                     'action': 'android.intent.action.VIEW',
                     'uri': url
                 }).then(() => {
-                    this._bannerNativeApi.BannerListenerApi.sendLeaveApplicationEvent(this._bannerAdViewId);
+                    if (!this._leaveApplicationEventTriggered) {
+                        this._leaveApplicationEventTriggered = true;
+                        this._bannerNativeApi.BannerListenerApi.sendLeaveApplicationEvent(this._bannerAdViewId);
+                    }
                 });
             }
         }
