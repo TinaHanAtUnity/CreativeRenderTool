4af34e85f6e163ec91470eb08bbb06502f66556f
Andrew Konecny <andrewko@unity3d.com>
2019-10-15 16:45:25 -0700

    Update merge master script

--- tools/release-scripts/Staging/1.merge_master_changes.sh
+++ tools/release-scripts/Staging/1.merge_master_changes.sh
@@ -16,7 +16,11 @@ do
     git merge master --no-commit --no-ff
     if [ "$?" -eq "1" ]; then
         git status | grep 'deleted by us' | sed 's/^.*deleted by us: //g' | xargs git rm
+        git status | grep 'added by them' | sed 's/^.*added by them: //g' | xargs git rm
         echo "Resolve merge conflicts then proceed by pressing any key."
+        read answer < /dev/tty
+    else
+        echo "Nothing to resolve. Continue if there is nothing to edit."
         read answer < /dev/tty
     fi
 done <"$releases"
