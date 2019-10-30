7c21d1336495165c8eb69de1e3e968de1e54a7f0
Tina Han <yunduan@unity3d.com>
2019-10-30 15:18:08 -0700

    Fyber sdk detection works on iOS

--- src/ts/Core/Models/SdkDetectionInfo.ts
+++ src/ts/Core/Models/SdkDetectionInfo.ts
@@ -47,8 +47,8 @@ export class SdkDetectionInfo extends Model<ISdkDetectionInfo> {
         } else if (this._platform === Platform.IOS) {
             promises.push(this._core.ClassDetection.isClassPresent('GADMobileAds').then(result => this.set('AdMob', result)).catch(err => this.handleDeviceInfoError(err)));
             promises.push(this._core.ClassDetection.isClassPresent('MoPub').then(result => this.set('MoPub', result)).catch(err => this.handleDeviceInfoError(err)));
-            promises.push(this._core.ClassDetection.isClassPresent('SdkCClassNameOnIOS').then(result => this.set('IronSource', result)).catch(err => this.handleDeviceInfoError(err)));
-            promises.push(this._core.ClassDetection.isClassPresent('SdkDClassNameOnIOS').then(result => this.set('Fyber', result)).catch(err => this.handleDeviceInfoError(err)));
+            promises.push(this._core.ClassDetection.isClassPresent('IronSource').then(result => this.set('IronSource', result)).catch(err => this.handleDeviceInfoError(err)));
+            promises.push(this._core.ClassDetection.isClassPresent('FyberSDK').then(result => this.set('Fyber', result)).catch(err => this.handleDeviceInfoError(err)));
             promises.push(this._core.ClassDetection.isClassPresent('SdkEClassNameOnIOS').then(result => this.set('SafeDK', result)).catch(err => this.handleDeviceInfoError(err)));
         }
         return Promise.all(promises);
