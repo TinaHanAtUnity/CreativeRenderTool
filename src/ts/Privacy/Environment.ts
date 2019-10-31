348f28e159902f5eecd6953a11b0199b5e7a27f9
Janne Nikula <jannen@unity3d.com>
2019-10-31 16:58:53 +0200

    Remove duplicate PrivacyTemplate definition

--- src/ts/Privacy/Environment.ts
+++ src/ts/Privacy/Environment.ts
@@ -1,8 +1,7 @@
-export class Environment extends PrivacyTemplate {
+export class Environment {
     private _env: { [key: string]: unknown };
 
     constructor (env: { [key: string]: unknown }) {
-        super();
         this._env = env;
     }
 
@@ -15,5 +14,61 @@ export class Environment extends PrivacyTemplate {
         }
 
         return retObj;
+    }
+
+    public getValueFor(type: unknown): unknown {
+        if (typeof type !== 'string') {
+            return type;
+        }
+
+        const typeSplit = type.split('.');
+        switch (typeSplit[0]) {
+            case 'device':
+                switch (typeSplit[1]) {
+                    case 'platform':
+                        return 'android';
+                    case 'androidId':
+                        return 'fakeAndroidId';
+                    default:
+                        return type;
+                }
+            case 'user':
+                switch  (typeSplit[1]) {
+                    case 'advertisingId':
+                        return 'fakeAdvertisingId';
+                    case 'UUID':
+                        return 'fakeUUID';
+                    case 'locales':
+                        return ['en_FI', 'fi_EN'];
+                    case 'defaultLocale':
+                        return 'en_FI';
+                    default:
+                        return type;
+                }
+            case 'build':
+                switch (typeSplit[1]) {
+                    case 'apiLevel':
+                        return 12;
+                    case 'osVersion':
+                        return '8.0.1';
+                    default:
+                        return type;
+                }
+            case 'privacy':
+                switch (typeSplit[1]) {
+                    case 'settings':
+                        return {};
+                    case 'id':
+                        return '75349hfsksfh';
+                    case 'env':
+                        return {};
+                    case 'developerConsent':
+                        return {};
+                    default:
+                        return type;
+                }
+            default:
+                return type;
+        }
     }
 }
