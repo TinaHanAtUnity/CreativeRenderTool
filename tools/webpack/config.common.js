e6fccdc423467a92e7d7f25cdf0f852e683b30cc
Andrew Konecny <andrewko@unity3d.com>
2019-10-31 12:24:43 -0700

    Fix regex

--- tools/webpack/config.common.js
+++ tools/webpack/config.common.js
@@ -84,7 +84,7 @@ module.exports = {
                 ],
             },
             {
-                test: /\.(jpe?g|png|ttf||eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                test: /\.(jpe?g|png|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
                 use: 'base64-inline-loader'
             },
             {
