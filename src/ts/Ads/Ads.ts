fcffda33177fdefe2b6bd7a41b13fd8d6a7764ff
Niko Kontkanen <nikok@unity3d.com>
2019-11-04 13:25:45 +0200

    Add portrait video, store id & store name support for forced creative packs.

--- src/ts/Ads/Ads.ts
+++ src/ts/Ads/Ads.ts
@@ -610,13 +610,18 @@ export class Ads implements IAds {
                 response = CreativePackResponseIos;
             }
 
-            response = response.replace('{ICON_PLACEHOLDER}', json.gameIcon ? json.gameIcon : '');
-            response = response.replace('{ENDSCREEN_PLACEHOLDER}', json.endScreen ? json.endScreen : '');
-            response = response.replace('{ENDSCREEN_LANDSCAPE_PLACEHOLDER}', json.endScreenLandscape ? json.endScreenLandscape : '');
-            response = response.replace('{ENDSCREEN_PORTRAIT_PLACEHOLDER}', json.endScreenPortrait ? json.endScreenPortrait : '');
-            response = response.replace('{TRAILER_DOWNLOADABLE_PLACEHOLDER}', json.trailerDownloadable ? json.trailerDownloadable : '');
-            response = response.replace('{TRAILER_DOWNLOADABLE_SIZE}', json.trailerDownloadableSize ? json.trailerDownloadableSize.toString() : '0');
-            response = response.replace('{TRAILER_STREAMING_PLACEHOLDER}', json.trailerStreaming ? json.trailerStreaming : '');
+            response = response.replace('{ICON_PLACEHOLDER}', json.gameIcon || '');
+            response = response.replace('{ENDSCREEN_PLACEHOLDER}', json.endScreen || '');
+            response = response.replace('{ENDSCREEN_LANDSCAPE_PLACEHOLDER}', json.endScreenLandscape || '');
+            response = response.replace('{ENDSCREEN_PORTRAIT_PLACEHOLDER}', json.endScreenPortrait || '');
+            response = response.replace('{TRAILER_DOWNLOADABLE_PLACEHOLDER}', json.trailerDownloadable || '');
+            response = response.replace('{TRAILER_DOWNLOADABLE_SIZE}', json.trailerDownloadableSize ? json.trailerDownloadableSize.toString() : '1');
+            response = response.replace('{TRAILER_STREAMING_PLACEHOLDER}', json.trailerStreaming || '');
+            response = response.replace('{TRAILER_PORTRAIT_DOWNLOADABLE_PLACEHOLDER}', json.trailerPortraitDownloadable || '');
+            response = response.replace('{TRAILER_PORTRAIT_DOWNLOADABLE_SIZE}', json.trailerPortraitDownloadableSize ? json.trailerPortraitDownloadableSize.toString() : '1');
+            response = response.replace('{TRAILER_PORTRAIT_STREAMING_PLACEHOLDER}', json.trailerPortraitStreaming || '');
+            response = response.replace('{APPSTORE_ID_PLACEHOLDER}', json.appStoreId || '');
+            response = response.replace('{APPSTORE_NAME_PLACEHOLDER}', json.appStoreName || '');
 
             CampaignManager.setCampaignResponse(response);
         }
