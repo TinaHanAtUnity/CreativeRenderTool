39796e5d264bf2d8ae2da0384343302ac251bd51
Andrew Konecny <andrewko@unity3d.com>
2019-11-04 11:54:46 -0800

    wip deviceinfo

--- src/ts/Core/Models/__mocks__/DeviceInfo.ts
+++ src/ts/Core/Models/__mocks__/DeviceInfo.ts
@@ -4,5 +4,34 @@ export type DeviceInfoMock = Base & {
 };
 
 export const DeviceInfo = jest.fn(() => {
-    return <DeviceInfoMock><unknown>{};
+    return <DeviceInfoMock><unknown>{
+        getAdvertisingIdentifier: jest.fn().mockImplementation(() => ''),
+        getLimitedAdTracking: jest.fn().mockImplementation(() => false),
+        getModel: jest.fn().mockImplementation(() => ''),
+        getNetworkType: jest.fn().mockImplementation(() => Promise.resolve(0)),
+        getNetworkOperator: jest.fn().mockImplementation(() => Promise.resolve(null)),
+        getNetworkOperatorName: jest.fn().mockImplementation(() => Promise.resolve(null)),
+        getOsVersion: jest.fn().mockImplementation(() => ''),
+        getScreenWidth: jest.fn().mockImplementation(() => Promise.resolve(567)),
+        getScreenHeight: jest.fn().mockImplementation(() => Promise.resolve(1234)),
+        isRooted: jest.fn().mockImplementation(() => false),
+        isMadeWithUnity: jest.fn().mockImplementation(() => false),
+        getConnectionType: jest.fn().mockImplementation(() => Promise.resolve('')),
+        getTimeZone: jest.fn().mockImplementation(() => ''),
+        getFreeSpace: jest.fn().mockImplementation(() => Promise.resolve(123456789)),
+        getTotalSpace: jest.fn().mockImplementation(() => Promise.resolve(12345678)),
+        getLanguage: jest.fn().mockImplementation(() => 'en_US'),
+        getHeadset: jest.fn().mockImplementation(() => Promise.resolve(false)),
+        getDeviceVolume: jest.fn().mockImplementation(() => Promise.resolve(1)),
+        checkIsMuted: jest.fn().mockImplementation(() => Promise.resolve()),
+        getScreenBrightness: jest.fn().mockImplementation(() => Promise.resolve(100)),
+        getBatteryLevel: jest.fn().mockImplementation(() => Promise.resolve(0.5)),
+        getBatteryStatus: jest.fn().mockImplementation(() => Promise.resolve(1)),
+        getFreeMemory: jest.fn().mockImplementation(() => Promise.resolve(1234567)),
+        getTotalMemory: jest.fn().mockImplementation(() => Promise.resolve(12345678)),
+        getDTO: jest.fn().mockImplementation(() => Promise.resolve({})),
+        getAnonymousDTO: jest.fn().mockImplementation(() => Promise.resolve({})),
+        getStaticDTO: jest.fn().mockImplementation(() => Promise.resolve({})),
+        getAnonymousStaticDTO: jest.fn().mockImplementation(() => Promise.resolve({}))
+    };
 });
