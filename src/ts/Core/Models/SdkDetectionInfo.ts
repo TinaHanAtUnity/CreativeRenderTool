5afb1365643239b4d9a2380baf361f56e35083fb
Tina Han <yunduan@unity3d.com>
2019-11-05 14:06:00 -0800

    Resolve comments

--- src/ts/Core/Models/SdkDetectionInfo.ts
+++ src/ts/Core/Models/SdkDetectionInfo.ts
@@ -3,54 +3,54 @@ import {ICoreApi} from 'Core/ICore';
 import {ISchema, Model} from 'Core/Models/Model';
 
 export interface ISdkDetectionInfo {
-    AdMob: boolean;
-    MoPub: boolean;
-    IronSource: boolean;
-    Fyber: boolean;
-    SafeDK: boolean;
-    UnityEngine: boolean;
+    AdMob: boolean | undefined;
+    MoPub: boolean | undefined;
+    IronSource: boolean | undefined;
+    Fyber: boolean | undefined;
+    SafeDK: boolean | undefined;
+    UnityEngine: boolean | undefined;
 }
 
-const SdkAndroidClassMap = {
-    AdMob: 'com.google.ads.mediation.admob.AdMobAdapter',
-    MoPub: 'com.mopub.common.MoPub',
-    IronSource: 'com.ironsource.mediationsdk.IronSource',
-    Fyber: 'com.fyber.FairBid',
-    SafeDK: 'com.safedk.android.SafeDK',
-    UnityEngine: 'com.unity3d.player.UnityPlayer'
-};
+const SdkAndroidClassMap = new Map<string, string>([
+    ['com.google.ads.mediation.admob.AdMobAdapter', 'AdMob'],
+    ['com.mopub.common.MoPub', 'MoPub'],
+    ['com.ironsource.mediationsdk.IronSource', 'IronSource'],
+    ['com.fyber.FairBid', 'Fyber'],
+    ['com.safedk.android.SafeDK', 'SafeDK'],
+    ['com.unity3d.player.UnityPlayer', 'UnityEngine']
+]);
 
-const SdkiOSClassMap = {
-    AdMob: 'GADMobileAds',
-    MoPub: 'MoPub',
-    IronSource: 'IronSource',
-    Fyber: 'FyberSDK',
-    SafeDK: 'SafeDK',
-    UnityEngine: 'UnityAppController'
-};
+const SdkiOSClassMap = new Map<string, string>([
+    ['GADMobileAds', 'AdMob'],
+    ['MoPub', 'MoPub'],
+    ['IronSource', 'IronSource'],
+    ['FyberSDK', 'FyberSDK'],
+    ['SafeDK', 'SafeDK'],
+    ['UnityAppController', 'UnityEngine']
+]);
 
 export class SdkDetectionInfo extends Model<ISdkDetectionInfo> {
 
     public static Schema: ISchema<ISdkDetectionInfo> = {
-        AdMob: ['boolean'],
-        MoPub: ['boolean'],
-        IronSource: ['boolean'],
-        Fyber: ['boolean'],
-        SafeDK: ['boolean'],
-        UnityEngine: ['boolean']
+        AdMob: ['boolean', 'undefined'],
+        MoPub: ['boolean', 'undefined'],
+        IronSource: ['boolean', 'undefined'],
+        Fyber: ['boolean', 'undefined'],
+        SafeDK: ['boolean', 'undefined'],
+        UnityEngine: ['boolean', 'undefined']
     };
 
     protected _platform: Platform;
     protected _core: ICoreApi;
 
     constructor(platform: Platform, core: ICoreApi) {
-        super('MediationDetectionInfo', {
-            AdMob: ['boolean'],
-            MoPub: ['boolean'],
-            IronSource: ['boolean'],
-            Fyber: ['boolean'],
-            SafeDK: ['boolean'],
-            UnityEngine: ['boolean']
+        super('SdkDetectionInfo', {
+            AdMob: ['boolean', 'undefined'],
+            MoPub: ['boolean', 'undefined'],
+            IronSource: ['boolean', 'undefined'],
+            Fyber: ['boolean', 'undefined'],
+            SafeDK: ['boolean', 'undefined'],
+            UnityEngine: ['boolean', 'undefined']
         });
 
         this._platform = platform;
@@ -61,20 +61,28 @@ export class SdkDetectionInfo extends Model<ISdkDetectionInfo> {
         const promises: Promise<unknown>[] = [];
         let classNames: string[];
         if (this._platform === Platform.ANDROID) {
-            classNames = [SdkAndroidClassMap.AdMob, SdkAndroidClassMap.MoPub, SdkAndroidClassMap.IronSource, SdkAndroidClassMap.Fyber, SdkAndroidClassMap.SafeDK, SdkAndroidClassMap.UnityEngine];
+            classNames = Array.from(SdkAndroidClassMap.keys());
         } else if (this._platform === Platform.IOS) {
-            classNames = [SdkiOSClassMap.AdMob, SdkiOSClassMap.MoPub, SdkiOSClassMap.IronSource, SdkiOSClassMap.Fyber, SdkiOSClassMap.SafeDK, SdkiOSClassMap.UnityEngine];
+            classNames = Array.from(SdkiOSClassMap.keys());
         } else {
             classNames = [];
         }
         promises.push(this._core.ClassDetection.areClassesPresent(classNames)
-            .then(result => {
-                this.set('AdMob', result[0]);
-                this.set('MoPub', result[1]);
-                this.set('IronSource', result[2]);
-                this.set('Fyber', result[3]);
-                this.set('SafeDK', result[4]);
-                this.set('UnityEngine', result[5]);
+            .then(results => {
+                results.forEach(r => {
+                    let name: string = '';
+                    if (this._platform === Platform.ANDROID) {
+                        name = SdkAndroidClassMap.get(r.class)!;
+                    } else if (this._platform === Platform.IOS) {
+                        name = SdkiOSClassMap.get(r.class)!;
+                    }
+                    if (name === 'AdMob') { this.set('AdMob', r.found); }
+                    if (name === 'MoPub') { this.set('MoPub', r.found); }
+                    if (name === 'IronSource') { this.set('IronSource', r.found); }
+                    if (name === 'Fyber') { this.set('Fyber', r.found); }
+                    if (name === 'SafeDK') { this.set('SafeDK', r.found); }
+                    if (name === 'UnityEngine') { this.set('UnityEngine', r.found); }
+                });
             }).catch(err => this.handleDeviceInfoError(err)));
         return Promise.all(promises);
     }
--- src/ts/Core/Native/ClassDetection.ts
+++ src/ts/Core/Native/ClassDetection.ts
@@ -1,13 +1,18 @@
 import { ApiPackage, NativeApi } from 'Core/Native/Bridge/NativeApi';
 import { NativeBridge } from 'Core/Native/Bridge/NativeBridge';
 
+export interface IClassInfo {
+    class: string;
+    found: boolean;
+}
+
 export class ClassDetectionApi extends NativeApi {
 
     constructor(nativeBridge: NativeBridge) {
         super(nativeBridge, 'ClassDetection', ApiPackage.CORE);
     }
 
-    public areClassesPresent(classNames: string[]): Promise<boolean[]> {
-        return this._nativeBridge.invoke<boolean[]>(this._fullApiClassName, 'areClassesPresent', [classNames]);
+    public areClassesPresent(classNames: string[]): Promise<IClassInfo[]> {
+        return this._nativeBridge.invoke<IClassInfo[]>(this._fullApiClassName, 'areClassesPresent', [classNames]);
     }
 }
