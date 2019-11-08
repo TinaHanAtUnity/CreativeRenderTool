34bbf5ca1f60fcf187fec0e5e878dd2319ededfd
Scott Wise <scottw@unity3d.com>
2019-11-08 13:30:37 -0800

    Switch extension to programmatic event handler

--- src/ts/AR/EventHandlers/ARMRAIDEventHandler.ts
+++ src/ts/AR/EventHandlers/ARMRAIDEventHandler.ts
@@ -1,6 +1,6 @@
-import { PerformanceMRAIDEventHandler } from 'MRAID/EventHandlers/PerformanceMRAIDEventHandler';
+import { ProgrammaticMRAIDEventHandler } from 'MRAID/EventHandlers/ProgrammaticMRAIDEventHandler';
 
-export class ARMRAIDEventHandler extends PerformanceMRAIDEventHandler {
+export class ARMRAIDEventHandler extends ProgrammaticMRAIDEventHandler {
 
     public onPlayableAnalyticsEvent(timeFromShow: number, timeFromPlayableStart: number, backgroundTime: number, event: string, eventData: unknown): void {
         super.onPlayableAnalyticsEvent(timeFromShow, timeFromPlayableStart, backgroundTime, 'ar_' + event, eventData);
