7f75f05528770f8696041963f994d2a82aebb51a
Scott Wise <scottw@unity3d.com>
2019-11-07 16:27:15 -0800

    Fix ar creative click path

--- src/ts/AR/EventHandlers/ARMRAIDEventHandler.ts
+++ src/ts/AR/EventHandlers/ARMRAIDEventHandler.ts
@@ -1,6 +1,6 @@
-import { MRAIDEventHandler } from 'MRAID/EventHandlers/MRAIDEventHandler';
+import { PerformanceMRAIDEventHandler } from 'MRAID/EventHandlers/PerformanceMRAIDEventHandler';
 
-export class ARMRAIDEventHandler extends MRAIDEventHandler {
+export class ARMRAIDEventHandler extends PerformanceMRAIDEventHandler {
 
     public onPlayableAnalyticsEvent(timeFromShow: number, timeFromPlayableStart: number, backgroundTime: number, event: string, eventData: unknown): void {
         super.onPlayableAnalyticsEvent(timeFromShow, timeFromPlayableStart, backgroundTime, 'ar_' + event, eventData);
