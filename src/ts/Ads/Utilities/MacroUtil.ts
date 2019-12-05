ebb7a724dd71482490fe5910663d199037288bd4
Alice <alice041189@gmail.com>
2019-12-05 11:34:25 -0800

    correct the misspelling and add a comment to the class

--- src/ts/Ads/Utilities/MacroUtil.ts
+++ src/ts/Ads/Utilities/MacroUtil.ts
@@ -1,3 +1,4 @@
+// To look for the string in src and replace with the value in map
 export class MacroUtil {
     public static replaceMacro(src: string, map: {[macros: string]: string}): string {
         Object.keys(map).forEach((macros) => {
