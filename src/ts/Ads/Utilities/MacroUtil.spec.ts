29fd41e65928f35d824feb38939e2ea86901e7c3
Alice <alice041189@gmail.com>
2019-12-05 12:56:54 -0800

    correct misspelling

--- src/ts/Ads/Utilities/MacroUtil.spec.ts
+++ src/ts/Ads/Utilities/MacroUtil.spec.ts
@@ -1,9 +1,9 @@
 import {MacroUtil} from 'Ads/Utilities/MacroUtil';
 
 describe('MacroUtilTest', () => {
-    const macroReplace1 = 'here is to test replcement of {{ CREATIVE_URL }}';
+    const macroReplace1 = 'here is to test replacement of {{ CREATIVE_URL }}';
     const macroReplace2 = 'nothing should be replaced here';
-    const macroReplace3 = 'here is to test replcement of {{ OMID_IMPLEMENTOR }} and {{ OMID_API_VERSION }}';
+    const macroReplace3 = 'here is to test replacement of {{ OMID_IMPLEMENTOR }} and {{ OMID_API_VERSION }}';
 
     it('should replace CREATIVE_URL correctly', () => {
         expect(MacroUtil.replaceMacro(macroReplace1, {'{{ CREATIVE_URL }}': 'https://cdn.unityads.unity3d.com/playables/production/ios/9innings_23022018/index.html'})).toBe('here is to test replcement of https://cdn.unityads.unity3d.com/playables/production/ios/9innings_23022018/index.html');
