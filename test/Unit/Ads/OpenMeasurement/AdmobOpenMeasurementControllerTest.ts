6b458be7c8293c259209098466a4b073cd94bdbd
Scott Wise <scottw@unity3d.com>
2019-12-10 17:22:12 -0800

    Add pts metric test for admob om controller

--- test/Unit/Ads/OpenMeasurement/AdmobOpenMeasurementControllerTest.ts
+++ test/Unit/Ads/OpenMeasurement/AdmobOpenMeasurementControllerTest.ts
@@ -154,7 +154,7 @@ import { ProgrammaticTrackingService, AdmobMetric } from 'Ads/Utilities/Programm
                 sandbox.restore();
             });
 
-            it('impression should build adview and om impression object', () => {
+            it('should build adview and om impression object', () => {
                 const impressionDataAndroid = {'mediaType': 'video', 'viewport': {'width': 540, 'height': 960},
                 'adView': {'percentageInView': 100, 'geometry': {'x': 0, 'y': 200, 'width': 300, 'height': 300}, 'onScreenGeometry': {'x': 0, 'y': 200, 'width': 300, 'height': 300, 'obstructions': []}, 'measuringElement': false, 'reasons': []}};
                 const impressionDataIOS = {'mediaType': 'video', 'viewport': {'width': 1080, 'height': 1920},
@@ -189,6 +189,16 @@ import { ProgrammaticTrackingService, AdmobMetric } from 'Ads/Utilities/Programm
                     } else {
                         assert.deepEqual(JSON.stringify((<sinon.SinonStub>OpenMeasurementController.prototype.impression).getCall(0).args[0]), JSON.stringify(impressionDataIOS));
                     }
+                });
+            });
+
+            it('should send admob om impression pts metric', () => {
+
+                const omAdViewBuilder = new OpenMeasurementAdViewBuilder(campaign, deviceInfo, platform);
+
+                return omManager.admobImpression(omAdViewBuilder).then(() => {
+                    sinon.assert.calledOnce(<sinon.SinonStub>programmaticTrackingService.reportMetricEvent);
+                    sinon.assert.calledWith(<sinon.SinonStub>programmaticTrackingService.reportMetricEvent, 'admob_om_impression');
                 });
             });
         });
