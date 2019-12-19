6b3d1bc0823969f17832bb1afa8e2c0c5f491118
Alice <alice041189@gmail.com>
2019-12-18 21:18:08 -0800

    fix lint error

--- src/ts/Ads/Utilities/MacroUtil.ts
+++ src/ts/Ads/Utilities/MacroUtil.ts
@@ -1,11 +1,10 @@
 
 /**
  * Can be used to replace static data in our markup prior to dom injection
- * @param {string} src The markup
- * @param {Object} map The data to replace
  */
 export class MacroUtil {
     public static replaceMacro(src: string, map: {[macros: string]: string}): string {
+       // @param {string} src The markup; @param {Object} map The data to replace
         Object.keys(map).forEach((macros) => {
             src = src.replace(macros, map[macros]);
         });
