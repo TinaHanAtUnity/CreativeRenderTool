6a482347294fab186558d35a0a40566adadef876
Tristan Caron <tristan.caron@unity3d.com>
2020-01-08 14:28:10 -0500

    Remove unnecessary event handler

--- src/ts/AR/EventHandlers/ARMRAIDEventHandler.ts
+++
@@ -1,7 +0,0 @@
-import { ProgrammaticMRAIDEventHandler } from 'MRAID/EventHandlers/ProgrammaticMRAIDEventHandler';
-
-export class ARMRAIDEventHandler extends ProgrammaticMRAIDEventHandler {
-    public onPlayableAnalyticsEvent(timeFromShow: number, timeFromPlayableStart: number, backgroundTime: number, event: string, eventData: unknown): void {
-        super.onPlayableAnalyticsEvent(timeFromShow, timeFromPlayableStart, backgroundTime, event, eventData);
-    }
-}
