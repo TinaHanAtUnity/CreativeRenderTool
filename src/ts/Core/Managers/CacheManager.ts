5bd3fcacf269e043a939221e299f95f8efd9133f
Steven Bankhead <stevenb@unity3d.com>
2020-01-14 15:40:18 -0800

    update CacheManager to send diagnostics on unknown error

--- src/ts/Core/Managers/CacheManager.ts
+++ src/ts/Core/Managers/CacheManager.ts
@@ -371,7 +371,8 @@ export class CacheManager {
                     return;
 
                 case CacheError[CacheError.UNKNOWN_ERROR]:
-                    this.handleRetry(callback, url, error);
+                    Diagnostics.trigger("CacheError.UNKNOWN_ERROR", new Error("CacheError.UNKNOWN_ERROR"));
+                    this.fulfillCallback(url, CacheStatus.FAILED);
                     return;
 
                 default:
