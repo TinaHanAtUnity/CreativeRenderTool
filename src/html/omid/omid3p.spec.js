d6b273e655a52579072760ec1abb339474774666
Scott Wise <scottw@unity3d.com>
2020-01-27 10:52:56 -0800

    add a data check and some test skeleton

--- src/html/omid/omid3p.spec.js
+++ src/html/omid/omid3p.spec.js
@@ -1,20 +1,31 @@
 const OmidVerification = require('html/omid/omid3p.html');
 
 describe('omid3p', () => {
-    let omid3p;
-    let postMessageSpy;
 
-    beforeAll(() => {
-        omid3p = OmidVerification.substring(34, OmidVerification.length - 30);
-        eval(omid3p);
+    describe('omid3p api', () => {
+        let omid3p;
+        let postMessageSpy;
+
+        beforeAll(() => {
+            omid3p = OmidVerification.substring(34, OmidVerification.length - 30);
+            eval(omid3p);
+        });
+
+        beforeEach(() => {
+            postMessageSpy = jest.fn();
+            window.postMessage = postMessageSpy;
+        });
+
+        it('isSupported should return true', () => {
+            expect(window.omid3p.isSupported()).toEqual(true);
+        });
+
+        xit('adding an event listener should pass adjusted event string', () => {
+
+        });
     });
 
-    beforeEach(() => {
-        postMessageSpy = jest.fn();
-        window.postMessage = postMessageSpy;
-    });
+    describe('session start', () => {
 
-    it('isSupported should return true', () => {
-        expect(window.omid3p.isSupported()).toEqual(true);
     });
 });
