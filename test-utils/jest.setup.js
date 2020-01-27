0a8fc7d6be336974278ce27b781b47f354a0f3d8
Andrew Konecny <andrewko@unity3d.com>
2020-01-26 18:57:31 -0800

    Add comment explanation

--- test-utils/jest.setup.js
+++ test-utils/jest.setup.js
@@ -1,1 +1,6 @@
+/**
+ * This file is called from the jest.config.js. It's currently called from the setupFiles
+ * field and is run before every test file.
+ */
+
 require('../src/ts/Ads/Utilities/__mocks__/ProgrammaticTrackingService').MockPTS();
