79e2e649178de669c6b3299bed534c016f3ee114
Sergey Yanchi <sergeyyanchi@unity3d.com>
2020-01-27 11:28:49 -0800

    Revert "Merge pull request #12959 from Applifier/feature/ad-unit-tracker-v3"
    This reverts commit 8f21b129a51391ff884ed4bd2774e7cb96e6fc89, reversing
    changes made to c5261cb11decbe36bd5243e90fce7d48da5729da.

--- src/ts/Core/Native/__mocks__/StorageApi.ts
+++ src/ts/Core/Native/__mocks__/StorageApi.ts
@@ -1,12 +1,9 @@
 import { StorageApi as Base } from 'Core/Native/Storage';
-import { ObservableMock, Observable } from 'Core/Utilities/__mocks__/Observable';
 
 export type StorageApiMock = Base & {
-    onSet: ObservableMock;
 };
 
 export const StorageApi = jest.fn(() => {
     return <StorageApiMock>{
-        onSet: Observable()
     };
 });
