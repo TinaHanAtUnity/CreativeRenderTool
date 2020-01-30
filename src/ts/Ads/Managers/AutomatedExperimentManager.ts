1f9408c1cc0bc3d9aace0d21c953d4b03515448f
Tristan Caron <tristan.caron@unity3d.com>
2020-01-30 09:52:06 -0500

    Add v1 to baseUrl

--- src/ts/Ads/Managers/AutomatedExperimentManager.ts
+++ src/ts/Ads/Managers/AutomatedExperimentManager.ts
@@ -83,7 +83,7 @@ export class AutomatedExperimentManager {
     private _userInfo: UserInfo;
     private _gameSessionId: number;
 
-    private static readonly _baseUrl = 'https://auiopt.staging.unityads.unity3d.com/';
+    private static readonly _baseUrl = 'https://auiopt.staging.unityads.unity3d.com/v1/';
     private static readonly _createEndPoint = 'experiment';
     private static readonly _actionEndPoint = 'action';
     private static readonly _rewardEndPoint = 'reward';
