856e67d5e5ee1faa30dbfa91b5182a5f8ede8dd3
Andrew Konecny <andrewko@unity3d.com>
2020-02-07 15:19:18 -0800

    Add tools root to run new tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   roots: [
-    '<rootDir>/src'
+    '<rootDir>/src',
+    '<rootDir>/tools'
   ],
   moduleDirectories: [
     'node_modules',
