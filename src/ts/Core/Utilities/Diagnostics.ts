d96d85f70aaa1b9f1bed22be8a730e2bd47efb4b
Tina Han <yunduan@unity3d.com>
2020-02-12 16:55:44 -0800

    Correct typo and extract private function

--- src/ts/Core/Utilities/Diagnostics.ts
+++ src/ts/Core/Utilities/Diagnostics.ts
@@ -1,29 +1,34 @@
 import { INativeResponse } from 'Core/Managers/RequestManager';
 import { HttpKafka, KafkaCommonObjectType } from 'Core/Utilities/HttpKafka';
-import { ABGroup, DignosticsRampDown } from 'Core/Models/ABGroup';
+import { ABGroup, DiagnosticsRampDown } from 'Core/Models/ABGroup';
 
 export class Diagnostics {
     public static trigger(type: string, error: {}): Promise<INativeResponse> {
-        if (Diagnostics._abGroup === undefined || DignosticsRampDown.isValid(Diagnostics._abGroup)) {
-            // ElasticSearch schema generation can result in dropping errors if root values are not the same type across errors
-            if (!error || typeof error !== 'object' || Array.isArray(error)) {
-                error = {
-                    value: error
-                };
-            }
-
-            const kafkaObject: { [key: string]: unknown } = {};
-            kafkaObject.type = type;
-            kafkaObject[type] = error;
-            kafkaObject.timestamp = Date.now();
-
-            return HttpKafka.sendEvent('ads.sdk2.diagnostics', KafkaCommonObjectType.ANONYMOUS, kafkaObject);
+        if (Diagnostics.shouldNotSendEvents()) {
+            return Promise.resolve(<INativeResponse>{});
         }
-        return Promise.resolve(<INativeResponse>{});
+
+        // ElasticSearch schema generation can result in dropping errors if root values are not the same type across errors
+        if (!error || typeof error !== 'object' || Array.isArray(error)) {
+            error = {
+                value: error
+            };
+        }
+
+        const kafkaObject: { [key: string]: unknown } = {};
+        kafkaObject.type = type;
+        kafkaObject[type] = error;
+        kafkaObject.timestamp = Date.now();
+
+        return HttpKafka.sendEvent('ads.sdk2.diagnostics', KafkaCommonObjectType.ANONYMOUS, kafkaObject);
     }
 
     public static setAbGroup(group: ABGroup) {
         Diagnostics._abGroup = group;
+    }
+
+    private static shouldNotSendEvents(): boolean {
+        return Diagnostics._abGroup !== undefined && DiagnosticsRampDown.isValid(Diagnostics._abGroup) === false;
     }
     private static _abGroup: ABGroup | undefined;
 }
