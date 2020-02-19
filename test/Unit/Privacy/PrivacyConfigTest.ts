926dd2db40cf70aecf33d07bb69b518104bf6fe5
Pekka Palmu <pekka.palmu@gmail.com>
2020-02-19 14:31:37 +0200

    Add PrivacyConfig tests

---
+++ test/Unit/Privacy/PrivacyConfigTest.ts
@@ -0,0 +1,33 @@
+import { assert } from 'chai';
+import 'mocha';
+import { PrivacyConfig } from 'Privacy/PrivacyConfig';
+
+describe('PrivacyConfigTest', () => {
+    let privacyConfig: PrivacyConfig;
+    const flow = { testFlowValue: 'testFlowValue'};
+    const userSettings = { ads: false, external: false, gameExp: false, agreedOverAgeLimit: false};
+    const env = { testEnvValue: 'testEnvValue'};
+    const html = '<html></html>';
+
+    beforeEach(() => {
+        privacyConfig = new PrivacyConfig(flow, userSettings, env, html);
+    });
+
+    context('Created as expected', () => {
+        it ('Flow', () => {
+           assert.deepEqual(privacyConfig.getFlow(), flow);
+        });
+
+        it ('UserSettings', () => {
+           assert.deepEqual(privacyConfig.getUserSettings(), userSettings);
+        });
+
+        it ('Environment', () => {
+            assert.deepEqual(privacyConfig.getEnv(), env);
+        });
+
+        it ('Html', () => {
+            assert.deepEqual(privacyConfig.getHtml(), html);
+        });
+    });
+});
