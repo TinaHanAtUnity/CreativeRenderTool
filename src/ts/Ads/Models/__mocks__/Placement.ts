209d9fe1649de0a862b5d101013ff5e5e7cb2cdb
Scott Wise <scottw@unity3d.com>
2020-02-22 00:14:23 -0800

    Add tests

--- src/ts/Ads/Models/__mocks__/Placement.ts
+++ src/ts/Ads/Models/__mocks__/Placement.ts
@@ -4,11 +4,13 @@ import Mock = jest.Mock;
 export type PlacementMock = Base & {
     allowSkip: Mock<boolean>;
     getId: Mock<string>;
+    muteVideo: Mock<boolean>;
 };
 
 export const Placement = jest.fn(() => {
     return <PlacementMock>{
         allowSkip: jest.fn(),
-        getId: jest.fn()
+        getId: jest.fn(),
+        muteVideo: jest.fn()
     };
 });
