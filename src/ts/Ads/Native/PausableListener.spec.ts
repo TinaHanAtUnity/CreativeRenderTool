e205cf2a035300c120b08a7cf8b96a0ba6e56eb8
Richard Hawkins <richardh@unity3d.com>
2020-03-17 17:58:20 -0500

    Fix tests

--- src/ts/Ads/Native/PausableListener.spec.ts
+++ src/ts/Ads/Native/PausableListener.spec.ts
@@ -5,45 +5,36 @@ import { NativeBridge, NativeBridgeMock } from 'Core/Native/Bridge/__mocks__/Nat
 
 describe('PausableListenerTest', () => {
     const methodNameCallIndex = 1;
-    const expectedMethodNames: string[] = [
-        'sendReadyEvent',
-        'sendStartEvent',
-        'sendFinishEvent',
-        'sendClickEvent',
-        'sendPlacementStateChangedEvent',
-        'sendErrorEvent'
-    ];
 
-    let nativeBridge: NativeBridgeMock;
-    let listener: PausableListenerApi;
-
-    beforeEach(() => {
-        nativeBridge = new NativeBridge();
-        nativeBridge.invoke = jest.fn().mockReturnValue(Promise.resolve(() => {}));
-        listener = new PausableListenerApi(nativeBridge);
-    });
 
     describe('when created', () => {
+        let nativeBridge: NativeBridgeMock;
+        let listener: PausableListenerApi;
+
+        beforeEach(() => {
+            nativeBridge = new NativeBridge();
+            nativeBridge.invoke = jest.fn().mockReturnValue(Promise.resolve());
+            listener = new PausableListenerApi(nativeBridge);
+        });
+
         it('should be unpaused', () => {
             listener.sendReadyEvent('video');
-            listener.sendStartEvent('video');
-            listener.sendFinishEvent('video', FinishState.COMPLETED);
-            listener.sendClickEvent('video');
-            listener.sendPlacementStateChangedEvent('video', 'WAITING', 'READY');
-            listener.sendErrorEvent('test', 'error details');
-
-            expect(nativeBridge.invoke.mock.calls.length).toBe(expectedMethodNames.length);
-            for (const methodName of expectedMethodNames) {
-                const call = nativeBridge.invoke.mock.calls.shift();
-                expect(methodName).toBe(call[methodNameCallIndex]);
-            }
+            expect(nativeBridge.invoke.mock.calls.length).toBe(1);
         });
     });
 
     describe('when paused', () => {
-        it('should not send events', () => {
-            listener.pauseEvents();
+        let nativeBridge: NativeBridgeMock;
+        let listener: PausableListenerApi;
 
+        beforeEach(() => {
+            nativeBridge = new NativeBridge();
+            nativeBridge.invoke = jest.fn().mockReturnValue(Promise.resolve());
+            listener = new PausableListenerApi(nativeBridge);
+            listener.pauseEvents();
+        });
+
+        it('should not send events', () => {
             listener.sendReadyEvent('video');
             listener.sendStartEvent('video');
             listener.sendFinishEvent('video', FinishState.COMPLETED);
@@ -55,40 +46,39 @@ describe('PausableListenerTest', () => {
         });
     });
 
-    describe('when unpaused', () => {
-        it('should send queued events', () => {
-            listener.pauseEvents();
+    describe('when resumed', () => {
+        let nativeBridge: NativeBridgeMock;
+        let listener: PausableListenerApi;
 
+        beforeEach(() => {
+            nativeBridge = new NativeBridge();
+            nativeBridge.invoke = jest.fn().mockReturnValue(Promise.resolve());
+            listener = new PausableListenerApi(nativeBridge);
+            listener.pauseEvents();
             listener.sendReadyEvent('video');
             listener.sendStartEvent('video');
             listener.sendFinishEvent('video', FinishState.COMPLETED);
             listener.sendClickEvent('video');
             listener.sendPlacementStateChangedEvent('video', 'WAITING', 'READY');
             listener.sendErrorEvent('test', 'error details');
-
-            listener.resumeEvents();
-
-            expect(nativeBridge.invoke.mock.calls.length).toBe(expectedMethodNames.length);
-            for (const methodName of expectedMethodNames) {
-                const call = nativeBridge.invoke.mock.calls.shift();
-                expect(methodName).toBe(call[methodNameCallIndex]);
-            }
         });
 
-        it('should send new events', () => {
-            listener.pauseEvents();
-
-            listener.sendReadyEvent('video');
-            listener.sendStartEvent('video');
-            listener.sendFinishEvent('video', FinishState.COMPLETED);
-
+        it('should send all events', () => {
             listener.resumeEvents();
+            expect(nativeBridge.invoke.mock.calls.length).toBe(6);
+        });
 
-            listener.sendClickEvent('video');
-            listener.sendPlacementStateChangedEvent('video', 'WAITING', 'READY');
-            listener.sendErrorEvent('test', 'error details');
+        it('should send the events in the order recieved', () => {
+            listener.resumeEvents();
+            const expectedMethodNames: string[] = [
+                'sendReadyEvent',
+                'sendStartEvent',
+                'sendFinishEvent',
+                'sendClickEvent',
+                'sendPlacementStateChangedEvent',
+                'sendErrorEvent'
+            ];
 
-            expect(nativeBridge.invoke.mock.calls.length).toBe(expectedMethodNames.length);
             for (const methodName of expectedMethodNames) {
                 const call = nativeBridge.invoke.mock.calls.shift();
                 expect(methodName).toBe(call[methodNameCallIndex]);
