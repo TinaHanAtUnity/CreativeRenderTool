0c5a2d27786d1920555cbbaa22500ecd2ee7c18c
Andrew Konecny <andrewko@unity3d.com>
2020-03-19 16:52:01 -0700

    Disable XHR Request ABTest

--- src/ts/Core/Models/ABGroup.ts
+++ src/ts/Core/Models/ABGroup.ts
@@ -70,7 +70,7 @@ export const LoadExperiment = new ABTest(13);
 export const LoadRefreshV4 = new ABTest(14);
 export const PrivacySDKTest = new ABTest(16);
 export const DiagnosticsRampDown = new ABTest(15);
-export const AuctionXHR = new ABTest(18);
+export const AuctionXHR = new DisabledABTest();
 
 // Disabled AB Tests
 export const OpenMeasurementTest = new DisabledABTest();
