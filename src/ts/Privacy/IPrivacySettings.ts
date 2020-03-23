0219a9049d020bf921c39691432fcac94654e136
Igor Lunev <iglunev@gmail.com>
2020-03-23 15:06:59 +0200

    Move agreementMethod to IUserPrivacySettings interface

--- src/ts/Privacy/IPrivacySettings.ts
+++ src/ts/Privacy/IPrivacySettings.ts
@@ -1,19 +1,12 @@
 export interface IPrivacySettings {
     user: IUserPrivacySettings;
     env: { [key: string]: unknown };
-    lastInteraction: IUserLastInteraction;
 }
 
 export interface IUserPrivacySettings {
     ads: boolean;
-    agreementMethod: string;
     external: boolean;
     gameExp: boolean;
     agreedOverAgeLimit: boolean;
-}
-
-export interface IUserLastInteraction {
-    id: string;
-    type: string;
-    timestamp: string;
+    agreementMethod?: string;
 }
