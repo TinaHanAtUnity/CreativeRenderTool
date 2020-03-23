7f2729c952d09dfc16a8a2bd798c932cd0349627
Pekka Palmu <pekka.palmu@gmail.com>
2020-03-23 12:53:11 +0200

    Add class for fetching privacy env metadata

---
+++ src/ts/Privacy/PrivacyEnvironment.ts
@@ -0,0 +1,28 @@
+import { MetaData } from 'Core/Utilities/MetaData';
+
+export class PrivacyEnvironment {
+    private static _privacyEnvironment: { [key: string]: unknown } = {};
+
+    public static setup(metaData: MetaData): Promise<void[]> {
+        return metaData.getKeys('privacyenv').then((keys) => {
+            const promises: Promise<void>[] = [];
+            keys.forEach((key) => {
+                promises.push(metaData.get('privacyenv.' + key, false).then(([found, value]) => {
+                    if (found) {
+                        this._privacyEnvironment[key] = value;
+                    }
+                }));
+            });
+
+            return Promise.all(promises);
+        });
+    }
+
+    public static get<T>(key: string): T {
+        return <T>PrivacyEnvironment._privacyEnvironment[key];
+    }
+
+    public static isSet(key: string): boolean {
+        return PrivacyEnvironment._privacyEnvironment.hasOwnProperty(key)
+    }
+}
