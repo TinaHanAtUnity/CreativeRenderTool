04c85ae5badbae23b5b26831da41e8293f8f5a66
Brahim Hadriche <brahim.hadriche@unity3d.com>
2020-03-25 09:50:25 -0400

    Color Util improvement

--- test/Unit/Core/Utilities/ColorTest.ts
+++ test/Unit/Core/Utilities/ColorTest.ts
@@ -4,15 +4,15 @@ import { Color } from 'Core/Utilities/Color';
 
 describe('Color Test', () => {
     it('should parse hex colors correctly', () => {
-        assert.deepEqual(Color.hexToRgba('#df80b8'), {r: 223, g: 128, b: 184, a: 255}, 'Color was parsed incorrectly');
-        assert.deepEqual(Color.hexToRgba('#0c36d4'), {r: 12, g: 54, b: 212, a: 255}, 'Color was parsed incorrectly');
-        assert.deepEqual(Color.hexToRgba('a6ca5c'), {r: 166, g: 202, b: 92, a: 255}, 'Color was parsed incorrectly');
-        assert.deepEqual(Color.hexToRgba('#9d72a9ca'), {r: 157, g: 114, b: 169, a: 202}, 'Color was parsed incorrectly');
-        assert.deepEqual(Color.hexToRgba('6d4488e2'), {r: 109, g: 68, b: 136, a: 226}, 'Color was parsed incorrectly');
-        assert.deepEqual(Color.hexToRgba('789'), {r: 119, g: 136, b: 153, a: 255}, 'Color was parsed incorrectly');
-        assert.deepEqual(Color.hexToRgba('36d4'), {r: 51, g: 102, b: 221, a: 68}, 'Color was parsed incorrectly');
-        assert.deepEqual(Color.hexToRgba('##0c36d4'), {r: 0, g: 0, b: 0, a: 0}, 'Invalid color was validated incorrectly');
-        assert.deepEqual(Color.hexToRgba('48F4c8x'), {r: 0, g: 0, b: 0, a: 0}, 'Invalid color was validated incorrectly');
+        assert.deepEqual(Color.hexToColor('#df80b8'), {r: 223, g: 128, b: 184, a: 255}, 'Color was parsed incorrectly');
+        assert.deepEqual(Color.hexToColor('#0c36d4'), {r: 12, g: 54, b: 212, a: 255}, 'Color was parsed incorrectly');
+        assert.deepEqual(Color.hexToColor('a6ca5c'), {r: 166, g: 202, b: 92, a: 255}, 'Color was parsed incorrectly');
+        assert.deepEqual(Color.hexToColor('#9d72a9ca'), {r: 157, g: 114, b: 169, a: 202}, 'Color was parsed incorrectly');
+        assert.deepEqual(Color.hexToColor('6d4488e2'), {r: 109, g: 68, b: 136, a: 226}, 'Color was parsed incorrectly');
+        assert.deepEqual(Color.hexToColor('789'), {r: 119, g: 136, b: 153, a: 255}, 'Color was parsed incorrectly');
+        assert.deepEqual(Color.hexToColor('36d4'), {r: 51, g: 102, b: 221, a: 68}, 'Color was parsed incorrectly');
+        assert.deepEqual(Color.hexToColor('##0c36d4'), {r: 0, g: 0, b: 0, a: 0}, 'Invalid color was validated incorrectly');
+        assert.deepEqual(Color.hexToColor('48F4c8x'), {r: 0, g: 0, b: 0, a: 0}, 'Invalid color was validated incorrectly');
     });
 
     it('should convert css rgba colors from hex colors correctly', () => {
