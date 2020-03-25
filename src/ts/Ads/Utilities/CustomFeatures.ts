2fc89e2eca78488f2d07bbe0ef3a9d30f65cf30a
Tina Han <yunduan@unity3d.com>
2020-03-25 12:29:09 -0700

    combine creative ids to one json file

--- src/ts/Ads/Utilities/CustomFeatures.ts
+++ src/ts/Ads/Utilities/CustomFeatures.ts
@@ -1,6 +1,6 @@
 import CheetahGamesJson from 'json/custom_features/CheetahGames.json';
 import BitmangoGamesJson from 'json/custom_features/BitmangoGames.json';
-import ApplePMPCreativesJson from 'json/custom_features/ApplePMPCreatives.json';
+import NestedIframePlayableCreativeJson from 'json/custom_features/NestedIframePlayableCreatives.json';
 import Game7GamesJson from 'json/custom_features/Game7Games.json';
 import LionStudiosGamesJson from 'json/custom_features/LionStudiosGames.json';
 import MobilityWareGamesJson from 'json/custom_features/MobilityWareGames.json';
@@ -36,21 +36,7 @@ export class CustomFeatures {
     }
 
     public static isNestedIframePlayable(creativeId: string | undefined) {
-        return  creativeId === '109455881' ||
-                creativeId === '109455877' ||
-                creativeId === '109091853' ||
-                creativeId === '109091754' ||
-                creativeId === '114617576' || // Hellfest
-                creativeId === '114617336' || // Hellfest
-                creativeId === '145941071' || // Miller Lite Fallback
-                creativeId === '145940860' || // Miller Lite Fallback
-                creativeId === '147367465' || // Carnival Creative
-                creativeId === '151099348' ||
-                creativeId === '151338976' ||
-                creativeId === '151337994' ||
-                creativeId === '152919353' ||
-                creativeId === '153119177' ||
-                creativeId !== undefined && this.existsInList(ApplePMPCreativesJson, creativeId);
+        return creativeId !== undefined && this.existsInList(NestedIframePlayableCreativeJson, creativeId);
     }
 
     public static isLoopMeSeat(seatId: number | undefined): boolean {
