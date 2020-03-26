684f33a837c767d52e60f577bd3b56e70e2d7c5a
Andrew Konecny <andrewko@unity3d.com>
2020-03-26 12:06:00 -0700

    [Auction V6] Add Mocks

--- src/ts/Core/Native/__mocks__/SdkApi.ts
+++ src/ts/Core/Native/__mocks__/SdkApi.ts
@@ -2,10 +2,12 @@ import { SdkApi as Base } from 'Core/Native/Sdk';
 
 export type SdkApiMock = Base & {
     logDebug: jest.Mock;
+    logInfo: jest.Mock;
 };
 
 export const SdkApi = jest.fn(() => {
     return <SdkApiMock>{
-        logDebug: jest.fn()
+        logDebug: jest.fn(),
+        logInfo: jest.fn()
     };
 });
--- src/ts/Core/Native/__mocks__/StorageApi.ts
+++ src/ts/Core/Native/__mocks__/StorageApi.ts
@@ -2,12 +2,14 @@ import { StorageApi as Base } from 'Core/Native/Storage';
 import { ObservableMock, Observable } from 'Core/Utilities/__mocks__/Observable';
 
 export type StorageApiMock = Base & {
+    get: jest.Mock;
     onSet: ObservableMock;
     getKeys: jest.Mock;
 };
 
 export const StorageApi = jest.fn(() => {
     return <StorageApiMock>{
+        get: jest.fn().mockImplementation(() => Promise.resolve()),
         onSet: Observable(),
         getKeys: jest.fn().mockResolvedValue([])
     };
