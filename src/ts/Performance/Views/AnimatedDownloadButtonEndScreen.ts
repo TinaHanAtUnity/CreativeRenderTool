f48cf237c01d8fb1a9c0cef91dd1b26c3b05c2ad
Brahim Hadriche <brahim.hadriche@unity3d.com>
2020-04-02 13:37:06 -0400

    Remove renderColorTheme from AnimatedDownloadButtonEndScreen

--- src/ts/Performance/Views/AnimatedDownloadButtonEndScreen.ts
+++ src/ts/Performance/Views/AnimatedDownloadButtonEndScreen.ts
@@ -7,8 +7,6 @@ import { IExperimentActionChoice } from 'Ads/Models/AutomatedExperiment';
 import { ButtonAnimationsExperiment, ButtonExperimentDeclaration } from 'Ads/Models/AutomatedExperimentsList';
 import { AUIMetric, SDKMetrics } from 'Ads/Utilities/SDKMetrics';
 import { Color } from 'Core/Utilities/Color';
-import { ImageAnalysis } from 'Performance/Utilities/ImageAnalysis';
-import { IColorTheme } from 'Performance/Utilities/Swatch';
 
 export class AnimatedDownloadButtonEndScreen extends PerformanceEndScreen {
     private _animation: string;
@@ -68,76 +66,5 @@ export class AnimatedDownloadButtonEndScreen extends PerformanceEndScreen {
         // for example).
         element.classList.remove('on-show');
         setTimeout(() => element.classList.add('on-show'), 0);
-    }
-
-    private renderColorTheme() {
-        const portraitImage = this._campaign.getPortrait();
-        const landscapeImage = this._campaign.getLandscape();
-        const squareImage = this._campaign.getSquare();
-
-        const deviceInfo = this._core.DeviceInfo;
-        Promise.all([deviceInfo.getScreenWidth(), deviceInfo.getScreenHeight()])
-        .then(([screenWidth, screenHeight]) => {
-            const isLandscape = screenWidth > screenHeight;
-            let image;
-            if (squareImage) {
-                image = squareImage;
-            } else if (isLandscape && portraitImage) {
-                image = portraitImage; // when the device is in landscape mode, we are showing a portrait image
-            } else if (landscapeImage) {
-                image = landscapeImage;
-            } else {
-                image = portraitImage;
-            }
-
-            if (image) {
-                ImageAnalysis.getImageSrc(this._core.Cache, image)
-                    .then(ImageAnalysis.analyseImage)
-                    .then(swatches => {
-                    if (!swatches || !swatches.length) {
-                        SDKMetrics.reportMetricEvent(AUIMetric.InvalidEndscreenColorTintSwitches);
-                        return;
-                    }
-
-                    const baseColorTheme = swatches[0].getColorTheme();
-                    const secondaryColorTheme = ((swatches.length > 1) ? swatches[1] : swatches[0]).getColorTheme();
-                    this.applyColorTheme(baseColorTheme, secondaryColorTheme);
-                }).catch((msg: string) => {
-                    SDKMetrics.reportMetricEventWithTags(AUIMetric.EndscreenColorTintError, {
-                        'msg': msg
-                    });
-                });
-            }
-        });
-    }
-
-    private applyColorTheme(baseColorTheme: IColorTheme, secondaryColorTheme: IColorTheme): void {
-        if (!baseColorTheme.light || !baseColorTheme.medium || !baseColorTheme.dark ||
-            !secondaryColorTheme.light || !secondaryColorTheme.medium || !secondaryColorTheme.dark) {
-            SDKMetrics.reportMetricEvent(AUIMetric.InvalidEndscreenColorTintTheme);
-            return;
-        }
-
-        const backgroundElement: HTMLElement | null = this._container.querySelector('.end-screen-info-background');
-        const downloadContainer: HTMLElement | null = this._container.querySelector('.download-container');
-        const gameNameContainer: HTMLElement | null = this._container.querySelector('.name-container');
-        const gameRatingContainer: HTMLElement | null = this._container.querySelector('.game-rating-count');
-        const privacyIconContainer: HTMLElement | null = this._container.querySelector('.bottom-container .icon-gdpr');
-        const unityIconContainer: HTMLElement | null = this._container.querySelector('.bottom-container .unityads-logo');
-        const chinaAdvertisementElement: HTMLElement | null = this._container.querySelector('.bottom-container .china-advertisement');
-
-        if (backgroundElement && downloadContainer && gameNameContainer && gameRatingContainer && privacyIconContainer && unityIconContainer && chinaAdvertisementElement) {
-            const secondary = Color.lerp(secondaryColorTheme.light, secondaryColorTheme.medium, 0.3);
-            const baseDark = baseColorTheme.dark.toCssRgb();
-            backgroundElement.style.background = `linear-gradient(${secondary.toCssRgb()},${baseColorTheme.light.toCssRgb()})`;
-            downloadContainer.style.background = baseColorTheme.medium.toCssRgb();
-            gameNameContainer.style.color = baseDark;
-            gameRatingContainer.style.color = baseDark;
-            privacyIconContainer.style.color = baseDark;
-            unityIconContainer.style.color = baseDark;
-            chinaAdvertisementElement.style.color = baseDark;
-        } else {
-            SDKMetrics.reportMetricEvent(AUIMetric.EndscreenColorTintThemingFailed);
-        }
     }
 }
