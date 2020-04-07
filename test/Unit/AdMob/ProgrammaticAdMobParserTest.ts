ce00f1490fe6ac7387fc1e91e16a0724ac5de48b
Andrew Konecny <andrewko@unity3d.com>
2020-04-07 12:29:10 -0700

    Reduce Admob Campaign Parsing Time

--- test/Unit/AdMob/ProgrammaticAdMobParserTest.ts
+++ test/Unit/AdMob/ProgrammaticAdMobParserTest.ts
@@ -1,5 +1,6 @@
 import { AdMobCampaign } from 'AdMob/Models/AdMobCampaign';
 import { ProgrammaticAdMobParser } from 'AdMob/Parsers/ProgrammaticAdMobParser';
+import { AuctionPlacement } from 'Ads/Models/AuctionPlacement';
 import { AuctionResponse } from 'Ads/Models/AuctionResponse';
 import { Session } from 'Ads/Models/Session';
 import { Backend } from 'Backend/Backend';
@@ -7,23 +8,21 @@ import { assert } from 'chai';
 import { Platform } from 'Core/Constants/Platform';
 import { ICore } from 'Core/ICore';
 import { RequestManager } from 'Core/Managers/RequestManager';
+import { CoreConfiguration } from 'Core/Models/CoreConfiguration';
 import { NativeBridge } from 'Core/Native/Bridge/NativeBridge';
 import { SdkApi } from 'Core/Native/Sdk';
-import { FileId } from 'Core/Utilities/FileId';
+import { TestFixtures } from 'TestHelpers/TestFixtures';
+
 import ValidAdMobCampaign from 'json/campaigns/admob/ValidAdMobCampaign.json';
+
 import 'mocha';
 import * as sinon from 'sinon';
-import { TestFixtures } from 'TestHelpers/TestFixtures';
-import { AuctionPlacement } from 'Ads/Models/AuctionPlacement';
-import { CoreConfiguration } from 'Core/Models/CoreConfiguration';
 
 [Platform.ANDROID, Platform.IOS].forEach(platform => {
     describe('ProgrammaticAdMobParser', () => {
         const placementId = 'TestPlacement';
         const mediaId = 'o2YMT0Cmps6xHiOwNMeCrH';
         const correlationId = '583dfda0d933a3630a53249c';
-        const url = 'https://r2---sn-n4v7knll.googlevideo.com/videoplayback?id=a6e915b5b0f41a1c&itag=22&source=youtube&requiressl=yes&mm=31&mn=sn-n4v7knll&ms=au&mv=m&pl=19&ei=eo3rWuGXD8-KuAL6oLvQAQ&susc=yti&mime=video/mp4&lmt=1518153041357987&mt=1525386488&ip=4.14.109.2&ipbits=0&expire=1525415418&sparams=ip,ipbits,expire,id,itag,source,requiressl,mm,mn,ms,mv,pl,ei,susc,mime,lmt&signature=4834094C1C09F34DE9D6473658D0B1EE75DB3E10.830B2F45714128B27549A3B15E8BE3CB8EFCBE19&key=ck2';
-        const urlNoMime = 'https://r2---sn-n4v7knll.googlevideo.com/videoplayback?id=a6e915b5b0f41a1c&itag=22&source=youtube&requiressl=yes&mm=31&mn=sn-n4v7knll&ms=au&mv=m&pl=19&ei=eo3rWuGXD8-KuAL6oLvQAQ&susc=yti&lmt=1518153041357987&mt=1525386488&ip=4.14.109.2&ipbits=0&expire=1525415418&sparams=ip,ipbits,expire,id,itag,source,requiressl,mm,mn,ms,mv,pl,ei,susc,mime,lmt&signature=4834094C1C09F34DE9D6473658D0B1EE75DB3E10.830B2F45714128B27549A3B15E8BE3CB8EFCBE19&key=ck2';
 
         let parser: ProgrammaticAdMobParser;
         let backend: Backend;
@@ -31,7 +30,6 @@ import { CoreConfiguration } from 'Core/Models/CoreConfiguration';
         let core: ICore;
         let request: RequestManager;
         let session: Session;
-        let setFileIdSpy: sinon.SinonSpy;
 
         describe('parsing a campaign', () => {
             let campaign: AdMobCampaign;
@@ -58,112 +56,7 @@ import { CoreConfiguration } from 'Core/Models/CoreConfiguration';
                 session = TestFixtures.getSession();
 
                 parser = new ProgrammaticAdMobParser(core);
-                setFileIdSpy = sinon.spy(FileId, 'setFileID');
-                (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(url));
             });
-
-            afterEach(() => {
-                setFileIdSpy.restore();
-            });
-
-            if (platform === Platform.ANDROID) {
-                describe('on Android', () => {
-
-                    beforeEach(() => {
-                        sinon.stub(nativeBridge, 'getPlatform').returns(Platform.ANDROID);
-                    });
-
-                    describe('without a mime type in url', () => {
-
-                        beforeEach(() => {
-                            (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(urlNoMime));
-                            return parse(ValidAdMobCampaign);
-                        });
-
-                        it('should FileId.setFileId without a mp4 mime type', () => {
-                            sinon.assert.calledWith(setFileIdSpy, urlNoMime, 'G2KkvNWTNuU');
-                        });
-                    });
-
-                    describe('with a mime type in url', () => {
-                        beforeEach(() => {
-                            (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(url));
-                            return parse(ValidAdMobCampaign);
-                        });
-
-                        it('should FileId.setFileId without a mp4 mime type', () => {
-                            sinon.assert.calledWith(setFileIdSpy, url, 'G2KkvNWTNuU');
-                        });
-                    });
-
-                    describe('should cache', () => {
-
-                        beforeEach(() => {
-                            (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(url));
-                            return parse(ValidAdMobCampaign);
-                        });
-
-                        afterEach(() => {
-                            setFileIdSpy.resetHistory();
-                        });
-
-                        it('and call FileId.setFileId', () => {
-                            sinon.assert.calledOnce(setFileIdSpy);
-                            sinon.assert.calledWith(setFileIdSpy, url, 'G2KkvNWTNuU');
-                        });
-                    });
-
-                });
-            }
-
-            if (platform === Platform.IOS) {
-                describe('on iOS', () => {
-
-                    beforeEach(() => {
-                        sinon.stub(nativeBridge, 'getPlatform').returns(Platform.IOS);
-                    });
-
-                    describe('without a mime type in url', () => {
-
-                        beforeEach(() => {
-                            (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(urlNoMime));
-                            return parse(ValidAdMobCampaign);
-                        });
-
-                        it('should not call FileId.setFileId', () => { // when no mime type should default to streaming
-                            sinon.assert.notCalled(setFileIdSpy);
-                        });
-                    });
-
-                    describe('with a mime type in url', () => {
-                        beforeEach(() => {
-                            (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(url));
-                            return parse(ValidAdMobCampaign);
-                        });
-
-                        it('should FileId.setFileId with a mp4 mime type', () => {
-                            sinon.assert.calledWith(setFileIdSpy, url, 'G2KkvNWTNuU.mp4');
-                        });
-                    });
-
-                    describe('should cache', () => {
-
-                        beforeEach(() => {
-                            (<sinon.SinonStub>request.followRedirectChain).returns(Promise.resolve(url));
-                            return parse(ValidAdMobCampaign);
-                        });
-
-                        afterEach(() => {
-                            setFileIdSpy.resetHistory();
-                        });
-
-                        it('and call FileId.setFileId', () => {
-                            sinon.assert.calledOnce(setFileIdSpy);
-                            sinon.assert.calledWith(setFileIdSpy, url, 'G2KkvNWTNuU.mp4');
-                        });
-                    });
-                });
-            }
 
             describe('with a proper JSON payload', () => {
 
@@ -178,7 +71,6 @@ import { CoreConfiguration } from 'Core/Models/CoreConfiguration';
                 describe(`on ${Platform[platform]}`, () => {
                     beforeEach(() => {
                         sinon.stub(nativeBridge, 'getPlatform').returns(platform);
-                        setFileIdSpy.resetHistory();
                         return parse(ValidAdMobCampaign);
                     });
 
