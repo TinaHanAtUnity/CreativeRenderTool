c2eee860ce039676a638753ee0969a21d3785264
jasonm-unity <jasonm@unity3d.com>
2020-04-08 14:39:20 -0400

    Update src/ts/Ads/Managers/AutomatedExperimentManager.ts
    Co-Authored-By: Maxime Gregoire <maximegregoire@users.noreply.github.com>

--- src/ts/Ads/Managers/AutomatedExperimentManager.ts
+++ src/ts/Ads/Managers/AutomatedExperimentManager.ts
@@ -100,7 +100,7 @@ class ExperimentCategory {
 //  1. initialize()                             // Done by Ads
 //  2. registerExperimentCategory()             // for each category of interest
 //  3. (optional) getSelectedExperiment()       // gets the name of the experiment choosen by AUi/Optmz
-//  4. activeSelectedExperiment()               // Signals that experiment is underway -> awayting aan outcome
+//  4. activeSelectedExperiment()               // Signals that experiment is underway -> awaiting an outcome
 //  5. (Optional) rewardSelectedExperiment()    // when experiment result is found to be positive (this sends positive outcome back to AUI/Optm)
 //  6. endSelectedExperiment()                  // Once the experiment is over (success or not) (this sends negative outcome back to AUI/Optmz if no reward was sent)
 
