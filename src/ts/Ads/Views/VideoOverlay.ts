88caeb87b6c6774b8f241c8f2c8b0321e1765b31
Gascon1 <mikelgascon@outlook.com>
2020-04-29 15:31:34 -0400

    make private

--- src/ts/Ads/Views/VideoOverlay.ts
+++ src/ts/Ads/Views/VideoOverlay.ts
@@ -367,7 +367,7 @@ export class VideoOverlay extends AbstractVideoOverlay implements IPrivacyHandle
         this._handlers.forEach(handler => handler.onOverlayPauseForTesting(true));
     }
 
-    protected onClick(event: Event) {
+    private onClick(event: Event) {
         if (this._disableFadeOutOnClick) {
             return;
         }
