f82ad9cf300176b17b21b861fb263f1831479b57
Tina Han <yunduan@unity3d.com>
2020-04-29 16:13:33 -0700

    modify unit tests

--- src/ts/VAST/EventHandlers/TencentVastOverlayEventHandler.spec.ts
+++ src/ts/VAST/EventHandlers/TencentVastOverlayEventHandler.spec.ts
@@ -18,10 +18,7 @@ import { PrivacySDK } from 'Privacy/__mocks__/PrivacySDK';
 import { Store } from 'Store/__mocks__/Store';
 import { IAdUnitParameters } from 'Ads/AdUnits/AbstractAdUnit';
 import { Core } from 'Core/__mocks__/Core';
-import {
-    VastAdUnitWithClickThroughURL,
-    VastAdUnitWithoutClickThroughURL
-} from 'VAST/AdUnits/__mocks__/VastAdUnit';
+import { VastAdUnit } from 'VAST/AdUnits/__mocks__/VastAdUnit';
 import { TencentVastOverlayEventHandler } from 'VAST/EventHandlers/TencentVastOverlayEventHandler';
 
 [Platform.ANDROID, Platform.IOS].forEach(platform => {
@@ -56,7 +53,9 @@ import { TencentVastOverlayEventHandler } from 'VAST/EventHandlers/TencentVastOv
 
         describe('when call button get clicked and there is no click through url', () => {
             it('the error message should be returned to indicate the url is null',  () => {
-                vastTencentOverlayHandler = new TencentVastOverlayEventHandler(new VastAdUnitWithoutClickThroughURL(), baseParams);
+                const vastAdUnit = new VastAdUnit();
+                vastAdUnit.getVideoClickThroughURL.mockReturnValue(null);
+                vastTencentOverlayHandler = new TencentVastOverlayEventHandler(vastAdUnit, baseParams);
                 vastTencentOverlayHandler.onOverlayCallButton().catch((error) => {
                     expect(error).toEqual(new Error('No clickThroughURL was defined'));
                 }).then(() => {
@@ -69,7 +68,9 @@ import { TencentVastOverlayEventHandler } from 'VAST/EventHandlers/TencentVastOv
             let replacedUrl: string;
             beforeEach(async () => {
                 replacedUrl = 'https://c2.gdt.qq.com/gdt_click.fcg?viewid=mlsITUl9ffgwhhyanfhFC6PSKRWegO22lApBWRQXHAV39lzK_kqg8TYfD8aX53xMdP7MVrwCKH4Feu4!3oa_BBhWt4YBqeKe1CQG54t8WpQkNtl9VuTpec1s21Mb21TlcDl4w8Bslj9pyTeoEYdHJIhHkPDr0DGYUOOtUf1gtgaJ7oQKIOfEjVP8S8o9DdM7WTg450XRS0g72SnvSznljQsVbq9Q_TH!SjpIgcBMdqh0vD5u2_dh5VT5BuedXIn3Qkmk3PlWoRQ&jtype=0&i=1&os=1&lpp=click_ext=eyJnZHRfcHJvZHVjdF9pZCI6IjE0ODM4ODQ4NTgiLCJpc2Zyb213diI6MX0%3D&clklpp=__CLICK_LPP__&cdnxj=1&xp=3&acttype=35&s={"req_width":"0","req_height":"0","width":"0","height":"0","down_x":"10","down_y":"20","up_x":"10","up_y":"20"}';
-                vastTencentOverlayHandler = new TencentVastOverlayEventHandler(new VastAdUnitWithClickThroughURL(), baseParams);
+                const vastAdUnit = new VastAdUnit();
+                vastAdUnit.getVideoClickThroughURL.mockReturnValue('https://c2.gdt.qq.com/gdt_click.fcg?viewid=mlsITUl9ffgwhhyanfhFC6PSKRWegO22lApBWRQXHAV39lzK_kqg8TYfD8aX53xMdP7MVrwCKH4Feu4!3oa_BBhWt4YBqeKe1CQG54t8WpQkNtl9VuTpec1s21Mb21TlcDl4w8Bslj9pyTeoEYdHJIhHkPDr0DGYUOOtUf1gtgaJ7oQKIOfEjVP8S8o9DdM7WTg450XRS0g72SnvSznljQsVbq9Q_TH!SjpIgcBMdqh0vD5u2_dh5VT5BuedXIn3Qkmk3PlWoRQ&jtype=0&i=1&os=1&lpp=click_ext=eyJnZHRfcHJvZHVjdF9pZCI6IjE0ODM4ODQ4NTgiLCJpc2Zyb213diI6MX0%3D&clklpp=__CLICK_LPP__&cdnxj=1&xp=3&acttype=35&s={"req_width":"__REQ_WIDTH__","req_height":"__REQ_HEIGHT__","width":"__WIDTH__","height":"__HEIGHT__","down_x":"__DOWN_X__","down_y":"__DOWN_Y__","up_x":"__UP_X__","up_y":"__UP_Y__"}');
+                vastTencentOverlayHandler = new TencentVastOverlayEventHandler(vastAdUnit, baseParams);
                 await vastTencentOverlayHandler.onOverlayCallButton();
             });
             it('the url should be replaced', () => {
