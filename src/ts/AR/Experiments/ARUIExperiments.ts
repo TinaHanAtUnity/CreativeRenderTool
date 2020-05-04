5b1b50fe5f766b63e518b08caae03905517652d7
SamsonStannus <samson.stannus@samson.stannus-2Q3M>
2020-05-04 13:18:19 -0400

    remove unneeded separated declaration

--- src/ts/AR/Experiments/ARUIExperiments.ts
+++ src/ts/AR/Experiments/ARUIExperiments.ts
@@ -4,9 +4,7 @@ import { AutomatedExperimentsCategories, ArAvailableButtonExperiment } from 'Mab
 import { Campaign } from 'Ads/Models/Campaign';
 
 export function arAvailableButtonDecision(automatedExperimentManager: AutomatedExperimentManager, campaign: Campaign): IExperimentActionChoice {
-    let actions: IExperimentActionChoice | undefined;
-
-    actions = automatedExperimentManager.activateSelectedExperiment(campaign, AutomatedExperimentsCategories.MRAID_AR);
+    let actions: IExperimentActionChoice | undefined = automatedExperimentManager.activateSelectedExperiment(campaign, AutomatedExperimentsCategories.MRAID_AR);
 
     if (actions === undefined) {
         actions = ArAvailableButtonExperiment.getDefaultActions();
