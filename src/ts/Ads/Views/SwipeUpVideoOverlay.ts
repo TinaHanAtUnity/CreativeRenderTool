9923ae0fd38774133f8dd95394b66ed0268d29bc
Max Grégoire <maxime.gregoire@unity3d.com>
2020-05-05 17:46:12 -0400

    Add reward calls to swipe up event

--- src/ts/Ads/Views/SwipeUpVideoOverlay.ts
+++ src/ts/Ads/Views/SwipeUpVideoOverlay.ts
@@ -7,13 +7,14 @@ import { ClientInfo } from 'Core/Models/ClientInfo';
 import { CoreConfiguration } from 'Core/Models/CoreConfiguration';
 import { Placement } from 'Ads/Models/Placement';
 import { Campaign } from 'Ads/Models/Campaign';
-import { VideoOverlayDownloadExperiment, VideoOverlayDownloadExperimentDeclaration } from 'MabExperimentation/Models/AutomatedExperimentsList';
+import { VideoOverlayDownloadExperiment, VideoOverlayDownloadExperimentDeclaration, AutomatedExperimentsCategories } from 'MabExperimentation/Models/AutomatedExperimentsList';
 import { AUIMetric, SDKMetrics } from 'Ads/Utilities/SDKMetrics';
 import { AbstractPrivacy, IPrivacyHandlerView } from 'Ads/Views/AbstractPrivacy';
 import { PerformanceCampaign } from 'Performance/Models/PerformanceCampaign';
 import { XPromoCampaign } from 'XPromo/Models/XPromoCampaign';
 import { Template } from 'Core/Utilities/Template';
 import SwipeUpVideoOverlayTemplate from 'html/SwipeUpVideoOverlay.html';
+import { AutomatedExperimentManager } from 'MabExperimentation/AutomatedExperimentManager';
 
 export interface IVideoOverlayParameters<T extends Campaign> {
     platform: Platform;
@@ -27,6 +28,7 @@ export interface IVideoOverlayParameters<T extends Campaign> {
 
 export class SwipeUpVideoOverlay extends VideoOverlay {
     private _ctaMode: string;
+    private _automatedExperimentManager: AutomatedExperimentManager;
     protected _swipeUpButtonElement: HTMLElement;
 
     constructor(
@@ -34,11 +36,13 @@ export class SwipeUpVideoOverlay extends VideoOverlay {
         privacy: AbstractPrivacy,
         showGDPRBanner: boolean,
         showPrivacyDuringVideo: boolean,
-        combination: IExperimentActionChoice | undefined
+        combination: IExperimentActionChoice | undefined,
+        automatedExperimentManager: AutomatedExperimentManager
     ) {
         super(parameters, privacy, showGDPRBanner, showPrivacyDuringVideo);
 
         this._template = new Template(SwipeUpVideoOverlayTemplate, this._localization);
+        this._automatedExperimentManager = automatedExperimentManager;
 
         if (combination) {
             if (!VideoOverlayDownloadExperiment.isValid(combination)) {
@@ -120,5 +124,11 @@ export class SwipeUpVideoOverlay extends VideoOverlay {
         this.resetFadeTimer();
         this._handlers.forEach(handler => handler.onOverlayCallButton());
         this.triggerOnOverlayDownload();
+        this._automatedExperimentManager.rewardSelectedExperiment(this._campaign, AutomatedExperimentsCategories.VIDEO_OVERLAY);
+    }
+
+    public hide(): void {
+        super.hide();
+        this._automatedExperimentManager.endSelectedExperiment(this._campaign, AutomatedExperimentsCategories.VIDEO_OVERLAY);
     }
 }
