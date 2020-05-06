a142145379ead31840d0aaa0e54c856b064f7950
Gascon1 <mikelgascon@outlook.com>
2020-05-06 15:01:23 -0400

    fix git diff

--- src/ts/MabExperimentation/AutomatedExperimentManager.ts
+++ src/ts/MabExperimentation/AutomatedExperimentManager.ts
@@ -156,7 +156,6 @@ export class AutomatedExperimentManager {
                 return Promise.resolve();
             }
 
-
             return this.publishCampaignOutcomes(campaign, categorizedExp, CategorizedExperimentStage.ENDED);
      }
 
