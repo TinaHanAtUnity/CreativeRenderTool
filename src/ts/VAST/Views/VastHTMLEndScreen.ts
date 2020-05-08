bb3a13ded4ec27e0dc52be480f81c67ae06a33cd
Tina Han <yunduan@unity3d.com>
2020-05-08 13:34:11 -0700

    change style class name to be html-endcard specific

--- src/ts/VAST/Views/VastHTMLEndScreen.ts
+++ src/ts/VAST/Views/VastHTMLEndScreen.ts
@@ -50,12 +50,12 @@ export class VastHTMLEndScreen extends VastEndScreen implements IPrivacyHandlerV
             {
                 event: 'click',
                 listener: (event: Event) => this.onCloseEvent(event),
-                selector: '.btn-close-region-endcard'
+                selector: '.btn-close-html-endcard'
             },
             {
                 event: 'click',
                 listener: (event: Event) => this.onPrivacyEvent(event),
-                selector: '.privacy-button-endcard'
+                selector: '.privacy-button-html-endcard'
             }
         ];
         this._privacy.addEventHandler(this);
