14f2c42b464ad0300111ba206ca624e08ed66612
Gascon1 <mikelgascon@outlook.com>
2020-05-25 11:58:08 -0400

    create new test to check for error when getting a campaign without images, try colors in hex

--- src/ts/Core/Utilities/ColorTheme.spec.ts
+++ src/ts/Core/Utilities/ColorTheme.spec.ts
@@ -1,23 +1,51 @@
 import { Core } from 'Core/__mocks__/Core';
-import { PerformanceCampaign } from 'Performance/Models/__mocks__/PerformanceCampaign';
+import { PerformanceCampaign, PerformanceCampaignWithoutImages, PerformanceCampaignMock } from 'Performance/Models/__mocks__/PerformanceCampaign';
 import { ColorTheme } from 'Core/Utilities/ColorTheme.ts';
 
 describe('ColorTheme', () => {
-    const campaign = new PerformanceCampaign();
+    const campaignWithImages = new PerformanceCampaign();
+    const campaignWithoutImages = new PerformanceCampaignWithoutImages();
     const core = new Core().Api;
 
-    it('should successfully converts the 6 variants to their RGB values', async () => {
-        const getColorTheme = async () => {
-            return ColorTheme.renderColorTheme(campaign, core);
-        };
+    const getColorTheme = async (campaign: PerformanceCampaignMock) => {
+        return ColorTheme.renderColorTheme(campaign, core);
+    };
 
-        const theme = await getColorTheme();
-        expect(theme.baseColorTheme.light.toCssRgb()).toEqual('rgb(215, 186, 247)');
-        expect(theme.baseColorTheme.medium.toCssRgb()).toEqual('rgb(98, 21, 183)');
-        expect(theme.baseColorTheme.dark.toCssRgb()).toEqual('rgb(61, 13, 114)');
-        expect(theme.secondaryColorTheme.light.toCssRgb()).toEqual('rgb(206, 192, 242)');
-        expect(theme.secondaryColorTheme.medium.toCssRgb()).toEqual('rgb(73, 36, 168)');
-        expect(theme.secondaryColorTheme.dark.toCssRgb()).toEqual('rgb(45, 22, 105)');
-        expect(theme.secondaryColorTheme.dark.toCssRgb()).toEqual('rgb(45, 22, 105)');
+    // RGBToHex transforms a string in the "rgb(1, 12, 123)" format to the "#010C7B"
+    const RGBToHex = (rgb: string): string => {
+        const sep = rgb.indexOf(',') > -1 ? ',' : ' ';
+        const splitRgb = rgb.substr(4).split(')')[0].split(sep);
+        let res = '#';
+        splitRgb.forEach((component: string) => {
+            let c = (+component).toString(16);
+            if (c.length === 1) {
+                c = '0' + c;
+            }
+            res += c;
+        });
+        return res;
+    };
+
+    it('should successfully converts the 6 variants to their RGB values', async () => {
+        const theme = await getColorTheme(campaignWithImages);
+        expect(RGBToHex(theme.baseColorTheme.light.toCssRgb())).toEqual('#d7baf7');
+        expect(RGBToHex(theme.baseColorTheme.medium.toCssRgb())).toEqual('#6215b7');
+        expect(RGBToHex(theme.baseColorTheme.dark.toCssRgb())).toEqual('#3d0d72');
+        expect(RGBToHex(theme.secondaryColorTheme.light.toCssRgb())).toEqual('#cec0f2');
+        expect(RGBToHex(theme.secondaryColorTheme.medium.toCssRgb())).toEqual('#4924a8');
+        expect(RGBToHex(theme.secondaryColorTheme.dark.toCssRgb())).toEqual('#2d1669');
+    });
+
+    it('should throw an error if the campaign provided has invalid images', async () => {
+        await getColorTheme(campaignWithoutImages).catch((error) => {
+            expect(error.message).toEqual('invalid_image_assets');
+        });
     });
 });
+
+// expect(RGBToHex(theme.baseColorTheme.light.toCssRgb())).toEqual('rgb(215, 186, 247)');
+// expect(RGBToHex(theme.baseColorTheme.medium.toCssRgb())).toEqual('rgb(98, 21, 183)');
+// expect(RGBToHex(theme.baseColorTheme.dark.toCssRgb())).toEqual('rgb(61, 13, 114)');
+// expect(RGBToHex(theme.secondaryColorTheme.light.toCssRgb())).toEqual('rgb(206, 192, 242)');
+// expect(RGBToHex(theme.secondaryColorTheme.medium.toCssRgb())).toEqual('rgb(73, 36, 168)');
+// expect(RGBToHex(theme.secondaryColorTheme.dark.toCssRgb())).toEqual('rgb(45, 22, 105)');
