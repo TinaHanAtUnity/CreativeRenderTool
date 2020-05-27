0032d5c35c87b5f483013c30f1ad1dea524c4412
Gascon1 <mikelgascon@outlook.com>
2020-05-27 17:28:31 -0400

    test to see if we receive the proper image url

--- src/ts/Ads/Utilities/CampaignAssetInfo.spec.ts
+++ src/ts/Ads/Utilities/CampaignAssetInfo.spec.ts
@@ -8,12 +8,18 @@ describe('CampaignAssetInfoTest', () => {
     const core = new Core().Api;
 
     let orientedImage: Image;
-
+    let orientedImageUrl: string;
     beforeEach(async () => {
         orientedImage = await CampaignAssetInfo.getOrientedImage(campaign, core.DeviceInfo);
+        orientedImageUrl = orientedImage.getUrl();
     });
 
     it('should return an image when provided with a valid campaign', () => {
         expect(orientedImage).toBeInstanceOf(Image);
+    });
+
+    it('should return the image url', () => {
+        const expectedUrl = 'http://cdn-creatives-highwinds-prd.unityads.unity3d.com/assets/0fd53267-0620-4dce-b04f-dd70cecd4990/600x800.png';
+        expect(orientedImageUrl).toEqual(expectedUrl);
     });
 });
