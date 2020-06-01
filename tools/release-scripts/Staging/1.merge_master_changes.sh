833a0ddf4a404e005503f3a545010fced5a86d5f
Andrew Konecny <andrewko@unity3d.com>
2020-06-01 15:54:23 -0700

    Update

--- tools/release-scripts/Staging/1.merge_master_changes.sh
+++ tools/release-scripts/Staging/1.merge_master_changes.sh
@@ -42,8 +42,8 @@ do
 
         case "$answer" in
             h)
-                echo "\n\tPress 'c' to automatically commit changes"
-                echo "\n\tPress 'n' to continue to the next branch"
+                echo -e "\n\tPress 'c' to automatically commit changes"
+                echo -e "\n\tPress 'n' to continue to the next branch"
                 ;;
 
             c)
