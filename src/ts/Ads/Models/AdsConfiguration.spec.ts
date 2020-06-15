a797e01bfbb6dd0c0b4e9c5f80ddbd68b2d52860
Alice He <alice041189@gmail.com>
2020-06-15 16:21:54 -0700

    add function getPlacementsForGroupId and test case

--- src/ts/Ads/Models/AdsConfiguration.spec.ts
+++ src/ts/Ads/Models/AdsConfiguration.spec.ts
@@ -25,21 +25,25 @@ describe('AdsConfigurationTest', () => {
             'placement_1': new Placement ({
                 id: 'placement_1',
                 adUnitId: 'adUnitId_1',
+                groupId: 'groupId_1',
                 ...commonFields
             }),
             'placement_2': new Placement ({
                 id: 'placement_2',
                 adUnitId: 'adUnitId_1',
+                groupId: 'groupId_1',
                 ...commonFields
             }),
             'placement_3': new Placement ({
                 id: 'placement_3',
                 adUnitId: 'adUnitId_1',
+                groupId: 'groupId_1',
                 ...commonFields
             }),
             'placement_4': new Placement ({
                 id: 'placement_4',
                 adUnitId: 'adUnitId_2',
+                groupId: 'groupId_2',
                 ...commonFields
             }),
             'placement_5': new Placement ({
@@ -49,6 +53,7 @@ describe('AdsConfigurationTest', () => {
             'placement_6': new Placement ({
                 id: 'placement_6',
                 adUnitId: 'adUnitId_2',
+                groupId: 'groupId_2',
                 ...commonFields
             })
         };
@@ -73,6 +78,18 @@ describe('AdsConfigurationTest', () => {
         ].forEach(({ adUnitId, expectedPlacements }) => {
             it(`should return valid placements for ${adUnitId}`, () => {
                 expect(adsConfiguration.getPlacementsForAdunit(adUnitId)).toEqual(expectedPlacements);
+            });
+        });
+    });
+
+    describe('normal behavior of getPlacementsForGroupId', () => {
+        [
+            { groupId: 'groupId_1', expectedPlacements: ['placement_1', 'placement_2', 'placement_3'] },
+            { groupId: 'groupId_2', expectedPlacements: ['placement_4', 'placement_6'] },
+            { groupId: 'unknown_groupId', expectedPlacements: [] }
+        ].forEach(({ groupId, expectedPlacements }) => {
+            it(`should return valid placements for ${groupId}`, () => {
+                expect(adsConfiguration.getPlacementsForGroupId(groupId)).toEqual(expectedPlacements);
             });
         });
     });
