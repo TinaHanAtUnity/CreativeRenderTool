96334319bfc2333db9de9c5a119da4c66006d5e8
Mikel Gascon <mikelgascon@outlook.com>
2020-06-16 17:23:03 -0400

    create color utils and tests for it. add colors needed for tests

---
+++ src/ts/Core/Utilities/ColorUtils.spec.ts
@@ -0,0 +1,20 @@
+import { ColorUtils } from 'Core/Utilities/ColorUtils';
+
+describe('ColorUtils', () => {
+    // the colors are assigned to a variable here for clarity,
+    // but these values come from AutomatedExperimentsList
+    const darkBlue = '0052c7';
+    const blue = '167dfb';
+
+    it('recieves a color that starts with DARK and returns true', () => {
+        expect(ColorUtils.isDarkSchemeColor(darkBlue)).toEqual(true);
+    });
+
+    it(`recieves a color that doesn't start with DARK and returns false`, () => {
+        expect(ColorUtils.isDarkSchemeColor(blue)).toEqual(false);
+    });
+
+    it('receives undefined and returns undefined', () => {
+        expect(ColorUtils.isDarkSchemeColor(undefined)).toEqual(undefined);
+    });
+});
