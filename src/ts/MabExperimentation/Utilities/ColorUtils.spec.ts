f51dc3212f04ffec311f3606ba346a617e2e5eb4
Mikel Gascon <mikelgascon@outlook.com>
2020-06-17 12:08:34 -0400

    make tests more thorough

--- src/ts/MabExperimentation/Utilities/ColorUtils.spec.ts
+++ src/ts/MabExperimentation/Utilities/ColorUtils.spec.ts
@@ -1,20 +1,18 @@
 import { ColorUtils } from 'MabExperimentation/Utilities/ColorUtils.ts';
+import { EndScreenExperimentDeclaration } from 'MabExperimentation/Models/AutomatedExperimentsList';
 
 describe('ColorUtils', () => {
-    // the colors are assigned to a variable here for clarity,
-    // but these values come from AutomatedExperimentsList
-    const darkBlue = '0052c7';
-    const blue = '167dfb';
-
-    it('recieves a color that starts with DARK and returns true', () => {
-        expect(ColorUtils.isDarkSchemeColor(darkBlue)).toEqual(true);
-    });
-
-    it(`recieves a color that doesn't start with DARK and returns false`, () => {
-        expect(ColorUtils.isDarkSchemeColor(blue)).toEqual(false);
-    });
-
-    it('receives undefined and returns undefined', () => {
-        expect(ColorUtils.isDarkSchemeColor(undefined)).toEqual(undefined);
+    describe(`returns true if the color starts with DARK, false if it doesn't, undefined if it's undefined`, () => {
+        Object.entries(EndScreenExperimentDeclaration.color).forEach(([name, colorCode]) => {
+            if (colorCode !== undefined) {
+                it(`${name}:${colorCode}`, () => {
+                    expect(ColorUtils.isDarkSchemeColor(colorCode)).toEqual(name.startsWith('DARK'));
+                });
+            } else {
+                it(`${name}:${colorCode}`, () => {
+                    expect(ColorUtils.isDarkSchemeColor(colorCode)).toEqual(undefined);
+                });
+            }
+        });
     });
 });
