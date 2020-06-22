6c6a518f47f12e5f90c310059726de6305342e91
Sergey Yanchi <sergeyyanchi@unity3d.com>
2020-06-22 11:44:53 -0700

    invalidate only already show programmatic campaign

--- src/ts/Ads/Managers/PerPlacementLoadManagerV5NoInvalidation.ts
+++ src/ts/Ads/Managers/PerPlacementLoadManagerV5NoInvalidation.ts
@@ -1,4 +1,7 @@
 import { PerPlacementLoadManagerV5 } from 'Ads/Managers/PerPlacementLoadManagerV5';
+import { LoadV5 } from 'Ads/Utilities/SDKMetrics';
+import { PerformanceAdUnitFactory } from 'Performance/AdUnits/PerformanceAdUnitFactory';
+import { PlacementState } from 'Ads/Models/Placement';
 
 export class PerPlacementLoadManagerV5NoInvalidation extends PerPlacementLoadManagerV5 {
     protected onNoFill(placementId: string): void {
@@ -6,6 +9,36 @@ export class PerPlacementLoadManagerV5NoInvalidation extends PerPlacementLoadMan
 
         if (placement) {
             placement.setInvalidationPending(false);
+
+            let shouldInvalidate = false;
+            let isPerformanceAd = false;
+
+            // Invalidate only Programmatic campaigns if it was already shown before.
+            const campaign = placement.getCurrentCampaign();
+            if (campaign) {
+                const contentType = campaign.getContentType();
+                switch (contentType) {
+                    case PerformanceAdUnitFactory.ContentType:
+                    case PerformanceAdUnitFactory.ContentTypeMRAID:
+                    case PerformanceAdUnitFactory.ContentTypeVideo:
+                        isPerformanceAd = true;
+                        break;
+                    default:
+                        isPerformanceAd = false;
+                }
+
+                if (!isPerformanceAd && this._lastShownCampaignId && campaign.getUniqueId() === this._lastShownCampaignId) {
+                    this._adRequestManager.reportMetricEvent(LoadV5.RefreshManagerForcedToInvalidate);
+                    shouldInvalidate = true;
+                }
+            }
+
+            if (shouldInvalidate) {
+                this._adRequestManager.reportMetricEvent(LoadV5.RefreshManagerCampaignFailedToInvalidate);
+                placement.setCurrentCampaign(undefined);
+                placement.setCurrentTrackingUrls(undefined);
+                this.setPlacementState(placementId, PlacementState.NO_FILL);
+            }
         }
     }
 }
