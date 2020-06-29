c63f368244a8d6ebf0d3974ba9ff9f522078b7d5
Ross Rothenstine <rossr@unity3d.com>
2020-06-29 10:50:46 -0700

    Add TrackingManagerApi

--- src/ts/Core/Core.ts
+++ src/ts/Core/Core.ts
@@ -54,6 +54,7 @@ import { NoGzipCacheManager } from 'Core/Managers/NoGzipCacheManager';
 import { createMetricInstance } from 'Ads/Networking/MetricInstance';
 import { createStopwatch } from 'Core/Utilities/Stopwatch';
 import { IsMadeWithUnity } from 'Ads/Utilities/IsMadeWithUnity';
+import { TrackingManagerApi } from 'Core/Native/iOS/TrackingManager';
 
 export class Core implements ICore {
 
@@ -107,7 +108,8 @@ export class Core implements ICore {
                 MainBundle: new MainBundleApi(nativeBridge),
                 Notification: new NotificationApi(nativeBridge),
                 Preferences: new IosPreferencesApi(nativeBridge),
-                UrlScheme: new UrlSchemeApi(nativeBridge)
+                UrlScheme: new UrlSchemeApi(nativeBridge),
+                TrackingManager: new TrackingManagerApi(nativeBridge)
             } : undefined
         };
 
